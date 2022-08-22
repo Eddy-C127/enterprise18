0902250085767cd7f1168c84e8c3717dd3d6c8ac
hiroh-odoo <hiro@odoo.com>
2022-08-22 17:59:42 +0530

    [IMP] project_account_budget: improve ux of project update
    Purpose of this commit to improve the generic usage of
    sale_timesheet_account_budget app
    
    So in this commit done the following changes:
      - add budget content in 'project.update' description.
    
    task-2956738
    
    closes odoo/enterprise#31499
    
    Related: odoo/odoo#100560
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>
    Co-authored-by: Xavier Bol (xbo) <xbo@odoo.com>

--- project_account_budget/models/project_update.py
+++ project_account_budget/models/project_update.py
@@ -1,7 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, models
-from odoo.tools import format_amount
 
 class ProjectUpdate(models.Model):
     _inherit = 'project.update'
@@ -10,15 +9,29 @@ class ProjectUpdate(models.Model):
     def _get_template_values(self, project):
         vals = super()._get_template_values(project)
         if project.analytic_account_id and self.user_has_groups('account.group_account_readonly'):
-            vals['show_activities'] = project.budget or vals.get('show_activities')
-            vals['show_profitability'] = project.budget or vals.get('show_profitability')
+            budget_items = project._get_budget_items(with_action=False)
+            budgets = {
+                'data': [],
+                'total': {
+                    'allocated': 0.0,
+                    'spent': 0.0,
+                },
+            }
+            if budget_items:
+                budgets = {
+                    'data': budget_items['data'],
+                    'total': budget_items['total'],
+                }
+            for budget in budgets['data']:
+                budget['progress'] = budget['allocated'] and (budget['spent'] - budget['allocated']) / abs(budget['allocated'])
+            vals['show_activities'] = bool(project.budget) or vals.get('show_activities', False)
+            vals['show_profitability'] = bool(project.budget) or vals.get('show_profitability', False)
             budget = project.budget
             cost = -project._get_budget_items()['total']['spent']
             vals['budget'] = {
                 'percentage': round((cost / budget) * 100 if budget != 0 else 0, 0),
-                'amount': format_amount(self.env, budget, project.currency_id),
-                'spent_budget': format_amount(self.env, - cost, project.currency_id),
-                'remaining_budget': format_amount(self.env, budget - cost, project.currency_id),
+                'data': budgets['data'],
+                'total': budgets['total'],
                 'remaining_budget_percentage': round(((budget - cost) / budget) * 100 if budget != 0 else 0, 0),
             }
         return vals
