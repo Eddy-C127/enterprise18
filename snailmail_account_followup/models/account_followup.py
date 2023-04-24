f14650451e9bc0d731545ea57b397b389c948c44
william-andre <wan@odoo.com>
2023-04-24 17:00:12 +0200

    [FIX] snailmail_account_followup: do not send by default
    The module is auto-installed, and activating its feature is paying, so
    not desired.
    
    opw-3180544
    
    closes odoo/enterprise#57313
    
    X-original-commit: 66183b017a5e31e79fd4c30ca3ce5caab18ebb29
    Signed-off-by: Quentin De Paoli <qdp@odoo.com>
    Signed-off-by: William André (wan) <wan@odoo.com>

--- snailmail_account_followup/models/account_followup.py
+++ snailmail_account_followup/models/account_followup.py
@@ -7,4 +7,4 @@ from odoo import fields, models
 class FollowupLine(models.Model):
     _inherit = 'account_followup.followup.line'
 
-    send_letter = fields.Boolean('Send a Letter', default=True)
+    send_letter = fields.Boolean('Send a Letter', default=False)
