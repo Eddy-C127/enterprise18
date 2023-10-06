5e802bebb064bd6b83491090a6f88cca4be6b1c0
Vincent Larcin <vila@odoo.com>
2023-10-06 10:46:35 +0200

    [REF] project_enterprise: adapt portal task groupby
    This commit changes the override of a method used to get the groupbys
    for tasks in the portal to adapt it to a new simpler way of handling things.
    
    Community: odoo/odoo#132877
    
    Task-3444823
    
    closes odoo/enterprise#46173
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- project_enterprise/controllers/portal.py
+++ project_enterprise/controllers/portal.py
@@ -6,6 +6,6 @@ from odoo.addons.project.controllers.portal import ProjectCustomerPortal
 
 class ProjectPortal(ProjectCustomerPortal):
     def _task_get_searchbar_sortings(self, milestones_allowed, project=False):
-        values = super()._task_get_searchbar_sortings(milestones_allowed, project)
-        values['planned_date_begin'] = {'label': _('Planned Date'), 'order': 'planned_date_begin asc', 'sequence': 7}
-        return values
+        return super()._task_get_searchbar_sortings(milestones_allowed, project) | {
+            'planned_date_begin asc': {'label': _('Planned Date'), 'sequence': 70},
+        }
