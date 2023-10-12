815b4cdafe3b1cd5b6f902c8720877c5ab377302
Kacper Gasior <kaga@odoo.com>
2023-10-12 13:29:55 +0200

    [IMP] hr_referral: not hired filter in mail abour refused applicant
    So far URL send via mail about referred applicant being refused didn't lead
    to not hired referals. To change that new action with appropriate filter
    set as default one was added and URL was changed to refer to that action
    
    task-3547079
    
    closes odoo/enterprise#48852
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- hr_referral/models/hr_applicant.py
+++ hr_referral/models/hr_applicant.py
@@ -124,16 +124,19 @@ class Applicant(models.Model):
     def archive_applicant(self):
         for applicant in self:
             if applicant.ref_user_id:
-                applicant._send_notification(_("Sorry, your referral %s has been refused in the recruitment process.", applicant.name))
+                applicant._send_notification(
+                    body=_("Sorry, your referral %s has been refused in the recruitment process.", applicant.name),
+                    action_value='hr_referral.action_hr_refused_applicant_employee_referral'
+                )
         self.write({'referral_state': 'closed'})
         return super(Applicant, self).archive_applicant()
 
-    def _send_notification(self, body):
+    def _send_notification(self, body, action_value='hr_referral.action_hr_applicant_employee_referral'):
         if self.partner_name:
             subject = _('Referral: %s (%s)', self.partner_name, self.name)
         else:
             subject = _('Referral: %s', self.name)
-        url = url_encode({'action': 'hr_referral.action_hr_applicant_employee_referral', 'active_model': self._name})
+        url = url_encode({'action': action_value, 'active_model': self._name})
         action_url = '/web#' + url
         body = Markup("<a class='o_document_link' href=%s>%s</a><br>%s") % (action_url, subject, body)
         odoobot = self.env.ref('base.partner_root')
