df26bb671ddb586fc8d2c140e9c81842e05ed3c8
ShreyaPatel(shpa) <shpa@odoo.com>
2023-10-26 06:03:39 +0000

    [FIX] mrp_workorder: add steps to operations
    Before this commit:
    -----------------------------
    - It is possible to add a step to an operation in the tablet view if it already
      has any steps, but it is not possible if it doesn't have any steps initially.
    
    After this commit:
    -----------------------------
    Case 1 : When an operation has no steps, clicking on 'Worksheet Improvement'
             will display only the 'Add Step' button.
    Case 2 : When an operation has steps, clicking on 'Worksheet Improvement' will
             show all buttons.
    - In both cases, clicking the 'Add Step' button will directly open a popup for
      adding a step.
    
    task_id : 3551582
    
    closes odoo/enterprise#53742
    
    X-original-commit: e8888666e722d19782b6818befd91a4cf39a3007
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- mrp_workorder/static/src/mrp_display/dialog/mrp_menu_dialog.js
+++ mrp_workorder/static/src/mrp_display/dialog/mrp_menu_dialog.js
@@ -98,7 +98,8 @@ export class MrpMenuDialog extends Component {
     }
 
     addStep(){
-        this.proposeChange('add_step');
+        const check = this.props.params.checks.length ? this.props.params.checks.at(-1).resId : [];
+        this.proposeChangeForCheck("add_step", check);
     }
 
     removeStep(){
