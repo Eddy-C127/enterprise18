cba700febafc45f66bf2553afff1c8f20a8ae399
Laurent Smet <las@odoo.com>
2023-11-21 10:42:31 +0000

    [FIX] account_accountant: Fix retrieval of st_line bank account from another company
    - Create a bank account linked to a partner in company A.
    - Create a statement line with the same account_number in company B.
    - Open the bank reco widget using a user having access to company A and B
    => check_company failed when validating the statement line.
    
    closes odoo/enterprise#51980
    
    X-original-commit: 48006c38f9a981bc4a8da5adc1780afaa104d410
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>

--- account_accountant/models/account_bank_statement.py
+++ account_accountant/models/account_bank_statement.py
@@ -184,7 +184,7 @@ class AccountBankStatementLine(models.Model):
             account_number_nums = sanitize_account_number(self.account_number)
             if account_number_nums:
                 domain = [('sanitized_acc_number', 'ilike', account_number_nums)]
-                for extra_domain in (self.env['res.partner.bank']._check_company_domain(self.company_id), []):
+                for extra_domain in ([('company_id', 'parent_of', self.company_id.id)], [('company_id', '=', False)]):
                     bank_accounts = self.env['res.partner.bank'].search(extra_domain + domain)
                     if len(bank_accounts.partner_id) == 1:
                         return bank_accounts.partner_id
