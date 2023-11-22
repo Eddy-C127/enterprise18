25ed4b94fda5aaa54c3fdbd2de0e89b0b0a6b3bb
kdeb@odoo.com <kdeb@odoo.com>
2023-11-22 13:48:30 +0000

    [FIX] industry_fsm_sale: enable quotation creation from task
    Issue:
    In a mono-company setup, users trying to create a quotation from a task
    face an error message because the mandatory 'company' field in the
    'other info' tab is not visible, unlike in multi-company setups.
    
    Steps to Reproduce:
    1. Install the Field Service app.
    2. Activate 'Extra Quotations' in the Field Service general settings.
    3. Enable 'Extra Quotation' in the settings of the relevant project.
    4. Try creating a quotation from a task in a mono-company environment.
    5. Encounter an error.
    
    Resolution:
    Modified the functionality so that the 'company_id' field defaults to
    the current company.
    
    opw-3596984
    
    closes odoo/enterprise#52057
    
    X-original-commit: 223b24572146bd799f48a8069bb5a54d43bd2d9c
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>
    Signed-off-by: Kawtar Drissi El Bouzaidi (kdeb) <kdeb@odoo.com>

--- industry_fsm_sale/models/project_task.py
+++ industry_fsm_sale/models/project_task.py
@@ -271,7 +271,7 @@ class Task(models.Model):
                 'fsm_mode': True,
                 'default_partner_id': self.partner_id.id,
                 'default_task_id': self.id,
-                'default_company_id': self.company_id.id,
+                'default_company_id': self.company_id.id or self.env.company.id,
                 'default_origin': _("%(project_name)s - %(task_name)s", project_name=self.project_id.name, task_name=self.name),
             },
         })
