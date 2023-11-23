217b610cda50cec53e7b075de3d6dfb401b51cfa
mhch-odoo <mhch@odoo.com>
2023-11-23 11:07:40 +0800

    [FIX] l10n_au_aba: RedirectWarning for batch payments
    Changes ValidationError with RedirectWarning to allow
    quick back account configuration for users.
    
    task - 3576607
    
    closes odoo/enterprise#53032
    
    X-original-commit: b82b8ae8842e7105f6c18e116417b43bd0016916
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>

--- l10n_au_aba/models/account_payment.py
+++ l10n_au_aba/models/account_payment.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, models, _
-from odoo.exceptions import ValidationError
+from odoo.exceptions import ValidationError, RedirectWarning
 
 
 class AccountPayment(models.Model):
@@ -23,19 +23,29 @@ class AccountPayment(models.Model):
     @api.constrains('payment_method_line_id', 'journal_id', 'currency_id')
     def _l10n_au_aba_check_bank_account(self):
         aba_payment_method = self.env.ref('l10n_au_aba.account_payment_method_aba_ct')
+        def _redirect_journal(journal, message):
+            raise RedirectWarning(
+                        message=message,
+                        action={
+                            "view_mode": "form",
+                            "res_model": "account.journal",
+                            "type": "ir.actions.act_window",
+                            "views": [[False, "form"]],
+                            "res_id": journal.id,
+                            "target": "new",
+                        },
+                        button_text="Configure Journal"
+                    )
         for rec in self:
             if rec.payment_method_id == aba_payment_method:
                 bank_acc = rec.journal_id.bank_account_id
 
                 if rec.currency_id.name != 'AUD':
                     raise ValidationError(_('ABA payments must be defined in AUD.'))
-
                 if bank_acc.acc_type != 'aba' or not bank_acc.aba_bsb:
-                    raise ValidationError(_("Journal '%s' requires a proper ABA account. Please configure it first.", rec.journal_id.name))
-
+                    _redirect_journal(rec.journal_id, _("Journal '%s' requires a proper ABA account. Please configure the Account first.", rec.journal_id.name))
                 if not rec.journal_id.aba_user_spec or not rec.journal_id.aba_fic or not rec.journal_id.aba_user_number:
-                    raise ValidationError(_("Please fill in the ABA data of account %s (journal %s) before using it to generate ABA payments.",
-                        bank_acc.acc_number, rec.journal_id.name))
+                    _redirect_journal(rec.journal_id, _("Please fill in the ABA data of account %s (journal %s) before using it to generate ABA payments.", bank_acc.acc_number, rec.journal_id.name))
 
     @api.constrains('payment_method_line_id', 'partner_bank_id')
     def _check_partner_bank_account(self):
@@ -43,4 +53,16 @@ class AccountPayment(models.Model):
         for rec in self:
             if rec.payment_method_id == aba_payment_method:
                 if rec.partner_bank_id.acc_type != 'aba' or not rec.partner_bank_id.aba_bsb:
-                    raise ValidationError(_("The partner requires a bank account with a valid BSB and account number. Please configure it first."))
+                    raise RedirectWarning(
+                        message=_("The partner requires a bank account with a valid BSB and account number.\
+                            Please configure the following account first:\n %s.", rec.partner_bank_id.display_name),
+                        action={
+                            "view_mode": "form",
+                            "res_model": "res.partner.bank",
+                            "type": "ir.actions.act_window",
+                            "views": [[False, "form"]],
+                            "res_id": rec.partner_bank_id.id,
+                            "target": "new",
+                        },
+                        button_text="Configure Account"
+                    )
