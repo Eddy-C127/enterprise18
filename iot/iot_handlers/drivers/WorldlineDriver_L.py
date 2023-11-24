2e90da3eefaf9a5696f72c110fe53a10eeccfc1d
lse-odoo <lse@odoo.com>
2023-11-24 11:55:25 +0100

    [IMP] iot_handlers: prevent delay after Worldline transaction
    Before this commit:
     Once a payment have been validated by Worldline payment terminal
     the PoS cashier would be forced to wait 5 seconds before receiving
     the validation on the PoS side.
    
     This delay was added to fix:
     #46684
     But the delay could be added in a "smarter" way:
     Rather than force sleeping at the end of the transaction (before code),
     we would sleep on incoming requests only if the previous transaction
     was done "too recently"
    
    After this commit:
     The result of the terminal (validation, cancellation, ...) is executed
     right away. But new transactions/interaction might be delayed if it is
     too close in time with the previous one.
    
    Also added the logger and some logs for troubleshooting future cases.
    
    opw-3462147
    
    closes odoo/enterprise#52095
    
    X-original-commit: 5270cfbbf918a70a4757e3b90897985185335eec
    Signed-off-by: Loan Sens (lse) <lse@odoo.com>

--- iot/iot_handlers/drivers/WorldlineDriver_L.py
+++ iot/iot_handlers/drivers/WorldlineDriver_L.py
@@ -2,6 +2,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import ctypes
+import datetime
+import logging
 from pathlib import Path
 from time import sleep
 from threading import Thread
@@ -9,6 +11,7 @@ from threading import Thread
 from odoo.addons.hw_drivers.driver import Driver
 from odoo.addons.hw_drivers.event_manager import event_manager
 
+_logger = logging.getLogger(__name__)
 
 easyCTEPPath = Path(__file__).parent.parent / 'lib/ctep/libeasyctep.so'
 
@@ -63,6 +66,8 @@ easyCTEP.lastTransactionStatus.argtypes = [
 class WorldlineDriver(Driver):
     connection_type = 'ctep'
 
+    DELAY_TIME_BETWEEN_TRANSACTIONS = 5  # seconds
+
     def __init__(self, identifier, device):
         super(WorldlineDriver, self).__init__(identifier, device)
         self.device_type = 'payment'
@@ -75,6 +80,7 @@ class WorldlineDriver(Driver):
         self._actions.update({
             '': self._action_default,
         })
+        self.next_transaction_min_dt = datetime.datetime.min
 
     @classmethod
     def supported(cls, device):
@@ -82,12 +88,25 @@ class WorldlineDriver(Driver):
         return True
 
     def _action_default(self, data):
+        _logger.debug('_action_default %s %s', data['messageType'], data)
         if data['messageType'] == 'Transaction':
             Thread(target=self.processTransaction, args=(data.copy(), self.data['owner'])).start()
         elif data['messageType'] == 'Cancel':
             Thread(target=self.cancelTransaction).start()
         elif data['messageType'] == 'LastTransactionStatus':
             Thread(target=self.lastTransactionStatus).start()
+
+    def _check_transaction_delay(self):
+        # After a payment has been processed, the display on the terminal still shows some
+        # information for about 4-5 seconds. No request can be processed during this period.
+        delay_diff = (self.next_transaction_min_dt - datetime.datetime.now()).total_seconds()
+        if delay_diff > 0:
+            if delay_diff > self.DELAY_TIME_BETWEEN_TRANSACTIONS:
+                # Theoretically not possible, but to avoid sleeping for ages, we cap the value
+                _logger.warning('Transaction delay difference is too high %.2f force set as default', delay_diff)
+                delay_diff = self.DELAY_TIME_BETWEEN_TRANSACTIONS
+            _logger.info('Previous transaction is too recent, will sleep for %.2f seconds', delay_diff)
+            sleep(delay_diff)
 
     def processTransaction(self, transaction, owner):
         self.cid = transaction['cid']
@@ -96,6 +115,8 @@ class WorldlineDriver(Driver):
         if transaction['amount'] <= 0:
             return self.send_status(error='The terminal cannot process negative or null transactions.')
 
+        # Force to wait before starting the transaction if necessary
+        self._check_transaction_delay()
         # Notify transaction start
         self.send_status(stage='WaitingForCard')
 
@@ -104,20 +125,23 @@ class WorldlineDriver(Driver):
         customer_receipt = ctypes.create_string_buffer(BUFFER_SIZE)
         card = ctypes.create_string_buffer(BUFFER_SIZE)
         error_code = ctypes.create_string_buffer(BUFFER_SIZE)
+        transaction_id = transaction['TransactionID']
+        transaction_amount = transaction['amount'] / 100
+        transaction_action_identifier = transaction['actionIdentifier']
+        _logger.info('start transaction #%d amount: %f action_identifier: %d', transaction_id, transaction_amount, transaction_action_identifier)
         result = easyCTEP.startTransaction(
             ctypes.byref(self.dev), # std::shared_ptr<ect::CTEPTerminal> trm
-            ctypes.c_char_p(str(transaction['amount'] / 100).encode('utf-8')),  # char const* amount
-            ctypes.c_char_p(str(transaction['TransactionID']).encode('utf-8')), # char const* reference
-            ctypes.c_ulong(transaction['actionIdentifier']),    # unsigned long action_identifier
+            ctypes.c_char_p(str(transaction_amount).encode('utf-8')),  # char const* amount
+            ctypes.c_char_p(str(transaction_id).encode('utf-8')), # char const* reference
+            ctypes.c_ulong(transaction_action_identifier),    # unsigned long action_identifier
             merchant_receipt,   # char* merchant_receipt
             customer_receipt, # char* customer_receipt
             card,   # char* card
             error_code, # char* error
         )
+        _logger.debug('finished transaction #%d with result %d', transaction_id, result)
 
-        # After a payment has been processed, the display on the terminal still shows some
-        # information for about 4-5 seconds. No request can be processed during this period.
-        sleep(5)
+        self.next_transaction_min_dt = datetime.datetime.now() + datetime.timedelta(seconds=self.DELAY_TIME_BETWEEN_TRANSACTIONS)
 
         if result == 1:
             # Transaction successful
@@ -133,6 +157,7 @@ class WorldlineDriver(Driver):
             # Transaction failed
             if error_code not in IGNORE_ERRORS:
                 error_msg = '%s (Error code: %s)' % (TERMINAL_ERRORS.get(error_code, 'Transaction was not processed correctly'), error_code)
+                logging.info(error_msg)
                 self.send_status(error=error_msg)
             # Transaction was cancelled
             else:
@@ -142,14 +167,19 @@ class WorldlineDriver(Driver):
             self.send_status(disconnected=True)
 
     def cancelTransaction(self):
+        # Force to wait before starting the transaction if necessary
+        self._check_transaction_delay()
         self.send_status(stage='waitingCancel')
 
         error_code = ctypes.create_string_buffer(BUFFER_SIZE)
+        _logger.info("cancel transaction request")
         result = easyCTEP.abortTransaction(ctypes.byref(self.dev), error_code) # std::shared_ptr<ect::CTEPTerminal> trm
+        _logger.debug("end cancel transaction request")
 
         if not result:
             error_code = error_code.value.decode('utf-8')
             error_msg = '%s (Error code: %s)' % (TERMINAL_ERRORS.get(error_code, 'Transaction could not be cancelled'), error_code)
+            _logger.info(error_msg)
             self.send_status(stage='Cancel', error=error_msg)
 
     def lastTransactionStatus(self):
@@ -157,7 +187,9 @@ class WorldlineDriver(Driver):
         amount = ctypes.c_double()
         time = ctypes.create_string_buffer(BUFFER_SIZE)
         error_code = ctypes.create_string_buffer(BUFFER_SIZE)
+        _logger.info("last transaction status request")
         result = easyCTEP.lastTransactionStatus(ctypes.byref(self.dev), ctypes.byref(action_identifier), ctypes.byref(amount), time, error_code)
+        _logger.debug("end last transaction status request")
 
         if result:
             self.send_status(value={
@@ -186,4 +218,6 @@ class WorldlineDriver(Driver):
             'owner': self.owner or self.data['owner'],
             'cid': self.cid,
         }
+        # TODO: add `stacklevel=2` in image with python version > 3.8
+        _logger.debug('send_status data: %s', self.data, stack_info=True)
         event_manager.device_changed(self)
