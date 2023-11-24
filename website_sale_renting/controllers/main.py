61ed458aa37e68a1c44b0ec233363291c5d071a6
Victor Feyens <vfe@odoo.com>
2023-11-24 16:31:35 +0100

    [MOV] website_sale_renting: merge controllers
    The two controllers extend the same website_sale class and have no reason
    to be split.  Furthermore, the product file name made no sens as it extended
    cart update routes.
    
    Part-of: odoo/enterprise#45427

--- website_sale_renting/controllers/main.py
+++ website_sale_renting/controllers/main.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields
@@ -67,3 +66,41 @@ class WebsiteSaleRenting(WebsiteSale):
             end_date=kwargs.get('end_date'),
         )
         return result
+
+    @route()
+    def cart_update(self, *args, start_date=None, end_date=None, **kw):
+        """ Override to parse to datetime optional pickup and return dates.
+        """
+        start_date = fields.Datetime.to_datetime(start_date)
+        end_date = fields.Datetime.to_datetime(end_date)
+        return super().cart_update(*args, start_date=start_date, end_date=end_date, **kw)
+
+    @route()
+    def cart_update_json(self, *args, start_date=None, end_date=None, **kwargs):
+        """ Override to parse to datetime optional pickup and return dates.
+        """
+        start_date = fields.Datetime.to_datetime(start_date)
+        end_date = fields.Datetime.to_datetime(end_date)
+        return super().cart_update_json(
+            *args, start_date=start_date, end_date=end_date, **kwargs
+        )
+
+    @route(
+        '/rental/product/constraints', type='json', auth="public", methods=['POST'], website=True
+    )
+    def renting_product_constraints(self):
+        """ Return rental product constraints.
+
+        Constraints are the days of the week where no pickup nor return can be processed and the
+        minimal time of a rental.
+
+        :rtype: dict
+        """
+        weekdays = request.env.company._get_renting_forbidden_days()
+        return {
+            'renting_unavailabity_days': {day: day in weekdays for day in range(1, 8)},
+            'renting_minimal_time': {
+                'duration': request.env.company.renting_minimal_time_duration,
+                'unit': request.env.company.renting_minimal_time_unit,
+            },
+        }
--- website_sale_stock_renting/controllers/main.py
+++ website_sale_stock_renting/controllers/main.py
@@ -1,24 +1,28 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields
-from odoo.http import request
+from odoo.http import request, route
+
 from odoo.addons.website_sale_renting.controllers.main import WebsiteSaleRenting
 
-
 class WebsiteSaleStockRenting(WebsiteSaleRenting):
-    def _shop_lookup_products(self, attrib_set, options, post, search, website):
-        # Override to add rental stock search on /shop
-        fuzzy_search_term, product_count, search_result = super()._shop_lookup_products(attrib_set, options, post, search, website)
-        if options.get('from_date') and options.get('to_date'):
-            try:
-                search_result = search_result.sudo()._filter_on_available_rental_products(
-                    fields.Datetime.to_datetime(options.get('from_date')),
-                    fields.Datetime.to_datetime(options.get('to_date')),
-                    request.website._get_warehouse_available(),
-                )
-                product_count = len(search_result)
-            except ValueError:
-                # Invalid date format
-                pass
-        return fuzzy_search_term, product_count, search_result
+
+    @route(
+        '/rental/product/availabilities', type='json', auth='public', methods=['POST'], website=True
+    )
+    def renting_product_availabilities(self, product_id, min_date, max_date):
+        """ Return rental product availabilities.
+
+        Availabilities are the available quantities of a product for a given period. This is
+        expressed by an ordered list of dict {'start': ..., 'end': ..., 'available_quantity': ...).
+
+        :rtype: list(dict)
+        """
+        product_sudo = request.env['product.product'].sudo().browse(product_id).exists()
+        result = {'preparation_time': product_sudo.preparation_time}
+        if not product_sudo.allow_out_of_stock_order:
+            result['renting_availabilities'] = product_sudo._get_availabilities(
+                fields.Datetime.to_datetime(min_date), fields.Datetime.to_datetime(max_date),
+                request.website._get_warehouse_available(), with_cart=True
+            )
+        return result
