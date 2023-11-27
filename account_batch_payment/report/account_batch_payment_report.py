ebc87856559dcd7034e00a633bccb39379d8b2ad
Anh Thao Pham (pta) <pta@odoo.com>
2023-11-27 14:56:51 +0000

    [FIX] account_batch_payment: removes 20 payments per page limit in print
    Steps to reproduce:
    - Install Accounting
    - Activate "Batch Payments" in Accounting settings
    - Go to "Accounting / Customers / Payments"
    - Create more than 20 payments
    - Select all the payments and create a batch payment
    - Confirm the batch payment and print it
    => Each page of the generated PDF only contains 20 payments,
    letting a lot of white space at the bottom of the page between
    each page.
    If the customer names are very long and are displayed on several lines,
    it can happen that the 1st page contains the 19 first payments (displayed
    on the whole page) and the 2nd page only contains the 2Oth payment.
    Then the next 20 payments are displayed on the 3rd page, leaving the 2nd
    page almost empty.
    
    opw-3597753
    
    closes odoo/enterprise#53364
    
    X-original-commit: 98e34c14e70d2a82933a7c00e84642bf6e8d3cd5
    Signed-off-by: Brice Bartoletti (bib) <bib@odoo.com>
    Signed-off-by: Anh Thao Pham (pta) <pta@odoo.com>

--- account_batch_payment/report/account_batch_payment_report.py
+++ account_batch_payment/report/account_batch_payment_report.py
@@ -3,34 +3,11 @@
 
 from odoo import api, models
 
-PAY_LINES_PER_PAGE = 20
-
 
 class PrintBatchPayment(models.AbstractModel):
     _name = 'report.account_batch_payment.print_batch_payment'
     _template = 'account_batch_payment.print_batch_payment'
     _description = 'Batch Deposit Report'
-
-    def get_pages(self, batch):
-        """ Returns the data structure used by the template
-        """
-        i = 0
-        payment_slices = []
-        while i < len(batch.payment_ids):
-            payment_slices.append(batch.payment_ids[i:i+PAY_LINES_PER_PAGE])
-            i += PAY_LINES_PER_PAGE
-
-        return [{
-            'date': batch.date,
-            'batch_name': batch.name,
-            'journal_name': batch.journal_id.name,
-            'company_name': batch.journal_id.company_id.name,
-            'bank_account_number': batch.journal_id.bank_acc_number,
-            'payments': payments,
-            'currency': batch.currency_id,
-            'total_amount': batch.amount if idx == len(payment_slices) - 1 else 0,
-            'footer': batch.journal_id.company_id.report_footer,
-        } for idx, payments in enumerate(payment_slices)]
 
     @api.model
     def _get_report_values(self, docids, data=None):
@@ -40,5 +17,4 @@ class PrintBatchPayment(models.AbstractModel):
             'doc_ids': docids,
             'doc_model': report.model,
             'docs': self.env[report.model].browse(docids),
-            'pages': self.get_pages,
         }
