be2900a1ced79c5aa695ad64a5b76a32caee2705
Bastien (bvdn) <bvdn@odoo.com>
2023-11-30 11:34:10 +0100

    [IMP] project_enterprise: mute PinItem text of task when unplanned
    Task-3617973
    
    closes odoo/enterprise#51830
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- project_enterprise/static/src/views/project_task_map/project_task_map_view.js
+++ project_enterprise/static/src/views/project_task_map/project_task_map_view.js
@@ -3,10 +3,12 @@
 import { registry } from "@web/core/registry";
 import { mapView } from "@web_map/map_view/map_view";
 import { ProjectTaskMapModel } from "./project_task_map_model";
+import { ProjectTaskMapRenderer } from "./project_task_map_renderer";
 import { ProjectControlPanel } from "@project/components/project_control_panel/project_control_panel";
 
 registry.category("views").add("project_task_map", {
     ...mapView,
     Model: ProjectTaskMapModel,
-    ControlPanel: ProjectControlPanel
+    ControlPanel: ProjectControlPanel,
+    Renderer: ProjectTaskMapRenderer,
 });
