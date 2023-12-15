1f72fc21fc7d3be026881f2736a78531c7751d4a
oco-odoo <oco@odoo.com>
2023-12-15 13:46:02 +0100

    [IMP] account_reports: better multi-company tax closing
    When performing the tax closing in a multi-company environment (either because of a tax unit or branches), the tax report pdf (and possibly XML export, for Belgium) was added as an attachment of each closing entry generated. This was misleading and error-prone: the attachments only need to be on the the main company's closing.
    
    Moreover, the posting of those entries was fully decoupled: so there was no guarantee all of them will be posted at the same time, leading to the possibility that the tax lock dates are not all set at the same time, and some entries impacting the tax reports are added between the posting of each closing. In that case, right the pdf/export file was the one on the last closing posted, all the other ones were outdated. Again, this could cause confusion, and potentially cause big mistakes.
    
    To handle both those cases, we now enforce the order in which closings need to be posted: the main company needs to be the last one. Posting the main company's closing move hence posts the closing of every sub-companies directly. Then, attachments are only generated on the main closing.
    
    Part-of: odoo/enterprise#52706

--- account_reports_tax_reminder/models/account_move.py
+++ account_reports_tax_reminder/models/account_move.py
@@ -5,8 +5,8 @@ from odoo import fields, models
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    def _close_tax_period(self):
-        ret = super()._close_tax_period()
+    def _close_tax_period(self, report, options):
+        ret = super()._close_tax_period(report, options)
 
         # add a next activity on the moves, as the attachments should be sent to the administration
         MailActivity = self.env['mail.activity'].with_context(mail_activity_quick_update=True)
@@ -15,7 +15,8 @@ class AccountMove(models.Model):
         if act_user and not (self.company_id in act_user.company_ids and self.env.ref('account.group_account_manager') in act_user.groups_id):
             act_user = self.env['res.users']
 
-        for move in self.filtered(lambda x: not x.posted_before):
+        tax_sender_company = report._get_sender_company_for_export(options)
+        for move in self.filtered(lambda x: not x.posted_before and x.company_id == tax_sender_company):
             MailActivity.create({
                 'res_id': move.id,
                 'res_model_id': self.env.ref('account.model_account_move').id,
