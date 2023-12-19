494373219f5ec56b21b037c6b9afa3eae516e2ea
Thomas Lefebvre (thle) <thle@odoo.com>
2023-12-19 09:32:29 +0000

    [FIX] website_helpdesk: force internal subtype to 'comment'
    Complete the fix of commit:
    https://github.com/odoo/enterprise/commit/aacf90852cfcba71ba4b62ceaefecf7da723ebf3
    
    We need to set the subtype of the message to 'comment' (`mt_comment`)
    to respect the domain which filters the messages to be displayed
    for a portal user.
    
    opw-3601205
    
    closes odoo/enterprise#53365
    
    X-original-commit: dc10998ac26cd2946612ae33b2f2d031cf68bc9b
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- website_helpdesk/controllers/main.py
+++ website_helpdesk/controllers/main.py
@@ -154,7 +154,9 @@ class WebsiteForm(form.WebsiteForm):
             ticket = model.env[model_name].browse(id_record)
             attachments = request.env['ir.attachment'].sudo().search([('res_model', '=', model_name), ('res_id', '=', ticket.id), ('access_token', '=', False)])
             attachments.generate_access_token()
-            ticket.message_ids.filtered(lambda m: m.attachment_ids == attachments).is_internal = False
+            message = ticket.message_ids.filtered(lambda m: m.attachment_ids == attachments)
+            message.is_internal = False
+            message.subtype_id = request.env.ref('mail.mt_comment')
 
     def insert_record(self, request, model, values, custom, meta=None):
         res = super().insert_record(request, model, values, custom, meta=meta)
