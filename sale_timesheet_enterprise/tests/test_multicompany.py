b535328505ee629c6b82e9e986dcc224245e85d9
kpt-odoo <kpt@odoo.com>
2023-12-19 12:20:06 +0530

    [REM] sale_timesheet_enterprise: remove "project.create.sale.order" wizard
    In this commit we have removed unused "project.create.sale.order" wizard
    this wizard was deprecated in this task 2637495, but it's still in the code
    and used in some tests.
    
    task-3632161
    
    closes odoo/enterprise#53390
    
    Related: odoo/odoo#147533
    Related: odoo/upgrade#5470
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- sale_timesheet_enterprise/tests/test_multicompany.py
+++ sale_timesheet_enterprise/tests/test_multicompany.py
@@ -35,20 +35,3 @@ class TestSaleTimesheetEnterpriseMultiCompany(TestCommonSaleTimesheet):
             'project_id': cls.project_billable_tasks.id,
             'task_id': cls.task.id,
         })
-
-    def test_billable_project(self):
-        wizard = self.env['project.create.sale.order'].with_context(
-            allowed_company_ids=[self.env.company.id, self.company_data_2['company'].id],
-            company_id=self.company_data_2['company'].id,
-            active_id=self.project_billable_tasks.id,
-            active_model='project.project'
-        ).create({
-            'line_ids': [
-                Command.create({'product_id': self.service_timesheet_product.id, 'price_unit': self.service_timesheet_product.lst_price}),
-            ],
-        })
-
-        action = wizard.action_create_sale_order()
-        sale_order = self.env['sale.order'].browse(action['res_id'])
-
-        self.assertEqual(sale_order.company_id.id, self.task.company_id.id, "The company of the sale order should be the same as the one from the task")
