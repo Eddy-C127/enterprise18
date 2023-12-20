7e2e59db621e39644abd6aa584fc9b1122bf4006
tsm-odoo <tsm@odoo.com>
2023-12-20 13:44:11 +0100

    [FIX] whatsapp: fix discuss sidebar sort function
    fixes runbot-47784
    
    closes odoo/enterprise#53213
    
    X-original-commit: 4fb337a5fc99219e209bf0e3b33dae35311e951e
    Related: odoo/odoo#147146
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- whatsapp/static/src/core/common/thread_service_patch.js
+++ whatsapp/static/src/core/common/thread_service_patch.js
@@ -1,6 +1,7 @@
 /** @odoo-module */
 
 import { ThreadService } from "@mail/core/common/thread_service";
+import { compareDatetime } from "@mail/utils/common/misc";
 import { patch } from "@web/core/utils/patch";
 
 patch(ThreadService.prototype, {
@@ -37,7 +38,8 @@ patch(ThreadService.prototype, {
         super.sortChannels();
         // WhatsApp Channels are sorted by most recent interest date time in the sidebar.
         this.store.discuss.whatsapp.threads.sort(
-            (t1, t2) => t2.lastInterestDateTime.ts - t1.lastInterestDateTime.ts
+            (t1, t2) =>
+                compareDatetime(t2.lastInterestDateTime, t1.lastInterestDateTime) || t2.id - t1.id
         );
     },
 });
