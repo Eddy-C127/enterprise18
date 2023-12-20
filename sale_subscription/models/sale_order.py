3a54d427a81df6608f86eb16564408f7afa14c42
pash-odoo <pash@odoo.com>
2023-12-20 13:02:35 +0530

    [IMP] sale_subscription: add button in the email to pay failed payments
    When a subscription is not paid automatically,
    We sent an email to customers to inform them that we would be retrying.
    
    In this commit, a button has been added to the email that enables users to
    pay for failed subscription payments directly.
    
    task-3640650
    
    closes odoo/enterprise#53046
    
    Signed-off-by: Arnaud Joset (arj) <arj@odoo.com>

--- sale_subscription/models/sale_order.py
+++ sale_subscription/models/sale_order.py
@@ -8,6 +8,8 @@ from ast import literal_eval
 from collections import defaultdict
 import traceback
 
+from werkzeug import urls
+
 from odoo import fields, models, _, api, Command, SUPERUSER_ID, modules
 from odoo.exceptions import UserError, ValidationError
 from odoo.tools.float_utils import float_is_zero
@@ -15,6 +17,8 @@ from odoo.osv import expression
 from odoo.tools import config, format_amount, plaintext2html, split_every, str2bool
 from odoo.tools.date_utils import get_timedelta
 from odoo.tools.misc import format_date
+
+from odoo.addons.payment import utils as payment_utils
 
 _logger = logging.getLogger(__name__)
 
@@ -1343,7 +1347,17 @@ class SaleOrder(models.Model):
             else:
                 msg_body = _('Automatic payment failed. No email sent this time. Error: %s', transaction and transaction.state_message or _('No valid Payment Method'))
                 if (fields.Date.today() - order.next_invoice_date).days in [2, 7, 14]:
-                    email_context.update({'date_close': date_close, 'payment_token': order.payment_token_id.display_name})
+                    base_url = self.get_base_url()
+                    url_params = {
+                        'amount': self.amount_total,
+                        'access_token': payment_utils.generate_access_token(self.partner_id.id, self.amount_total, self.currency_id.id),
+                        'currency_id': self.currency_id.id,
+                        'partner_id': self.partner_id.id,
+                        'company_id': self.company_id.id,
+                        'sale_order_id': self.id,
+                    }
+                    payment_link = f'{base_url}/payment/pay?{urls.url_encode(url_params)}'
+                    email_context.update({'date_close': date_close, 'payment_token': order.payment_token_id.display_name, 'payment_link': payment_link})
                     reminder_mail_template.with_context(email_context).send_mail(order.id)
                     _logger.debug("Sending Payment Failure Mail to %s for contract %s and setting contract to pending", order.partner_id.email, order.id)
                     msg_body = _('Automatic payment failed. Email sent to customer. Error: %s', transaction and transaction.state_message or _('No Payment Method'))
