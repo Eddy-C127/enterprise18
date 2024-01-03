5ea0655a21b1e5a79b86163dd4143f9b6d7955de
Rémy Voet (ryv) <ryv@odoo.com>
2024-01-03 13:19:00 +0100

    [REF] l10n_be_reports_post_wizard: alternative solution of `__ignore_missing_keys__`
    Alternative solution of ef656039f6d31bd80c6c292051dc2fe7e5e8c66a,
    because of the revert in the community version.
    
    closes odoo/enterprise#53594
    
    Related: odoo/odoo#147932
    Signed-off-by: Xavier Dollé (xdo) <xdo@odoo.com>

--- l10n_be_reports_post_wizard/tests/test_l10n_be_reports_post_wizard.py
+++ l10n_be_reports_post_wizard/tests/test_l10n_be_reports_post_wizard.py
@@ -51,18 +51,18 @@ class TestL10nBeReportsPostWizard(TestAccountReportsCommon):
         action = self.tax_return_move.action_post()
 
         self.assertRecordValues(self.tax_return_move, [{'state': 'draft'}])
-        self.assertEqual(action, {
-            '__ignore_missing_keys__': True,
+        self.assertGreaterEqual(action.items(), {
+            'name': 'Post a tax report entry',
+            'view_mode': 'form',
             'views': [[self.env.ref('l10n_be_reports_post_wizard.view_account_financial_report_export').id, 'form']],
             'res_model': 'l10n_be_reports.periodic.vat.xml.export',
             'type': 'ir.actions.act_window',
             'target': 'new',
-            'context': {
-                '__ignore_missing_keys__': True,
-                'l10n_be_reports_generation_options': {},
-                'l10n_be_action_resume_post_move_ids': self.tax_return_move.ids,
-            },
-        })
+        }.items())
+        self.assertGreaterEqual(action['context'].items(), {
+            'l10n_be_reports_generation_options': {},
+            'l10n_be_action_resume_post_move_ids': self.tax_return_move.ids,
+        }.items())
 
     def test_validating_wizard_posts_move(self):
         ''' Test that validating the wizard posts the move
