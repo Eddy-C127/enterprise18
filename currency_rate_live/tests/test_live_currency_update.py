9939e78d79ff7a4d672dd3185abd511bb65d8583
Antoine Boonen <aboo@odoo.com>
2024-01-08 14:55:25 +0000

    [IMP] currency_rate_live: Use Bulgaria National Bank for exchange rates
    Objective
    ---------
    Add the Bulgarian National Bank (BNB) as a Currency Provider in Odoo.
    
    Solution
    ---------
    Rates are given against BGN in an XML file of format:
    
    <ROWSET>
      <ROW>
        <GOLD>1</GOLD>
        <NAME_>Currency</NAME_>
        <CODE>Code</CODE>
        <RATIO>Per unit of currency/gold</RATIO>
        <REVERSERATE>Reverse rate for 1 BGN</REVERSERATE>
        <RATE>Levs (BGN)</RATE>
        <CURR_DATE>Date</CURR_DATE>
        <F_STAR>0</F_STAR>
     </ROW>
    </ROWSET>
    
    Source: https://www.bnb.bg/AboutUs/AUFAQ/Contr_Exchange_Rates_FAQ?toLang=_EN
    
    Currencies returned on the 8th of January 2024:
        AUD, BRL, CAD, CHF, CNY, CZK, DKK, GBP, HKD, HUF, IDR, ILS, INR,
        ISK, JPY, KRW, MXN, MYR, NOK, NZD, PHP, PLN, RON, RUB*, SEK, SGD,
        THB, TRY, USD, ZAR, XAU
    * Due to the situation on the international financial markets, the
    Bulgarian National Bank (BNB) is not in a position to set a reference
    exchange rate for the Bulgarian Lev against the Russian Rouble that is
    representative of market conditions.
    
    If a currency has no rate, it is skipped.
    
    task-3672394
    
    closes odoo/enterprise#54879
    
    X-original-commit: 325c63b98b650cd5aecc73a4934d1849c571987b
    Signed-off-by: Habib Ayob (ayh) <ayh@odoo.com>
    Signed-off-by: Antoine Boonen (aboo) <aboo@odoo.com>

--- currency_rate_live/tests/test_live_currency_update.py
+++ currency_rate_live/tests/test_live_currency_update.py
@@ -151,3 +151,10 @@ class CurrencyTestCase(TransactionCase):
         self.assertEqual(len(pln.rate_ids), pln_rates_count + 1)
         self.assertEqual(pln.rate_ids[-1].rate, 1.0)
         self.assertEqual(len(usd.rate_ids), usd_rates_count + 1)
+
+    def test_live_currency_update_bnb(self):
+        self.test_company.currency_provider = 'bnb'
+        usd_rates_count = len(self.currency_usd.rate_ids)
+        res = self.test_company.update_currency_rates()
+        self.assertTrue(res)
+        self.assertEqual(len(self.currency_usd.rate_ids), usd_rates_count + 1)
