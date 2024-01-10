ab28060e26c56da3632764810687dffe2ade6caa
mhch-odoo <mhch@odoo.com>
2024-01-10 17:36:04 +0800

    [IMP] l10n_au_aba, l10n_au_hr_payroll_account: Replace UserErrors with RedirectWarnings
    Issue:
    Aba is meant for batch payments and raising `UserErrors` individually for
    each employee/vendor prevents User from knowing all the faulty accounts.
    At the same time, difficult to find bank accounts for partners with multiple
    accounts.
    
    Fix:
    Raise a `RedirectWarning` with tree view to show all faulty partners/vendors
    simultaneously. And makes it simpler to directly find the account without
    the need to discard/recreate the faulty payments.
    
    3635231
    
    closes odoo/enterprise#56118
    
    X-original-commit: 9dde135ac0a62c5fa943ccb668b60f23caec0610
    Signed-off-by: Nicolas Viseur (vin) <vin@odoo.com>
    Signed-off-by: Muhammad Haris Choudhary (mhch) <mhch@odoo.com>

--- l10n_au_aba/models/account_payment.py
+++ l10n_au_aba/models/account_payment.py
@@ -23,46 +23,34 @@ class AccountPayment(models.Model):
     @api.constrains('payment_method_line_id', 'journal_id', 'currency_id')
     def _l10n_au_aba_check_bank_account(self):
         aba_payment_method = self.env.ref('l10n_au_aba.account_payment_method_aba_ct')
-        def _redirect_journal(journal, message):
-            raise RedirectWarning(
-                        message=message,
-                        action={
-                            "view_mode": "form",
-                            "res_model": "account.journal",
-                            "type": "ir.actions.act_window",
-                            "views": [[False, "form"]],
-                            "res_id": journal.id,
-                            "target": "new",
-                        },
-                        button_text="Configure Journal"
-                    )
         for rec in self:
             if rec.payment_method_id == aba_payment_method:
                 bank_acc = rec.journal_id.bank_account_id
-
                 if rec.currency_id.name != 'AUD':
                     raise ValidationError(_('ABA payments must be defined in AUD.'))
                 if bank_acc.acc_type != 'aba' or not bank_acc.aba_bsb:
-                    _redirect_journal(rec.journal_id, _("Journal '%s' requires a proper ABA account. Please configure the Account first.", rec.journal_id.name))
+                    raise RedirectWarning(
+                        message=_("Journal '%s' requires a proper ABA account. Please configure the Account first.", rec.journal_id.name),
+                        action=rec.journal_id._get_records_action(name=_("Configure Journal"), target="new"),
+                        button_text=_("Configure Journal")
+                    )
                 if not rec.journal_id.aba_user_spec or not rec.journal_id.aba_fic or not rec.journal_id.aba_user_number:
-                    _redirect_journal(rec.journal_id, _("Please fill in the ABA data of account %s (journal %s) before using it to generate ABA payments.", bank_acc.acc_number, rec.journal_id.name))
+                    raise RedirectWarning(
+                        message=_("ABA fields for account '%s' on journal '%s' are not set. Please set the fields under ABA section!", bank_acc.acc_number, rec.journal_id.name),
+                        action=rec.journal_id._get_records_action(name=_("Configure Journal"), target="new"),
+                        button_text=_("Configure Journal")
+                    )
 
     @api.constrains('payment_method_line_id', 'partner_bank_id')
     def _check_partner_bank_account(self):
         aba_payment_method = self.env.ref('l10n_au_aba.account_payment_method_aba_ct')
-        for rec in self:
-            if rec.payment_method_id == aba_payment_method:
-                if rec.partner_bank_id.acc_type != 'aba' or not rec.partner_bank_id.aba_bsb:
-                    raise RedirectWarning(
-                        message=_("The partner requires a bank account with a valid BSB and account number.\
-                            Please configure the following account first:\n %s.", rec.partner_bank_id.display_name),
-                        action={
-                            "view_mode": "form",
-                            "res_model": "res.partner.bank",
-                            "type": "ir.actions.act_window",
-                            "views": [[False, "form"]],
-                            "res_id": rec.partner_bank_id.id,
-                            "target": "new",
-                        },
-                        button_text="Configure Account"
-                    )
+
+        faulty_partners = self.filtered(lambda rec: rec.payment_method_id == aba_payment_method and (rec.partner_bank_id.acc_type != 'aba' or not rec.partner_bank_id.aba_bsb)).partner_id
+        if faulty_partners:
+            # Redirects to the contacts as some contacts may not have accounts set
+            raise RedirectWarning(
+                message=_("The Contact(s) requires a bank account with a valid BSB and account number."
+                          "Please configure the account(s) for the following Contact(s):\n%s", "\n".join(faulty_partners.mapped("display_name"))),
+                action=faulty_partners._get_records_action(name=_("Configure Contact Account(s)"), target='new', views=[(False, "form"), (self.env.ref("l10n_au_aba.view_partner_tree").id, "tree")]),
+                button_text=_("Configure Contact Account(s)")
+            )
