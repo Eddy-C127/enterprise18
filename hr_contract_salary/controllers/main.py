13b08518379efaa42bd44d66cfb044fb31c14d00
Yannick Tivisse <yti@odoo.com>
2024-01-10 14:26:00 +0100

    [FIX] hr_contract_salary: Fix ip signature value
    closes odoo/enterprise#54084
    
    X-original-commit: 35bf138195d9adea2fd029ab416421c4e9ec20b2
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- hr_contract_salary/controllers/main.py
+++ hr_contract_salary/controllers/main.py
@@ -936,6 +936,8 @@ class HrContractSalary(http.Controller):
                 # YTI FIXME: Clean that brol
                 elif item.name == 'l10n_be_group_insurance_rate':
                     new_value = 1 if item.name in new_contract and new_contract[item.name] else 0
+                elif item.name == "ip_wage_rate":
+                    new_value = new_values if new_contract.ip else 0
                 else:
                     new_values = new_contract.mapped(item.name)
                     if not new_values or isinstance(new_values, models.BaseModel):
