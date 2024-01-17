8305baf87713ddba06580a248770206d2745401c
vlst-odoo <vlst@odoo.com>
2024-01-17 11:45:34 +0100

    [IMP] pos_iot: remove no longer needed code
    In the corresponding community commit we refactor the customer display into it's own module. In this commit we remove the code that is no longer needed, because it is replaced by said customer display module
    
    closes odoo/enterprise#54501
    
    Task: 3684195
    Related: odoo/odoo#148870
    Related: odoo/upgrade#5585
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- pos_iot/models/pos_config.py
+++ pos_iot/models/pos_config.py
@@ -9,7 +9,6 @@ class PosConfig(models.Model):
 
     iface_print_via_proxy = fields.Boolean(compute="_compute_print_via_proxy")
     iface_printer_id = fields.Many2one('iot.device', domain="['&', ('type', '=', 'printer'), ('subtype', '=', 'receipt_printer'), '|', ('company_id', '=', False), ('company_id', '=', company_id)]")
-    iface_customer_facing_display_via_proxy = fields.Boolean(compute="_compute_customer_facing_display_via_proxy")
     iface_display_id = fields.Many2one('iot.device', domain="[('type', '=', 'display'), '|', ('company_id', '=', False), ('company_id', '=', company_id)]")
     iface_scan_via_proxy = fields.Boolean(compute="_compute_scan_via_proxy")
     iface_scanner_ids = fields.Many2many('iot.device', domain="[('type', '=', 'scanner'), '|', ('company_id', '=', False), ('company_id', '=', company_id)]",
@@ -24,11 +23,6 @@ class PosConfig(models.Model):
     def _compute_print_via_proxy(self):
         for config in self:
             config.iface_print_via_proxy = config.iface_printer_id.id is not False
-
-    @api.depends('iface_display_id')
-    def _compute_customer_facing_display_via_proxy(self):
-        for config in self:
-            config.iface_customer_facing_display_via_proxy = config.iface_display_id.id is not False
 
     @api.depends('iface_scanner_ids')
     def _compute_scan_via_proxy(self):
