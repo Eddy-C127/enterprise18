cc3e6fe86f4fd6e7810cff9bb3721438124e7fc9
Panagiotis Kyriakou <paky@odoo.com>
2024-01-22 16:50:03 +0100

    [IMP] industry_fsm: add tests for the user_id group expand
    The user_id group expand implementation was changed recently
    in order for the field service and the project apps to share
    implementations.
    
    There is a test for the project's functionality but no tests
    for the field service functionality and thus a new test was
    added for it.
    
    task-3696707
    
    closes odoo/enterprise#54851
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- industry_fsm/tests/__init__.py
+++ industry_fsm/tests/__init__.py
@@ -9,3 +9,4 @@ from . import test_project_milestone
 from . import test_auto_create_fsm_project
 from . import test_fsm_task_analysis
 from . import test_fsm_project_task
+from . import test_task_group_expand
