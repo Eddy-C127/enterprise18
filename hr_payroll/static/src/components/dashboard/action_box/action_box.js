10d9377c4d9aa6fa3ea91032880f6bd9e18a9df6
jugj-odoo <jugj@odoo.com>
2024-01-26 11:08:57 +0100

    [FIX] hr_payroll: Fix performance issue with dashboard warning loading time
    task-3597092
    
    closes odoo/enterprise#55369
    
    X-original-commit: 28de6a85ce267469c2dcfbe7c6a969845c24a986
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>
    Signed-off-by: Jurgen Gjini (jugj) <jugj@odoo.com>

--- hr_payroll/static/src/components/dashboard/action_box/action_box.js
+++ hr_payroll/static/src/components/dashboard/action_box/action_box.js
@@ -1,7 +1,9 @@
 /** @odoo-module **/
 
 import { useService } from "@web/core/utils/hooks";
-import { Component } from "@odoo/owl";
+import { Component, useState, onWillStart} from "@odoo/owl";
+import { rpc } from "@web/core/network/rpc";
+
 
 export class PayrollDashboardActionBox extends Component {
     static template = "hr_payroll.ActionBox";
@@ -9,5 +11,17 @@ export class PayrollDashboardActionBox extends Component {
 
     setup() {
         this.actionService = useService("action");
+        this.state = useState({
+            loading: true,
+            warnings: {},
+        })
+        onWillStart(() => {
+          rpc('get_payroll_warnings').then(data => {
+              this.state.warnings = data;
+              this.state.loading = false;
+            }
+          )
+          return Promise.resolve(true);
+        })
     }
 }
