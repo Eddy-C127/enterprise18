fb91c45f5e4fc108982ae504c983144da0c6876a
Xavier Bol (xbo) <xbo@odoo.com>
2024-01-29 08:55:21 +0100

    [FIX] web_grid: avoid altering {row,section,column} id in 2 rpc calls
    Before this commit, when the load is slow and the user does not wait the
    load method be executed and launches another search, the section, row and
    column ids could be altered by the both load are currently executed
    (the current one and the previous one whom is not yet finished). The problem
    with that is the section, row and column id is a global variable in the file
    and so the id is incremented by the both loads once a section, row or column
    is created and so the user could see a big gap between the grid header and his
    first result displayed because the row id could be for instance 21 instead of 1.
    
    This commit fixes the issue by removing the global variable and define the
    attributes to increment for section, row and column id into `GridDataPoint`
    to be sure the id will only be incremented by only one load.
    
    closes odoo/enterprise#55361
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- web_grid/static/src/views/grid_model.js
+++ web_grid/static/src/views/grid_model.js
@@ -10,10 +10,6 @@ import { Model } from "@web/model/model";
 
 const { DateTime, Interval } = luxon;
 
-let sectionId = 0;
-let rowId = 0;
-let columnId = 0;
-
 export class GridCell {
     /**
      * Constructor
@@ -110,7 +106,7 @@ export class GridRow {
         this._dataPoint = dataPoint;
         this.cells = {};
         this.valuePerFieldName = valuePerFieldName;
-        this.id = rowId++;
+        this.id = dataPoint.rowId++;
         this.model = dataPoint.model;
         this.section = section;
         if (section) {
@@ -221,7 +217,7 @@ export class GridRow {
 export class GridSection extends GridRow {
     constructor() {
         super(...arguments);
-        this.sectionId = sectionId++;
+        this.sectionId = this._dataPoint.sectionId++;
         this.rows = {};
         // @deprecated remove me in master
         this.grandTotalPerColumn = {};
@@ -325,7 +321,7 @@ export class GridColumn {
         this.title = title;
         this.value = value;
         this.cells = [];
-        this.id = columnId++;
+        this.id = dataPoint.columnId++;
         this.grandTotal = 0;
         this.readonly = readonly;
     }
@@ -383,6 +379,9 @@ export class GridDataPoint {
         this.rowFields = rowFields;
         this.sectionField = sectionField;
         this.searchParams = searchParams;
+        this.sectionId = 0;
+        this.rowId = 0;
+        this.columnId = 0;
     }
 
     get orm() {
@@ -1109,7 +1108,6 @@ export class GridModel extends Model {
      * @return {Promise<void>}
      */
     async load(params = {}) {
-        sectionId = rowId = columnId = 0;
         const searchParams = {
             ...this.searchParams,
             ...params,
