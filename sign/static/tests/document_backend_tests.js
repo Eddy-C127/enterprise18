a9a37341fadae346f5a0522420eb165dbd23f2fd
Leonardo Pavan Rocha <lpr@odoo.com>
2024-01-30 18:32:33 +0000

    [FIX] sign: public document showing send button
    After the sign conversion to OWL, when accessing an unsigned shared document it
    would show a "send" button close to the public user. However, since it's a
    shared document, there is no email to send the document to. Clicking on it,
    will cause a traceback. This commit hides this button for shared requests.
    
    task-3710635
    
    closes odoo/enterprise#57021
    
    X-original-commit: 01f7b5cdd8706d7ac24961aa1da3089e8491e52c
    Signed-off-by: Arnaud Joset (arj) <arj@odoo.com>
    Signed-off-by: Leonardo Pavan Rocha <lpr@odoo.com>

--- sign/static/tests/document_backend_tests.js
+++ sign/static/tests/document_backend_tests.js
@@ -1,10 +1,11 @@
 /** @odoo-module **/
 
-import { getFixture, nextTick } from "@web/../tests/helpers/utils";
+import { getFixture, nextTick, patchWithCleanup } from "@web/../tests/helpers/utils";
 import { doAction } from "@web/../tests/webclient/helpers";
 import { createDocumentWebClient, actionId } from "./action_utils";
 import { signInfoService } from "@sign/services/sign_info_service";
 import { registry } from "@web/core/registry";
+import { session } from "@web/session";
 
 let serverData;
 let config;
@@ -52,6 +53,7 @@ QUnit.module("document_backend_tests", ({ beforeEach }) => {
 
     QUnit.test("simple rendering", async function (assert) {
         assert.expect(7);
+        patchWithCleanup(session, { uid: 1 });
 
         const getDataFromHTML = () => {
             assert.step("getDataFromHTML");
@@ -86,6 +88,30 @@ QUnit.module("document_backend_tests", ({ beforeEach }) => {
             target.querySelectorAll(".o_sign_resend_access_button")[1].textContent,
             "Send"
         );
+        assert.containsOnce(target, ".d-xl-inline-flex .o_sign_sign_directly");
+    });
+
+    QUnit.test("render shared document", async function (assert) {
+        assert.expect(3);
+
+        config = {
+            ...config,
+            actionContext: {
+                need_to_sign: true,
+                state: "shared",
+            },
+        };
+
+        const webClient = await createDocumentWebClient(config, serverData);
+        await doAction(webClient, actionId);
+
+        assert.strictEqual(
+            target.querySelector(".o_sign_document").innerText.trim(),
+            "def",
+            "should display text from server"
+        );
+
+        assert.containsN(target, ".o_sign_resend_access_button", 0);
         assert.containsOnce(target, ".d-xl-inline-flex .o_sign_sign_directly");
     });
 
