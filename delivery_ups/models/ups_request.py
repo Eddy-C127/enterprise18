cd014746179f4511a45051726787f697ed30f2c1
Ahmad (khah) <khah@odoo.com>
2024-02-13 10:09:46 +0100

    [FIX] delivery_ups: fix capitalization of email address
    For email address to be considered in the UPS request, the
    `M` needs to be capitalized. UPS documentation for reference:
    https://developer.ups.com/api/reference?loc=en_US#operation/Shipment!path=ShipmentRequest/Shipment/ShipTo/EMailAddress&t=request
    
    opw-3487959
    
    closes odoo/enterprise#56769
    
    X-original-commit: 6d1ef3cff61b38c822058b2f68ec3dadc2c395d4
    Signed-off-by: Tiffany Chang (tic) <tic@odoo.com>

--- delivery_ups/models/ups_request.py
+++ delivery_ups/models/ups_request.py
@@ -483,7 +483,7 @@ class UPSRequest():
         shipment.Shipper.ShipperNumber = self.shipper_number or ''
         shipment.Shipper.Phone = self.factory_ns2.ShipPhoneType()
         shipment.Shipper.Phone.Number = self._clean_phone_number(shipper.phone)
-        shipment.Shipper.EmailAddress = shipper.email or ''
+        shipment.Shipper.EMailAddress = shipper.email or ''
 
         shipment.ShipFrom = self.factory_ns2.ShipFromType()
         shipment.ShipFrom.Address = self.factory_ns2.ShipAddressType()
@@ -497,7 +497,7 @@ class UPSRequest():
             shipment.ShipFrom.Address.StateProvinceCode = ship_from.state_id.code or ''
         shipment.ShipFrom.Phone = self.factory_ns2.ShipPhoneType()
         shipment.ShipFrom.Phone.Number = self._clean_phone_number(ship_from.phone)
-        shipment.ShipFrom.EmailAddress = ship_from.email or ''
+        shipment.ShipFrom.EMailAddress = ship_from.email or ''
 
         shipment.ShipTo = self.factory_ns2.ShipToType()
         shipment.ShipTo.Address = self.factory_ns2.ShipToAddressType()
@@ -511,7 +511,7 @@ class UPSRequest():
             shipment.ShipTo.Address.StateProvinceCode = ship_to.state_id.code or ''
         shipment.ShipTo.Phone = self.factory_ns2.ShipPhoneType()
         shipment.ShipTo.Phone.Number = self._clean_phone_number(shipment_info['phone'])
-        shipment.ShipTo.EmailAddress = ship_to.email or ''
+        shipment.ShipTo.EMailAddress = ship_to.email or ''
         if not ship_to.commercial_partner_id.is_company:
             shipment.ShipTo.Address.ResidentialAddressIndicator = None
 
