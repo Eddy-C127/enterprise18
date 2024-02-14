025370cd47dbe789f4d7a32cb69131fc0feee1a8
Luis Torres <luis_t@vauxoo.com>
2024-02-14 16:31:11 +0000

    [FIX] l10n_mx_edi: Avoid force the same payment method on payment
    It has removed the l10n_mx_edi_payment_method_id from the batch key and
    avoids group payments with different payment methods.
    
    Any SAT reason indicates that must be grouped the payments by payment
    method, so, if I receive a payment in any payment method and I register
    a payment, only a payment complement must be generated.
    
    Fixed the following case:
    
    Generate a customer invoice with payment method: 99 - To define
    Generate a second customer invoice with payment method: 01 - Efectivo
    Register a payment for the invoice 1 and 2 in cash
    In this case, only a payment complement for the total amount related to
    the 2 invoices must be generated.
    
    Note 1: If the user want generate 2 payment complements, 1 by each payment
    method, could select the records and register 2 payments.
    Note 2: The key was introduced here: https://github.com/odoo/enterprise/commit/b2dd65b9a9c054570d5796a81f2c390c4d8cb6c4#diff-8fbd6620744df804292d8f4158343ef05564035bed962f68bd5ef931d1cb9d70R29
    but without any reference that indicates why this was necessary.
    
    closes odoo/enterprise#70621
    
    X-original-commit: 33becc280cbc58f5913f974341601e5ea391e866
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- l10n_mx_edi/models/account_payment_register.py
+++ l10n_mx_edi/models/account_payment_register.py
@@ -14,18 +14,6 @@ class AccountPaymentRegister(models.TransientModel):
              "Cash, Nominal Check, Credit Card, etc.")
 
     # -------------------------------------------------------------------------
-    # HELPERS
-    # -------------------------------------------------------------------------
-
-    @api.model
-    def _get_line_batch_key(self, line):
-        # OVERRIDE
-        # Group moves also using these additional fields.
-        res = super()._get_line_batch_key(line)
-        res['l10n_mx_edi_payment_method_id'] = line.move_id.l10n_mx_edi_payment_method_id.id
-        return res
-
-    # -------------------------------------------------------------------------
     # COMPUTE METHODS
     # -------------------------------------------------------------------------
 
@@ -34,7 +22,7 @@ class AccountPaymentRegister(models.TransientModel):
         for wizard in self:
             if wizard.can_edit_wizard:
                 batches = wizard._get_batches()
-                wizard.l10n_mx_edi_payment_method_id = batches[0]['payment_values']['l10n_mx_edi_payment_method_id']
+                wizard.l10n_mx_edi_payment_method_id = wizard.line_ids.move_id.l10n_mx_edi_payment_method_id[:1]
             else:
                 wizard.l10n_mx_edi_payment_method_id = False
 
@@ -51,5 +39,5 @@ class AccountPaymentRegister(models.TransientModel):
     def _create_payment_vals_from_batch(self, batch_result):
         # OVERRIDE
         payment_vals = super()._create_payment_vals_from_batch(batch_result)
-        payment_vals['l10n_mx_edi_payment_method_id'] = batch_result['payment_values']['l10n_mx_edi_payment_method_id']
+        payment_vals['l10n_mx_edi_payment_method_id'] = self.l10n_mx_edi_payment_method_id.id
         return payment_vals
