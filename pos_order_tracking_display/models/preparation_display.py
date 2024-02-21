592bbe0dc4e48c21eebc50fdf254ef16547d5e35
David Monnom (moda) <moda@odoo.com>
2024-02-21 11:09:26 +0100

    [IMP] pos_order_tracking_display: Change stage behavior
    **Before the change:**
    The behavior before the change was that in the preparation display, the
    system considered the last stage as an indicator that the order is done.
    Orders in others stages were not differentiated and were included in the
    list displayed to customers as not ready.
    
    **After the change:**
    After the change, the system now correctly identifies the second to last
    stage as the indicator that the order is ready for display. Completed
    orders are now filtered out based on the last stage, ensuring that
    only relevant orders are shown to customers. This adjustment improves
    clarity and accuracy in the order tracking process for both customers
    and staff.
    
    closes odoo/enterprise#57106
    
    Signed-off-by: Quentin Lejeune (qle) <qle@odoo.com>

--- pos_order_tracking_display/models/preparation_display.py
+++ pos_order_tracking_display/models/preparation_display.py
@@ -6,26 +6,31 @@ class PosPreparationDisplay(models.Model):
 
     def _get_pos_orders(self):
         self.ensure_one()
+        if len(self.stage_ids) <= 1:
+            return {"done": [], "notDone": []}
+        last_stage = self.stage_ids[-1]  # last stage always means that the order is done.
+        second_last_stage = self.stage_ids[-2]  # order will be displated as ready.
+
         orders_completed = set()
         orders_not_completed = set()
         Orders = self.env["pos_preparation_display.order"]
         pdis_orders = Orders.get_preparation_display_order(self.id)
-        pdis_order_ids = Orders.browse(obj["id"] for obj in pdis_orders)
-        last_stage = self.stage_ids[-1]
+        pdis_order_ids = Orders.browse(obj["id"] for obj in pdis_orders).filtered(lambda o: o.order_stage_ids[-1].stage_id != last_stage)
+
         for pdis_order_id in pdis_order_ids:
             order_stage_id = pdis_order_id.order_stage_ids[-1].stage_id
             pos_order_tracking_ref = pdis_order_id.pos_order_id.tracking_number
             unfinished_pdis_orders = (
                 (
                     order.pos_order_id == pdis_order_id.pos_order_id
-                    and order.order_stage_ids[-1].stage_id != last_stage
+                    and order.order_stage_ids[-1].stage_id != second_last_stage
                     and pdis_order_id != order
                 )
                 for order in pdis_order_ids
             )
-            if order_stage_id == last_stage and not any(unfinished_pdis_orders):
+            if order_stage_id == second_last_stage and not any(unfinished_pdis_orders):
                 orders_completed.add(pos_order_tracking_ref)
-            else:
+            elif order_stage_id != last_stage:
                 orders_not_completed.add(pos_order_tracking_ref)
         return {
             "done": list(orders_completed),
