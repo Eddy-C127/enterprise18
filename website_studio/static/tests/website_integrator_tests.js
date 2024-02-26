Eddy-C127/enterprise18

commit e3c8f97d0b8553abc1e77bd3699fcb880e48ab0a

[IMP] web_studio, *: add ThumbnailItem component
*: website_studio

This commit aims to reduce duplication of code in studio, and creates the
ThumbnailItem component. Before this commit, the same exact layout was used
in different places of the UI to show a card with an icon, a name and an
optional 'More' menu at the lower part of the card.

Instead of reusing the same template, and to assure an identical behavior and
style to this element, we added this component instead. The style related to the
feature has been moved to be located next to the component, in its folder. This
folder is located in a 'components' folder under the studio module. Maintaining
the component should be easier.

This commit also reduces the usage of scss mixins, and uses bootstrap classes
instead for many properties that were present in the original files. Tests have
been adapted to this naming change.

task-3572251

Part-of: odoo/enterprise#49809

web_studio/static/src/client_action/components/thumbnail_item/thumbnail_item.xml
@@ -1,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<templates>
+
+<t t-name="web_studio.ThumbnailItem">
+    <div t-attf-class="o_web_studio_thumbnail_item position-relative d-flex flex-column justify-content-center align-items-center bg-light border rounded-2 {{ this.props.className || '' }}" t-on-click="this.props.onClick">
+        <div class="o_web_studio_thumbnail w-100 cursor-pointer d-flex justify-content-center align-items-center">
+            <img class="m-auto" t-att-src="this.props.icon.src" alt="this.props.icon.alt"/>
+        </div>
+        <div class="o_web_studio_name d-flex w-100 gap-1 text-capitalize fw-bold text-black" t-att-class="hasDropdown ? 'justify-content-between ps-2' : 'justify-content-center'">
+            <span class="d-block align-self-center fw-bold text-wrap text-break">
+                <t t-slot="default" />
+            </span>
+            <t t-if="hasDropdown">
+                <Dropdown togglerClass="'o_web_studio_more cursor-pointer p-0 rounded-circle border-0 bg-light'">
+                    <t t-set-slot="toggler"><i class="fa fa-ellipsis-v" /></t>
+                    <t t-slot="dropdown" />
+                </Dropdown>
+            </t>
+        </div>
+    </div>
+</t>
+
+</templates>

website_studio/static/src/website_integrator/website_integrator.xml
@@ -1,60 +1,23 @@
 <?xml version="1.0" encoding="utf-8"?>
 <templates>
 
-<t t-name="website_studio.WebsiteIntegrator.Card.Dropdown">
-  <Dropdown togglerClass="'o_web_studio_more'">
-    <t t-set-slot="toggler">
-      <i class="fa fa-ellipsis-v" />
-    </t>
-    <DropdownItem onSelected="openPageUrl" href="'#'" t-if="openPageUrl">
-      View in website
-    </DropdownItem>
-    <DropdownItem onSelected="configurePage" href="'#'">
-      Configure
-    </DropdownItem>
-    <DropdownItem onSelected="deletePage" href="'#'">
-      Delete
-    </DropdownItem>
-  </Dropdown>
-</t>
-
-<t t-name="website_studio.WebsiteIntegrator.ItemCard">
-    <t t-set="item" t-value="item or {}" />
-    <div class="o-website-studio-item-card d-flex flex-column align-items-center bg-white" t-att-class="cardClass" t-att-data-tooltip="item.tooltip">
-        <div class="o_web_studio_thumbnail" t-on-click="() => onItemClicked and onItemClicked()">
-            <img t-att-src="item.icon.src" t-att-alt="item.icon.alt"/>
-        </div>
-        <div class="o_web_studio_name d-flex w-100 pb-1 gap-1" t-att-class="afterName ? 'justify-content-between' : 'justify-content-center'">
-            <span class="d-block align-self-center ps-2 fw-bold text-wrap text-break" t-esc="item.name"/>
-            <t t-out="afterName" />
-        </div>
-    </div>
-</t>
-
 <t t-name="website_studio.WebsiteIntegrator.ListingOrPage">
-    <div class="d-flex align-items-stretch flex-row gap-3 o-web-studio-card-container pb-5">
-        <div class="o_website_studio_new_card o-website-studio-item-card d-flex flex-column align-items-center" t-att-class="!isDesigner ? 'disabled' : ''" t-on-click="() => this.onNewPage(group.type, website.id)">
-            <div class="o_web_studio_thumbnail">
-                <img t-att-src="'/web_studio/static/src/img/default_icon_app.png'" alt="App icon"/>
-            </div>
-            <div class="o_web_studio_name">
-                <span t-if="group.type === 'listing'">New Listing</span>
-                <span t-else="">New Page</span>
-            </div>
-        </div>
+    <div class="overflow-auto d-flex flex-wrap align-items-stretch flex-row gap-3 pb-5">
+        <ThumbnailItem className="'o_website_studio_new_card' + (!isDesigner ? 'disabled' : '')" onClick="() => this.onNewPage(group.type, website.id)" icon="{src: '/web_studio/static/src/img/default_icon_app.png', alt: 'App icon'}">
+            <t t-if="group.type === 'listing'">New Listing</t>
+            <t t-else="">New Page</t>
+        </ThumbnailItem>
         <t t-foreach="this.getPages(group.type, website.id)" t-as="page" t-key="page.id">
-            <t t-call="website_studio.WebsiteIntegrator.ItemCard">
-                <t t-set="page" t-value="page" />
-                <t t-set="item" t-value="{...page, name: page.page_name, icon: group.iconData, tooltip: this.computePageUrl(page) }" />
-                <t t-set="onItemClicked" t-value="page.page_type === 'listing' ? this.openPageUrl.bind(this, page) : this.onConfigurePage.bind(this, page)" />
-                <t t-set="openPageUrl" t-value="page.page_type === 'listing' ? onItemClicked : null" />
-                <t t-set="afterName">
-                    <t t-call="website_studio.WebsiteIntegrator.Card.Dropdown">
-                        <t t-set="configurePage" t-value="() => this.onConfigurePage(page)" />
-                        <t t-set="deletePage" t-value="() => this.deletePage(page)" />
-                    </t>
+            <t t-set="item" t-value="{...page, name: page.page_name, icon: group.iconData, tooltip: this.computePageUrl(page) }" />
+            <t t-set="onItemClicked" t-value="page.page_type === 'listing' ? this.openPageUrl.bind(this, page) : this.onConfigurePage.bind(this, page)" />
+            <ThumbnailItem onClick="page.page_type === 'listing' ? this.openPageUrl.bind(this, page) : this.onConfigurePage.bind(this, page)" icon="item.icon">
+                <t t-esc="item.name" />
+                <t t-set-slot="dropdown">
+                    <DropdownItem t-if="openPageUrl" onSelected="openPageUrl" href="'#'">View in website</DropdownItem>
+                    <DropdownItem onSelected="() => this.onConfigurePage(page)" href="'#'">Configure</DropdownItem>
+                    <DropdownItem onSelected="() => this.deletePage(page)" href="'#'">Delete</DropdownItem>
                 </t>
-            </t>
+            </ThumbnailItem>
         </t>
     </div>
 </t>
@@ -85,25 +48,21 @@
 </t>
 
 <t t-name="website_studio.WebsiteIntegrator">
-    <div class="o_website_studio_website_integrator d-flex flex-column gap-4 p-4">
+    <div class="o_website_studio_website_integrator w-100 h-100 overflow-auto d-flex flex-column gap-4 p-4">
         <div class="o_website_studio_form">
             <div class="mb-2">
                 <span class="fs-4 fw-bold">Forms</span>
                 <span> &#8212; Allow visitors of your website to create records themselves</span>
             </div>
-            <div class="o-web-studio-card-container d-flex flex-row gap-4 ms-4 pb-5">
-                <div class="o-website-studio-item-card o_website_studio_new_card d-flex flex-column align-items-center" t-att-class="!isDesigner ? 'disabled' : ''" t-on-click="onNewForm">
-                    <div class="o_web_studio_thumbnail">
-                        <img t-att-src="'/web_studio/static/src/img/default_icon_app.png'" alt="App icon"/>
-                    </div>
-                    <div class="o_web_studio_name"><span>New Form</span></div>
-                </div>
-                <div t-foreach="state.forms" t-as="form" class="o-website-studio-item-card d-flex flex-column align-items-center" t-key="form.id" t-on-click="() => this.openFormUrl(form.url)">
-                    <div class="o_web_studio_thumbnail">
-                        <img t-att-src="'/web_studio/static/src/img/view_type/form.png'" alt="View form"/>
-                    </div>
-                    <div class="o_web_studio_name"><span t-esc="form.name"/></div>
-                </div>
+            <div class="d-flex flex-row gap-4 ms-4 pb-5">
+                <ThumbnailItem className="'o_website_studio_new_card' + (!isDesigner ? 'disabled' : '')" onClick="onNewForm" icon="{src: '/web_studio/static/src/img/default_icon_app.png', alt: 'App icon'}">
+                    New Form
+                </ThumbnailItem>
+                <t t-foreach="state.forms" t-as="form" t-key="form.id">
+                    <ThumbnailItem onClick="() => this.openFormUrl(form.url)" icon="{src: '/web_studio/static/src/img/view_type/form.png', alt: 'View form'}">
+                        <t t-esc="form.name" />
+                    </ThumbnailItem>
+                </t>
             </div>
         </div>
         <div t-foreach="this.pageGroups" t-as="group" t-key="group.type" t-attf-class="o_website_studio_#{group.type}">

website_studio/static/tests/website_integrator_tests.js
@@ -57,12 +57,12 @@ QUnit.module("Website Integrator", (hooks) => {
                 (el) => el.textContent === "Website"
             )[0];
             await click(websiteItem);
-            assert.containsN(target, ".o_website_studio_form .o-website-studio-item-card", 2);
+            assert.containsN(target, ".o_website_studio_form .o_web_studio_thumbnail_item", 2);
             const websiteStudioForms = target.querySelectorAll(
-                ".o_website_studio_form .o-website-studio-item-card"
+                ".o_website_studio_form .o_web_studio_thumbnail_item"
             );
-            assert.strictEqual(websiteStudioForms[0].textContent, "New Form");
-            assert.strictEqual(websiteStudioForms[1].textContent, "partner");
+            assert.strictEqual(websiteStudioForms[0].innerText, "New Form");
+            assert.strictEqual(websiteStudioForms[1].innerText, "Partner");
 
             assert.verifySteps(["/website_studio/get_forms", "/website_studio/get_website_pages"]);
         }

web_studio/__manifest__.py
@@ -54,6 +54,8 @@ Note: Only the admin user is allowed to make those customizations.
 
             'web_studio/static/src/client_action/studio_action_loader.js',
             'web_studio/static/src/client_action/app_creator/app_creator_shortcut.js',
+            'web_studio/static/src/client_action/components/**/*.js',
+            'web_studio/static/src/client_action/components/**/*.xml',
             'web_studio/static/src/home_menu/**/*.js',
             'web_studio/static/src/views/**/*.js',
             ('remove', 'web_studio/static/src/views/kanban_report/**/*'),

web_studio/static/src/client_action/action_editor/action_editor.js
@@ -4,7 +4,6 @@ import { useOwnedDialogs, useService } from "@web/core/utils/hooks";
 import { registry } from "@web/core/registry";
 import { _t } from "@web/core/l10n/translation";
 import { sortBy } from "@web/core/utils/arrays";
-import { Dropdown } from "@web/core/dropdown/dropdown";
 import { DropdownItem } from "@web/core/dropdown/dropdown_item";
 import { AlertDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 import { standardActionServiceProps } from "@web/webclient/actions/action_service";
@@ -17,6 +16,7 @@ import {
 import { CharField } from "@web/views/fields/char/char_field";
 import { TextField } from "@web/views/fields/text/text_field";
 
+import { ThumbnailItem } from "@web_studio/client_action/components/thumbnail_item/thumbnail_item";
 import { viewTypeToString, useStudioServiceAsReactive } from "@web_studio/studio_service";
 import { NewViewDialog } from "../editor/new_view_dialogs/new_view_dialog";
 import { MapNewViewDialog } from "../editor/new_view_dialogs/map_new_view_dialog";
@@ -75,7 +75,7 @@ class ActionEditor extends Component {
     static template = "web_studio.ActionEditor";
     static props = { ...standardActionServiceProps };
     static components = {
-        Dropdown,
+        ThumbnailItem,
         DropdownItem,
         Record,
         CharField,

web_studio/static/src/client_action/action_editor/action_editor.scss
@@ -1,90 +1,30 @@
-.o_studio {
-    .o_web_studio_action_editor {
-        @include o-web-studio-thumbnails-container;
+.o_web_studio_action_editor {
+    background-color: $o-web-studio-bg-light;
 
-        .o_web_studio_views {
-            flex: 1 1 auto;
-            @include o-web-studio-thumbnails;
+    .o_web_studio_views {
+        flex: 1 1 auto;
 
-            @include media-breakpoint-up(lg) {
-                flex-flow: column wrap;
+        @include media-breakpoint-up(lg) {
+            flex-flow: column wrap;
+        }
+
+        @include media-breakpoint-up(xl) {
+            align-content: center;
+        }
+
+        .o_web_studio_thumbnail_item.disabled {
+            filter: grayscale(100%);
+
+            .o_web_studio_view_label {
+                transition: all 0.2s ease 0.1s;
             }
 
-            @include media-breakpoint-up(xl) {
-                align-content: center;
-            }
+            &:hover {
+                opacity: 1;
+                filter: grayscale(0%);
 
-            .o_web_studio_view_category {
-                > h6 {
-                    margin-bottom: $grid-gutter-width*0.5;
-                }
-
-                .o_web_studio_view_type {
-                    @include o-web-studio-thumbnail;
-
-                    transition: all .2s ease 0s;
-
-                    &.o_web_studio_inactive {
-                        opacity: 0.5;
-                        overflow: hidden;
-                        transition: all 0.3s ease 0s;
-
-                        .o_web_studio_view_label {
-                            z-index: 1;
-                            border-radius: 2px 0 0 2px;
-                            padding: 2px 17px 2px 10px;
-                            font-size: 0.8em;
-                            background: desaturate($o-brand-primary,20%);
-                            color: white;
-                            @include o-position-absolute(4px, -100px);
-                            transition: all 0.2s ease 0.1s;
-                        }
-
-                        .o_web_studio_thumbnail {
-                            height: 100%;
-                            padding-bottom: 20px;
-                            -webkit-filter: grayscale(100%);
-                            filter: grayscale(100%);
-                        }
-
-                        .o_web_studio_name, .o_web_studio_thumbnail img {
-                            pointer-events: none;
-                        }
-
-                        &:hover {
-                            opacity: 1;
-                            .o_web_studio_thumbnail{
-                                -webkit-filter: grayscale(0%);
-                                filter: grayscale(0%);
-                            }
-                            .o_web_studio_view_label {
-                                right: 0px;
-                            }
-                        }
-                    }
-
-                    .o_web_studio_default_view {
-                        font-size: 10px;
-                        color: darken($o-brand-primary, 5%);
-                        .fa-home {
-                            color: white;
-                        }
-                    }
-
-                    .o_web_studio_more {
-                        border: 1px solid white;
-                        border-width: 1px 1px 0 1px;
-                        padding: 5px 9px;
-                        border-radius: 50%;
-                        font-size: 10px;
-                        cursor: pointer;
-                        color: $o-web-studio-bg-dark;
-                        background-color: white;
-
-                        &:hover, &:focus {
-                            background-color: $o-web-studio-bg-light;
-                        }
-                    }
+                .o_web_studio_view_label {
+                    transform: translateX(-100%);
                 }
             }
         }

web_studio/static/src/client_action/action_editor/action_editor.xml
@@ -38,14 +38,36 @@
 </t>
 
 <t t-name="web_studio.ActionEditor.ViewsDashboard">
-  <div class="o_web_studio_views p-5" >
+  <div class="o_web_studio_views d-flex overflow-auto gap-3 p-5" >
     <div class="grid w-75 justify-content-center" style="grid-template-columns: repeat(auto-fit, minmax(400px, 35%)); gap: 40px;">
       <t t-foreach="viewCategories" t-as="category" t-key="category" >
         <div class="o_web_studio_view_category" t-att-name="category">
-            <h6><strong t-esc="category_value.title"/></h6>
+            <h6 class="mb-3 fw-bolder" t-esc="category_value.title" />
             <div class="grid" style="grid-template-columns: repeat(3, 115px); gap: 20px;">
               <t t-foreach="getOrderedViewTypes(category_value.viewTypes)" t-as="viewInfo" t-key="viewInfo.name">
-                <t t-call="web_studio.ActionEditor.ViewsDashboard.ViewThumbnail"/>
+                <ThumbnailItem onClick="() => this.onThumbnailClicked(viewInfo.name)" icon="{src: viewInfo.imgUrl, alt:'View ' + viewInfo.title }" showMoreMenu="viewInfo.isActive and (viewInfo.canBeDefault or viewInfo.canBeDisabled)" className="`o_web_studio_thumbnail_${viewInfo.name} overflow-hidden ${viewInfo.isActive ? '' : 'disabled'}`">
+                  <t t-set-slot="default">
+                    <t t-out="viewInfo.title" />
+                    <span t-if="viewInfo.isDefault" class="o_web_studio_default_view fa-stack fa-sm position-absolute top-0 end-0 text-primary m-1" role="img" aria-label="Default view" title="Default view">
+                        <i class="fa fa-circle fa-stack-2x"></i>
+                        <i class="fa fa-home fa-stack-1x text-white"></i>
+                    </span>
+                    <span t-if="!viewInfo.isActive" class="o_web_studio_view_label bg-dark bg-opacity-50 rounded-start text-white position-absolute top-0 start-100 my-1 py-1 px-2 fw-normal text-nowrap">Activate View</span>
+                  </t>
+                  <t t-set-slot="dropdown">
+                    <DropdownItem t-if="viewInfo.canBeDefault" onSelected="() => this.setDefaultView(viewInfo.name)" href="'#'" class="viewInfo.isDefault ? 'pe-none opacity-50' : ''" dataset="{ action: 'set_default_view' }">
+                      <i t-if="viewInfo.isDefault" class="fa fa-check" role="img" aria-label="Is default view" title="Is default view"/>
+                      Set As Default
+                    </DropdownItem>
+                    <div t-if="viewInfo.canBeDefault and viewInfo.canBeDisabled" class="dropdown-divider" role="separator"/>
+                    <DropdownItem t-if="env.debug" onSelected="() => this.restoreDefaultView(viewInfo.name)" href="'#'" dataset="{ action: 'restore_default_view' }">
+                      Restore Default View
+                    </DropdownItem>
+                    <DropdownItem t-if="viewInfo.canBeDisabled" onSelected="() => this.disableView(viewInfo.name)" dataset="{ action: 'disable_view' }">
+                      Disable View
+                    </DropdownItem>
+                  </t>
+                </ThumbnailItem>
               </t>
             </div>
         </div>
@@ -54,41 +76,4 @@
   </div>
 </t>
 
-<t t-name="web_studio.ActionEditor.ViewsDashboard.ViewThumbnail">
-    <div t-attf-class="o_web_studio_view_type flex-column {{!viewInfo.isActive ? 'o_web_studio_inactive' : ''}}" t-att-data-type="viewInfo.name">
-      <div class="o_web_studio_thumbnail w-100" t-on-click="() => this.onThumbnailClicked(viewInfo.name)">
-          <span t-if="!viewInfo.isActive" class="o_web_studio_view_label">Activate View</span>
-          <span t-if="viewInfo.isDefault" class="o_web_studio_default_view fa-stack fa-sm position-absolute end-0" role="img" aria-label="Default view" title="Default view">
-              <i class="fa fa-circle fa-stack-2x"></i>
-              <i class="fa fa-home fa-stack-1x"></i>
-          </span>
-          <img t-att-src="viewInfo.imgUrl" t-attf-alt="View {{ viewInfo.title }}"/>
-      </div>
-      <div class="d-flex w-100 justify-content-between align-items-center p-2">
-        <div class="o_web_studio_name" t-esc="viewInfo.title"/>
-        <t t-call="web_studio.ActionEditor.ViewsDashboard.ViewThumbnail.Dropdown" t-if="viewInfo.isActive">
-          <t t-set="viewInfo" t-value="viewInfo" />
-        </t>
-    </div>
-  </div>
-</t>
-
-<t t-name="web_studio.ActionEditor.ViewsDashboard.ViewThumbnail.Dropdown">
-  <Dropdown t-if="viewInfo.canBeDefault or viewInfo.canBeDisabled" togglerClass="'o_web_studio_more'">
-    <t t-set-slot="toggler">
-      <i class="fa fa-ellipsis-v" /> 
-    </t>
-    <DropdownItem t-if="viewInfo.canBeDefault" onSelected="() => this.setDefaultView(viewInfo.name)" href="'#'" class="viewInfo.isDefault ? 'pe-none opacity-50' : ''" dataset="{ action: 'set_default_view' }">
-      <i t-if="viewInfo.isDefault" class="fa fa-check" role="img" aria-label="Is default view" title="Is default view"/>
-      Set As Default
-    </DropdownItem>
-    <div t-if="viewInfo.canBeDefault and viewInfo.canBeDisabled" class="dropdown-divider" role="separator"/>
-    <DropdownItem t-if="env.debug" onSelected="() => this.restoreDefaultView(viewInfo.name)" href="'#'" dataset="{ action: 'restore_default_view' }">
-       Restore Default View
-    </DropdownItem>
-    <DropdownItem t-if="viewInfo.canBeDisabled" onSelected="() => this.disableView(viewInfo.name)" dataset="{ action: 'disable_view' }">
-      Disable View
-    </DropdownItem>
-  </Dropdown>
-</t>
 </templates>

web_studio/static/src/client_action/components/thumbnail_item/thumbnail_item.js
@@ -1,0 +1,23 @@
+/** @odoo-module */
+import { Component } from "@odoo/owl";
+import { Dropdown } from "@web/core/dropdown/dropdown";
+
+export class ThumbnailItem extends Component {
+    static defaultProps = {
+        showMoreMenu: true,
+        onClick: () => {},
+    };
+    static template = "web_studio.ThumbnailItem";
+    static props = {
+        className: { type: String, optional: true },
+        showMoreMenu: { type: Boolean, optional: true },
+        icon: { type: Object },
+        onClick: { type: Function, optional: true },
+        slots: true,
+    };
+    static components = { Dropdown };
+
+    get hasDropdown() {
+        return this.props.showMoreMenu && this.props.slots && this.props.slots.dropdown;
+    }
+}

commit 4d6d2c1e539e1c92ae57995b51a49543c49b2fe9

[FIX] web_studio,website_studio: fix adding a website form
In the website tabl, try to add a website form
Before this commit, there was a crash because of some callback that was not bound.
This was caused by commit e3c8f97d0b8553abc1e77bd3699fcb880e48ab0a

After this commit, the flow works as expected.

opw-3735624

closes odoo/enterprise#57155

X-original-commit: f8b96474f7b461ce3913183cae5219a75b8ba9a1
Signed-off-by: Luca Vitali (luvi) <luvi@odoo.com>

web_studio/static/src/client_action/components/thumbnail_item/thumbnail_item.xml
@@ -4,7 +4,7 @@
 <t t-name="web_studio.ThumbnailItem">
     <div t-attf-class="o_web_studio_thumbnail_item position-relative d-flex flex-column justify-content-center align-items-center bg-light border rounded-2 {{ this.props.className || '' }}" t-on-click="this.props.onClick">
         <div class="o_web_studio_thumbnail w-100 cursor-pointer d-flex justify-content-center align-items-center">
-            <img class="m-auto" t-att-src="this.props.icon.src" alt="this.props.icon.alt"/>
+            <img class="m-auto" t-att-src="this.props.icon.src" t-att-alt="this.props.icon.alt"/>
         </div>
         <div class="o_web_studio_name d-flex w-100 gap-1 text-capitalize fw-bold text-black" t-att-class="hasDropdown ? 'justify-content-between ps-2' : 'justify-content-center'">
             <span class="d-block align-self-center fw-bold text-wrap text-break">

website_studio/static/src/website_integrator/website_integrator.xml
@@ -3,7 +3,7 @@
 
 <t t-name="website_studio.WebsiteIntegrator.ListingOrPage">
     <div class="overflow-auto d-flex flex-wrap align-items-stretch flex-row gap-3 pb-5">
-        <ThumbnailItem className="'o_website_studio_new_card' + (!isDesigner ? 'disabled' : '')" onClick="() => this.onNewPage(group.type, website.id)" icon="{src: '/web_studio/static/src/img/default_icon_app.png', alt: 'App icon'}">
+        <ThumbnailItem className="'o_website_studio_new_card' + (!isDesigner ? ' disabled' : '')" onClick="() => this.onNewPage(group.type, website.id)" icon="{src: '/web_studio/static/src/img/default_icon_app.png', alt: 'App icon'}">
             <t t-if="group.type === 'listing'">New Listing</t>
             <t t-else="">New Page</t>
         </ThumbnailItem>
@@ -55,7 +55,7 @@
                 <span> &#8212; Allow visitors of your website to create records themselves</span>
             </div>
             <div class="d-flex flex-row gap-4 ms-4 pb-5">
-                <ThumbnailItem className="'o_website_studio_new_card' + (!isDesigner ? 'disabled' : '')" onClick="onNewForm" icon="{src: '/web_studio/static/src/img/default_icon_app.png', alt: 'App icon'}">
+                <ThumbnailItem className="'o_website_studio_new_card' + (!isDesigner ? ' disabled' : '')" onClick="() => this.onNewForm()" icon="{src: '/web_studio/static/src/img/default_icon_app.png', alt: 'App icon'}">
                     New Form
                 </ThumbnailItem>
                 <t t-foreach="state.forms" t-as="form" t-key="form.id">

website_studio/static/tests/website_integrator_tests.js
@@ -1,6 +1,8 @@
 /** @odoo-module **/
-import { click, getFixture, nextTick } from "@web/../tests/helpers/utils";
+import { click, getFixture, nextTick, patchWithCleanup } from "@web/../tests/helpers/utils";
 import { getActionManagerServerData } from "@web/../tests/webclient/helpers";
+import { browser } from "@web/core/browser/browser";
+import { user } from "@web/core/user";
 import { createEnterpriseWebClient } from "@web_enterprise/../tests/helpers";
 import { openStudio, registerStudioDependencies } from "@web_studio/../tests/helpers";
 
@@ -67,4 +69,64 @@ QUnit.module("Website Integrator", (hooks) => {
             assert.verifySteps(["/website_studio/get_forms", "/website_studio/get_website_pages"]);
         }
     );
+
+    QUnit.test("create new form", async (assert) => {
+        let isDesigner = false;
+        patchWithCleanup(user, {
+            hasGroup: async () => isDesigner,
+        });
+
+        patchWithCleanup(browser, {
+            open: (...args) => {
+                assert.step(["open", ...args].join(" "));
+                return { closed: false };
+            },
+        });
+
+        await createEnterpriseWebClient({
+            serverData,
+            mockRPC: (route) => {
+                if (route === "/website_studio/get_forms") {
+                    return Promise.resolve([]);
+                }
+                if (route === "/website_studio/get_website_pages") {
+                    return { websites: [], pages: [] };
+                }
+                if (route === "/website_studio/create_form") {
+                    assert.step("create_form");
+                    return "/some/url";
+                }
+            },
+        });
+        await nextTick();
+        // open app Ponies (act window action)
+        await click(target, ".o_app[data-menu-xmlid=app_1]");
+        await nextTick();
+        await openStudio(target);
+        const websiteItem = [...target.querySelectorAll(".o_menu_sections a")].filter(
+            (el) => el.textContent === "Website"
+        )[0];
+        await click(websiteItem);
+        assert.strictEqual(
+            target
+                .querySelector(".o_website_studio_form .o_website_studio_new_card img")
+                .getAttribute("data-alt"),
+            "App icon"
+        );
+        await click(target, ".o_website_studio_form .o_website_studio_new_card");
+        assert.containsOnce(target, ".o_notification");
+        const notifText = target.querySelector(
+            ".o_notification .o_notification_content"
+        ).textContent;
+        assert.ok(
+            notifText.includes(
+                "Sorry, only users with the following access level are currently allowed to do that"
+            )
+        );
+
+        isDesigner = true;
+        await click(websiteItem);
+        await click(target, ".o_website_studio_form .o_website_studio_new_card");
+        assert.verifySteps(["create_form", "open /some/url?enable_editor=1 _blank"]);
+    });
 });
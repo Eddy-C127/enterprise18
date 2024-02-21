4d6d2c1e539e1c92ae57995b51a49543c49b2fe9
Lucas Perais <lpe@odoo.com>
2024-02-21 11:39:32 +0100

    [FIX] web_studio,website_studio: fix adding a website form
    In the website tabl, try to add a website form
    Before this commit, there was a crash because of some callback that was not bound.
    This was caused by commit e3c8f97d0b8553abc1e77bd3699fcb880e48ab0a
    
    After this commit, the flow works as expected.
    
    opw-3735624
    
    closes odoo/enterprise#57155
    
    X-original-commit: f8b96474f7b461ce3913183cae5219a75b8ba9a1
    Signed-off-by: Luca Vitali (luvi) <luvi@odoo.com>

--- website_studio/static/tests/website_integrator_tests.js
+++ website_studio/static/tests/website_integrator_tests.js
@@ -1,6 +1,8 @@
 /** @odoo-module **/
-import { click, getFixture, nextTick } from "@web/../tests/helpers/utils";
+import { click, getFixture, nextTick, patchWithCleanup } from "@web/../tests/helpers/utils";
 import { getActionManagerServerData } from "@web/../tests/webclient/helpers";
+import { browser } from "@web/core/browser/browser";
+import { user } from "@web/core/user";
 import { createEnterpriseWebClient } from "@web_enterprise/../tests/helpers";
 import { openStudio, registerStudioDependencies } from "@web_studio/../tests/helpers";
 
@@ -67,4 +69,64 @@ QUnit.module("Website Integrator", (hooks) => {
             assert.verifySteps(["/website_studio/get_forms", "/website_studio/get_website_pages"]);
         }
     );
+
+    QUnit.test("create new form", async (assert) => {
+        let isDesigner = false;
+        patchWithCleanup(user, {
+            hasGroup: async () => isDesigner,
+        });
+
+        patchWithCleanup(browser, {
+            open: (...args) => {
+                assert.step(["open", ...args].join(" "));
+                return { closed: false };
+            },
+        });
+
+        await createEnterpriseWebClient({
+            serverData,
+            mockRPC: (route) => {
+                if (route === "/website_studio/get_forms") {
+                    return Promise.resolve([]);
+                }
+                if (route === "/website_studio/get_website_pages") {
+                    return { websites: [], pages: [] };
+                }
+                if (route === "/website_studio/create_form") {
+                    assert.step("create_form");
+                    return "/some/url";
+                }
+            },
+        });
+        await nextTick();
+        // open app Ponies (act window action)
+        await click(target, ".o_app[data-menu-xmlid=app_1]");
+        await nextTick();
+        await openStudio(target);
+        const websiteItem = [...target.querySelectorAll(".o_menu_sections a")].filter(
+            (el) => el.textContent === "Website"
+        )[0];
+        await click(websiteItem);
+        assert.strictEqual(
+            target
+                .querySelector(".o_website_studio_form .o_website_studio_new_card img")
+                .getAttribute("data-alt"),
+            "App icon"
+        );
+        await click(target, ".o_website_studio_form .o_website_studio_new_card");
+        assert.containsOnce(target, ".o_notification");
+        const notifText = target.querySelector(
+            ".o_notification .o_notification_content"
+        ).textContent;
+        assert.ok(
+            notifText.includes(
+                "Sorry, only users with the following access level are currently allowed to do that"
+            )
+        );
+
+        isDesigner = true;
+        await click(websiteItem);
+        await click(target, ".o_website_studio_form .o_website_studio_new_card");
+        assert.verifySteps(["create_form", "open /some/url?enable_editor=1 _blank"]);
+    });
 });
