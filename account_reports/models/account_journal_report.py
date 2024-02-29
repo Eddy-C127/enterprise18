22c5b9dcc5c3400d902eb31ef295ef856970f011
Mélanie Cruypeninck (crm) <crm@odoo.com>
2024-02-29 16:16:07 +0100

    [FIX] account_reports : fix account type filter on journal report
    Module : account_reports
    
    Issue reproduction :
    
    Go to Accounting / Configuration / Management / Accounting reports
    Edit "Journal Report"
    Activate the filter "Account types" in the options tab
    Go on Accounting / Reporting / Audit reports / Journal Report
    traceback with a psycopg2.errors.UndefinedTable error
    Cause :
    The query generated in _query_journal() on account_journal_report.py doesn't use account_account table in the FROM clause. This table is included in the tables value created from _query_get() but this variable can't be used as it is because it starts with the account_move_line table for the FROM clause.
    
    Solution :
    Use the tables variable and modify the sql query.
    
    Versions impacted :
    
    16.0, saas-16-1, saas-16-2, saas-16-3, saas-16-4
    17, saas-17-1
    Note : filter_account_type change from a boolean field to a selection field from version 17 but the error is the same
    
    Support ticket : 3708809
    
    closes odoo/enterprise#59439
    
    X-original-commit: c4d25cfb0fc239aefe2912b1b4600e466d365abc
    Signed-off-by: Quentin De Paoli <qdp@odoo.com>

--- account_reports/models/account_journal_report.py
+++ account_reports/models/account_journal_report.py
@@ -193,8 +193,8 @@ class JournalReportCustomHandler(models.AbstractModel):
                     j.currency_id,
                     journal_curr.name as currency_name,
                     cp.currency_id as company_currency
-                FROM account_journal j
-                JOIN account_move_line ON j.id = account_move_line.journal_id
+                FROM {tables}
+                JOIN account_journal j ON j.id = account_move_line.journal_id
                 JOIN res_company cp ON cp.id = j.company_id
                 LEFT JOIN res_currency journal_curr on journal_curr.id = j.currency_id
                 WHERE {where_clause}
