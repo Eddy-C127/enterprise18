ba5be073c7e63098c35d44a3756273964e64b4fc
Arthur Pierrot (arpi) <arpi@odoo.com>
2024-03-01 12:49:21 +0100

    [IMP] l10n_ae_hr_payroll: add master report
    This will add a new Reporting menu for the UAE to create a master report.
    This master report is an Excel file representing one or more month(s)
    with each employee and the amount earned by them.
    
    closes odoo/enterprise#61917
    
    Task: 3744558
    Related: odoo/upgrade#6160
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- l10n_ae_hr_payroll/__manifest__.py
+++ l10n_ae_hr_payroll/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'U.A.E. - Payroll',
@@ -12,11 +11,14 @@ United Arab Emirates Payroll and End of Service rules.
     """,
     'depends': ['hr_payroll'],
     'data': [
+        'security/ir.model.access.csv',
         'data/hr_payroll_structure_type_data.xml',
         'data/hr_payroll_structure_data.xml',
         'data/hr_salary_rule_data.xml',
         'data/hr_payslip_input_type_data.xml',
         'views/hr_contract_views.xml',
+        'views/hr_payroll_master_report_views.xml',
+        'data/l10n_ae_hr_payroll_demo.xml',
     ],
     'license': 'OEEL-1',
     'auto_install': True,
