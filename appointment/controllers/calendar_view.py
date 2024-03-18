0ae79ee391739ac5b980d129f87c38b216489a4d
Noe Antoine <nan@odoo.com>
2024-03-18 10:17:46 +0100

    [IMP] appointment: add form dialog on sharing availabilities
    In order to ease the use of custom appointment types,
    the user, after selecting custom slots in the calendar view,
    can go to "Configure" button. This creates the appointment
    type, and before this commit, it redirected to the form view.
    
    In order to stay on this view and keep the flow simple and
    smooth, the button now opens a custom dialog form view, simplified
    and having custom buttons: "Save and Copy Link" (also closing the
    dialog) and "More options" leading to the full form view, as it
    was working before.
    
    The user can change the created appointment type through one
    of those two form views: the appointment is saved in both cases
    when leaving the dialog form with 'Save and Copy Link' or
    'More options'.
    
    On closing the dialog using the discard button, the slots will
    still appear. This allows easy update, and if one adds or
    removes slots, then on opening the dialog again / on copying the
    link, the slots will be updated. Once the link is copied in one
    of both possible ways, the slots disappear as we consider the
    slots properly configured.
    
    In order to have links properly working, even after changing
    users in configuration form or full form, we use on creation an
    appointment.invite with 'all_assigned_resources' for resource_choice.
    This way, any copied link will be redirecting to appropriate users.
    A few conditions are added to differentiate this behavior from the
    one used when sharing other types of appointments.
    
    Also, to avoid creating useless links, and because this feature
    is designed to allow customization when creating the appointment
    and invitation on the fly, the same link is passed as props and
    used as long as we are on the calendar view and do not use
    onClickDiscard.
    
    Task-3805720
    
    closes odoo/enterprise#59017
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- appointment/controllers/calendar_view.py
+++ appointment/controllers/calendar_view.py
@@ -59,9 +59,35 @@ class AppointmentCalendarView(http.Controller):
                 'allday': slot.get('allday'),
                 'slot_type': 'unique',
             }) for slot in slots],
+            'staff_user_ids': request.env.user.ids,
         })
 
         return self._get_staff_user_appointment_invite_info(appointment_type)
+
+    @route('/appointment/appointment_type/update_custom', type='json', auth='user')
+    def appointment_type_update_custom(self, appointment_type_id, slots):
+        """
+            Updates the slots of a custom appointment when a user changes them on
+            the calendar view, on sharing link / opening the configuration form.
+        """
+        if not appointment_type_id or not request.env.user._is_internal():
+            raise Forbidden()
+        if not slots:
+            raise ValidationError(_("A list of slots information is needed to update this custom appointment type"))
+
+        appointment_type = request.env['appointment.type'].browse(int(appointment_type_id))
+        if appointment_type.category != 'custom' or appointment_type.create_uid != request.env.user:
+            raise Forbidden()
+
+        appointment_type.write({
+            'slot_ids': [(5, 0, 0)] + [(0, 0, {
+                'allday': slot.get('allday'),
+                'end_datetime': fields.Datetime.from_string(slot.get('end')),
+                'slot_type': 'unique',
+                'start_datetime': fields.Datetime.from_string(slot.get('start')),
+            }) for slot in slots],
+        })
+        return True
 
     @route('/appointment/appointment_type/get_book_url', type='json', auth='user')
     def appointment_get_book_url(self, appointment_type_id, context=None):
@@ -122,18 +148,46 @@ class AppointmentCalendarView(http.Controller):
         appointment_invitation_domain = self._get_staff_user_appointment_invite_domain(appointment_type)
         appointment_invitation = request.env['appointment.invite'].search(appointment_invitation_domain, limit=1)
         if not appointment_invitation:
+            invitation_values = {
+                'appointment_type_ids': appointment_type.ids,
+                'resources_choice': 'current_user',
+                'staff_user_ids': request.env.user.ids,
+            }
+            if appointment_type.category == 'custom':
+                # Custom appointment users may be edited on the spot. 'all_assigned_resources'
+                # allows the link copied to be correctly configured with latest users.
+                invitation_values.update({
+                    'resources_choice': 'all_assigned_resources',
+                    'staff_user_ids': False,
+                })
             appointment_invitation = request.env['appointment.invite'].with_context(
                 request.env['appointment.type']._get_clean_appointment_context()
-            ).create({
-                'appointment_type_ids': appointment_type.ids,
-                'staff_user_ids': request.env.user.ids,
-            })
+            ).create(invitation_values)
+
+        dialog_form_view = request.env.ref(
+            'appointment.appointment_type_view_form_custom_share',
+            raise_if_not_found=False
+        )
         return {
             'appointment_type_id': appointment_type.id,
             'invite_url': appointment_invitation.book_url,
+            'view_id': dialog_form_view.id if dialog_form_view else False,
         }
 
     def _get_staff_user_appointment_invite_domain(self, appointment_type):
+        """ Returns the domain used to search for an existing invitation when copying a link
+        to any 'punctual' or 'recurring' appointment type in the calendar view. When sharing an
+        'anytime' appointment, we search for existing invitations (as the appointment may already
+        exist). This prevents duplicating appointment.invite.
+        The user can modify 'custom' appointments and share their link more than once. Indeed, the
+        'configure' button allows them to change staff users. They can even remove themselves from
+        the staff users. As a new 'custom' appointment is created on using the 'select dates' feature
+        on the fly, we want to search for the invitation that has been created by the current user. """
+        if appointment_type.category == 'custom':
+            return [
+                ('appointment_type_ids', '=', appointment_type.id),
+                ('create_uid', '=', request.env.user.id),
+            ]
         return [
             ('appointment_type_ids', '=', appointment_type.id),
             ('staff_user_ids', '=', request.env.user.id),
--- appointment/static/src/views/appointment_calendar/appointment_calendar_controller.js
+++ appointment/static/src/views/appointment_calendar/appointment_calendar_controller.js
@@ -8,9 +8,11 @@ import { rpc } from "@web/core/network/rpc";
 import { Tooltip } from "@web/core/tooltip/tooltip";
 import { browser } from "@web/core/browser/browser";
 import { serializeDateTime } from "@web/core/l10n/dates";
+import { useService } from "@web/core/utils/hooks";
 import { useRef, useState, useSubEnv, onWillStart } from "@odoo/owl";
 import { Dropdown } from "@web/core/dropdown/dropdown";
 import { user } from "@web/core/user";
+import { CustomAppointmentFormViewDialog } from "../custom_appointment_form_dialog/custom_appointment_form_dialog";
 
 patch(AttendeeCalendarController, {
     components: { ...AttendeeCalendarController.components, Dropdown },
@@ -21,6 +23,7 @@ patch(AttendeeCalendarController.prototype, {
         super.setup(...arguments);
         this.popover = usePopover(Tooltip, { position: "bottom" });
         this.copyLinkRef = useRef("copyLinkRef");
+        this.orm = useService("orm");
 
         this.appointmentState = useState({
             data: {},
@@ -42,26 +45,21 @@ patch(AttendeeCalendarController.prototype, {
     },
 
     async _createCustomAppointmentType() {
-        const slots = Object.values(this.model.data.slots).map(slot => ({
-            start: serializeDateTime(slot.start),
-            end: serializeDateTime(slot.start === slot.end ? slot.end.plus({ days: 1 }) : slot.end), //TODO: check if necessary
-            allday: slot.isAllDay,
-        }));
         const customAppointment = await rpc(
             "/appointment/appointment_type/create_custom",
             {
-                slots: slots,
+                slots: this._getSlots(),
                 context: this.props.context, // This allows to propagate keys like default_opportunity_id / default_applicant_id
             },
         );
         if (customAppointment.appointment_type_id) {
             this.appointmentState.lastAppointment = {
                 'id': customAppointment.appointment_type_id,
+                'isCustom': true,
                 'url': customAppointment.invite_url,
+                'viewId': customAppointment.view_id,
             }
         }
-        this.env.calendarState.mode = "default";
-        this.model.clearSlots();
     },
 
     /**
@@ -69,6 +67,24 @@ patch(AttendeeCalendarController.prototype, {
      */
     hasSlotEvents() {
         return Object.keys(this.model.data.slots).length;
+    },
+
+    _getSlots() {
+        return Object.values(this.model.data.slots).map(slot => ({
+            start: serializeDateTime(slot.start),
+            end: serializeDateTime(slot.start === slot.end ? slot.end.plus({ days: 1 }) : slot.end), //TODO: check if necessary
+            allday: slot.isAllDay,
+        }));
+    },
+
+    async _updateCustomAppointmentSlots() {
+        await rpc(
+            "/appointment/appointment_type/update_custom",
+            {
+                appointment_type_id: this.appointmentState.lastAppointment.id,
+                slots: this._getSlots(),
+            },
+        );
     },
 
     _writeUrlToClipboard() {
@@ -99,11 +115,18 @@ patch(AttendeeCalendarController.prototype, {
     async onClickGetShareLink() {
         if (!this.appointmentState.lastAppointment) {
             await this._createCustomAppointmentType();
+        } else if (this.appointmentState.lastAppointment.isCustom && this.env.calendarState.mode === "slots-creation") {
+            await this._updateCustomAppointmentSlots();
         }
         this._writeUrlToClipboard();
         if (!this.copyLinkRef.el) {
             return;
         }
+        if (this.appointmentState.lastAppointment.isCustom) {
+            this.env.calendarState.mode = "default";
+            this.model.clearSlots();
+        }
+        this.appointmentState.lastAppointment.waitingCopyLinkCustomType = false;
         this.popover.open(this.copyLinkRef.el, { tooltip: _t("Copied!") });
         browser.setTimeout(this.popover.close, 800);
     },
@@ -111,14 +134,38 @@ patch(AttendeeCalendarController.prototype, {
     async onClickOpenForm() {
         if (!this.appointmentState.lastAppointment) {
             await this._createCustomAppointmentType();
+        } else if (this.appointmentState.lastAppointment.isCustom && this.env.calendarState.mode === "slots-creation") {
+            await this._updateCustomAppointmentSlots();
         }
-        this.actionService.doAction({
-            name: "Open Appointment Type Form",
-            type: 'ir.actions.act_window',
-            res_model: 'appointment.type',
-            views: [[false, 'form']],
-            res_id: this.appointmentState.lastAppointment.id,
-        });
+
+        if (this.appointmentState.lastAppointment.isCustom) {
+            // The url copy paste may be done in the configuration dialog.
+            // This is used in order to not display the "Link Copied" message too early.
+            this.appointmentState.lastAppointment.waitingCopyLinkCustomType = this.env.calendarState.mode === "slots-creation";
+            this.displayDialog(CustomAppointmentFormViewDialog, {
+                context: this.props.context,
+                inviteUrl: this.appointmentState.lastAppointment.url,
+                resId: this.appointmentState.lastAppointment.id,
+                resModel: 'appointment.type',
+                size: 'md',
+                title: _t('Share Availabilities'),
+                viewId: this.appointmentState.lastAppointment.viewId,
+                onLinkCopied: () => {;
+                    this.appointmentState.lastAppointment.waitingCopyLinkCustomType = false;
+                    this.env.calendarState.mode = "default";
+                    this.model.clearSlots();
+                },
+            });
+        }
+        else {
+            this.actionService.doAction({
+                name: _t('Open Appointment Type Form'),
+                type: 'ir.actions.act_window',
+                res_model: 'appointment.type',
+                views: [[false, 'form']],
+                res_id: this.appointmentState.lastAppointment.id,
+            });
+        }
     },
 
     onClickDiscard() {
