250063c5863d68eeafcb73b38261e0931023cde3
Mohammed Basioni <basm@odoo.com>
2024-04-01 14:37:38 +0000

    [FIX] *: convert inline templates with text to regular templates
    * = hr_payroll_holidays
    
    Strings within inline templates are not translatable, so we convert
    these templates into standard templates so that they can be.
    
    Task-3761551
    
    closes odoo/enterprise#60357
    
    X-original-commit: 20cf5edca609a5bcfacd0b024425397503ca201f
    Related: odoo/odoo#161176
    Signed-off-by: Louis Wicket (wil) <wil@odoo.com>

--- hr_payroll_holidays/static/src/views/hooks.js
+++ hr_payroll_holidays/static/src/views/hooks.js
@@ -1,27 +1,33 @@
-/** @odoo-module **/
+import { Component, onWillStart } from "@odoo/owl";
 
+import { _t } from "@web/core/l10n/translation";
 import { user } from "@web/core/user";
 import { useService } from "@web/core/utils/hooks";
-import { Component, onWillStart, xml } from "@odoo/owl";
 
 export class TimeOffToDeferWarning extends Component {
-    // inline template is used as the component is dynamically loaded
-    static template = xml`
-        <div class="alert alert-warning text-center mb-0" role="alert">
-            <p class="mb-0">
-                You have some <button class="btn btn-link p-0 o_open_defer_time_off" role="button" t-on-click="onTimeOffToDefer">time off</button> to defer to the next month.
-            </p>
-        </div>
-    `;
     static props = {};
+    static template = "hr_payroll_holidays.TimeOffToDeferWarning";
 
     setup() {
         this.actionService = useService("action");
     }
+
+    /** @returns {string} */
+    get timeOffButtonText() {
+        const [, before, inside, after] = _t(
+            "You have some <button>time off</button> to defer to the next month."
+        ).match(/(.*)<button>(.*)<\/button>(.*)/) ?? [
+            "You have some",
+            "time off",
+            "to defer to the next month.",
+        ];
+        return { before, inside, after };
+    }
+
     onTimeOffToDefer() {
         this.actionService.doAction("hr_payroll_holidays.hr_leave_action_open_to_defer");
     }
-};
+}
 
 export function useTimeOffToDefer() {
     const orm = useService("orm");
