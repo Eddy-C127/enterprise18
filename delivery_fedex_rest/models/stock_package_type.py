a432925a7f8520afb4ecf870b0c4c3fbc61c45a6
Pieter Claeys (clpi) <clpi@odoo.com>
2024-04-02 08:57:49 +0000

    [ADD] delivery_fedex_rest: FedEx integration using new REST API
    The existing FedEx implementation used the old FedEx SOAP API which is
    no longer in development. This new version makes use of the current REST
    APIs available at developer.fedex.com
    
    task-3759206
    
    closes odoo/enterprise#73899
    
    X-original-commit: 44e32359bb79eed08a41861e155c98f664c6d09a
    Related: odoo/odoo#187392
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- delivery_fedex_rest/models/stock_package_type.py
+++ delivery_fedex_rest/models/stock_package_type.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from odoo import fields, models
 
@@ -6,4 +5,4 @@ from odoo import fields, models
 class PackageType(models.Model):
     _inherit = 'stock.package.type'
 
-    package_carrier_type = fields.Selection(selection_add=[('fedex', 'FedEx (Legacy)')])
+    package_carrier_type = fields.Selection(selection_add=[('fedex_rest', 'FedEx')])
