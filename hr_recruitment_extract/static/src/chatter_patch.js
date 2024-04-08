ad64a79f605381295f8c1b76db8fe8715d544ce9
tsm-odoo <tsm@odoo.com>
2024-04-08 10:25:48 +0200

    [FIX] hr_recruitement_extract: upload file on temporary threads
    closes odoo/enterprise#60312
    
    X-original-commit: 817461c4ab736aa0c686d58977870d42c4bc18b9
    Related: odoo/odoo#161057
    Signed-off-by: Matthieu Stockbauer (tsm) <tsm@odoo.com>

--- hr_recruitment_extract/static/src/chatter_patch.js
+++ hr_recruitment_extract/static/src/chatter_patch.js
@@ -1,21 +1,21 @@
 /* @odoo-module */
 
 import { Chatter } from "@mail/chatter/web_portal/chatter";
+import { useService } from "@web/core/utils/hooks";
 import { patch } from "@web/core/utils/patch";
-import { useAttachmentUploader } from "@mail/core/common/attachment_uploader_hook";
 
 patch(Chatter.prototype, {
     setup() {
         super.setup();
-        this.attachmentUploader = useAttachmentUploader(
-            this.store.Thread.insert({ model: this.props.threadModel, id: this.props.threadId }),
-            {
-                onFileUploaded: () => {
-                    if (this.state.thread?.model === "hr.applicant") {
-                        this.reloadParentView();
-                    }
-                },
+        this.attachmentUploadService = useService("mail.attachment_upload");
+        const thread = this.store.Thread.insert({
+            model: this.props.threadModel,
+            id: this.props.threadId,
+        });
+        this.attachmentUploadService.onFileUploaded(thread, () => {
+            if (this.state.thread?.model === "hr.applicant") {
+                this.reloadParentView();
             }
-        );
+        });
     },
 });
