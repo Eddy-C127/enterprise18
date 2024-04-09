bd5b190f39211fae71ea2a2045b37e2aa9878a2b
Ricardo Gomes Rodrigues <rigr@odoo.com>
2024-04-09 16:50:34 +0200

    [FIX] l10n_be_codabox: deprecate module methods in 17.0
    We refactored the CodaBox module in https://github.com/odoo/enterprise/pull/55858
    and made `l10n_be_codabox` and `l10n_be_codabox_bridge` deprecated.
    
    All methods of `l10n_be_codabox_bridge` were marked as deprecated and that should
    have been sufficient since this module was on auto_install=True if `l10n_be_codabox`
    is installed. However, some DBs seem to have `l10n_be_codabox` without `l10n_be_codabox_bridge`.
    Therefore, with this commit, we also deprecate the methods in `l10n_be_codabox` just to be sure.
    
    Task-id: none (validated with WTA)
    Part-of: odoo/enterprise#60333

--- l10n_be_codabox/models/res_company.py
+++ l10n_be_codabox/models/res_company.py
@@ -5,7 +5,7 @@ import requests
 
 from odoo import models, fields, _, api
 from odoo.exceptions import UserError
-from odoo.addons.l10n_be_codabox.const import get_error_msg, get_iap_endpoint
+from odoo.addons.l10n_be_codabox.const import get_error_msg, raise_deprecated
 
 
 class ResCompany(models.Model):
@@ -42,42 +42,7 @@ class ResCompany(models.Model):
             raise UserError(_("The fiduciary VAT number is not set."))
 
     def _l10n_be_codabox_connect(self):
-        self._l10n_be_codabox_verify_prerequisites()
-        params = {
-            "db_uuid": self.env["ir.config_parameter"].sudo().get_param("database.uuid"),
-            "company_vat": re.sub("[^0-9]", "", self.vat),
-            "fidu_vat": re.sub("[^0-9]", "", self.l10n_be_codabox_fiduciary_vat or ""),
-            "iap_token": self.l10n_be_codabox_iap_token,
-            "callback_url": self.get_base_url(),
-        }
-        try:
-            result = self._l10_be_codabox_call_iap(f"{get_iap_endpoint(self.env)}/connect", params)
-            self.l10n_be_codabox_is_connected = True
-            if result.get("iap_token"):
-                self.l10n_be_codabox_iap_token = result["iap_token"]
-            url = result.get("confirmation_url")
-            if url != self.get_base_url():  # Redirect user to CodaBox website to confirm the connection
-                return {
-                    "name": _("CodaBox"),
-                    "type": "ir.actions.act_url",
-                    "url": url,
-                    "target": "self",
-                }
-        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
-            raise UserError(get_error_msg({"type": "error_connecting_iap"}))
+        raise_deprecated(self.env)
 
     def _l10n_be_codabox_revoke(self):
-        self._l10n_be_codabox_verify_prerequisites()
-        params = {
-            "db_uuid": self.env["ir.config_parameter"].sudo().get_param("database.uuid"),
-            "fidu_vat": re.sub("[^0-9]", "", self.l10n_be_codabox_fiduciary_vat or ""),
-            "company_vat": re.sub("[^0-9]", "", self.vat),
-            "iap_token": self.l10n_be_codabox_iap_token or "",
-        }
-        try:
-            self._l10_be_codabox_call_iap(f"{get_iap_endpoint(self.env)}/revoke", params)
-            self.l10n_be_codabox_fiduciary_vat = False
-            self.l10n_be_codabox_is_connected = False
-            self.l10n_be_codabox_iap_token = False
-        except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
-            raise UserError(get_error_msg({"type": "error_connecting_iap"}))
+        raise_deprecated(self.env)
