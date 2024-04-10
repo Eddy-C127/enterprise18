99fb0cb3ae3ab891d5f4754de9b5571541254f02
Earth Patel <eapa@odoo.com>
2024-04-10 15:23:52 +0530

    [FIX] web_grid: resolve traceback on consolidation trial balance grid
    Steps to reproduce
    ==================
    
    - Go to consolidation
    - Open a record from the dashboard
    - Go to "trial balance grid" by clicking on the edit button
    - Clicks on any consolidation account
    
    => It gives the traceback
    
    Cause of the issue
    ==================
    
    The value of the "range" variable is undefined, thus giving the traceback while
    trying to access the "name".
    
    closes odoo/enterprise#60668
    
    X-original-commit: 41cb7c3652b09df56c506c1ce2d47f92e2a20059
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>
    Signed-off-by: Earth Patel (eapa) <eapa@odoo.com>

--- web_grid/static/src/views/grid_controller.js
+++ web_grid/static/src/views/grid_controller.js
@@ -75,7 +75,7 @@ export class GridController extends Component {
                 const { anchor, range } = this.model.navigationInfo;
                 return {
                     anchor,
-                    activeRangeName: range.name,
+                    activeRangeName: range?.name,
                 };
             }
         })
