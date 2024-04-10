1233fc0ae6c43f04bd2e9ecc1e3784e5c93b2971
Mahamadasif Ansari <maan@odoo.com>
2024-04-10 12:21:48 +0530

    [FIX] pos_iot: Prevent TypeError while uninstalling IoT for PoS
    Currently, an exception is generated when the user tries to uninstall
    IoT for PoS.
    
    Error:
    `TypeError: uninstall_hook() missing 1 required positional argument: 'registry'`
    
    This is because the commit [1] added the uninstall hook at 15, and the uninstall
    hook requires two arguments in 15.0. But from saas-16.3 uninstall hook require
    only one argument as 'env'; it is not changed with commit [1]'s forwarded port.
    
    This commit will fix this issue by providing the argumnet 'env' that is
    required in the uninstall hook.
    
    [1]-https://github.com/odoo/enterprise/commit/6b10cc80ea2441b5b2ab86aab52abbf7084d4319
    
    sentry-5167509820
    
    closes odoo/enterprise#60958
    
    X-original-commit: 7f61a9331627f31e2604478fbd2996034766017d
    Signed-off-by: Robin Engels (roen) <roen@odoo.com>
    Signed-off-by: ANSARI MAHAMADASIF (maan) <maan@odoo.com>

--- pos_iot/__init__.py
+++ pos_iot/__init__.py
@@ -3,7 +3,7 @@
 
 from . import models
 
-def uninstall_hook(cr, registry):
-    cr.execute("SELECT 1 FROM ir_module_module WHERE name = 'pos_enterprise' AND state = 'installed'")
-    if cr.rowcount:
-        cr.execute("UPDATE pos_config SET is_posbox = False WHERE is_posbox")
+def uninstall_hook(env):
+    env.cr.execute("SELECT 1 FROM ir_module_module WHERE name = 'pos_enterprise' AND state = 'installed'")
+    if env.cr.rowcount:
+        env.cr.execute("UPDATE pos_config SET is_posbox = False WHERE is_posbox")
