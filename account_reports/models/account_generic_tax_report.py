f09c842bc3f733d5ccaf6fe3bc8b6d190b397b3f
oco-odoo <oco@odoo.com>
2024-04-15 14:46:01 +0200

    [FIX] account_reports: skip the 'draft entries' warning for tax closings properly for all tax report
    Some specific code was added by https://github.com/odoo/enterprise/commit/649960d66eaf0c1cdf0c81b86a6226d77d244b98 in order to ignore the tax closing entries in the warning stating there are draft entries in the period, if on the tax report. However, this was badly forward-ported, and only worked for the generic tax report. With this fix, every tax report will benefit from it, as originally intended.
    
    X-original-commit: 8514d3d0b61a56faf7094f743f1358a81bbfc77c
    Part-of: odoo/enterprise#60825

--- account_reports/models/account_generic_tax_report.py
+++ account_reports/models/account_generic_tax_report.py
@@ -39,6 +39,19 @@ class AccountTaxReportHandler(models.AbstractModel):
 
         self._enable_export_buttons_for_common_vat_groups_in_branches(options)
 
+    def _custom_line_postprocessor(self, report, options, lines, warnings=None):
+        if warnings is not None:
+            if 'account_reports.common_warning_draft_in_period' in warnings:
+                # Recompute the warning 'common_warning_draft_in_period' to not include tax closing entries in the banner of unposted moves
+                if not self.env['account.move'].search_count(
+                    [('state', '=', 'draft'), ('date', '<=', options['date']['date_to']),
+                     ('tax_closing_end_date', '=', False)],
+                    limit=1,
+                ):
+                    warnings.pop('account_reports.common_warning_draft_in_period')
+
+        return lines
+
     # -------------------------------------------------------------------------
     # TAX CLOSING
     # -------------------------------------------------------------------------
@@ -498,16 +511,6 @@ class GenericTaxReportCustomHandler(models.AbstractModel):
     _description = 'Generic Tax Report Custom Handler'
 
     def _dynamic_lines_generator(self, report, options, all_column_groups_expression_totals, warnings=None):
-
-        if warnings is not None and 'account_reports.common_warning_draft_in_period' in warnings:
-            # Recompute the warning 'common_warning_draft_in_period' to not include tax closing entries in the banner of unposted moves
-            if not self.env['account.move'].search_count(
-                [('state', '=', 'draft'), ('date', '<=', options['date']['date_to']),
-                 ('tax_closing_end_date', '=', False)],
-                limit=1,
-            ):
-                warnings.pop('account_reports.common_warning_draft_in_period')
-
         return self._get_dynamic_lines(report, options, 'default')
 
     def _caret_options_initializer(self):
