dfc79a07e385142ff9749cc24b72801bb6cdf70b
Thibault Delavallée <tde@odoo.com>
2024-04-16 14:46:38 +0200

    [FIX] test_mail_*: fix query counters tests and runbot builds
    Ensure everything is flushed before running tests. Update counters. See
    community PR for more details.
    
    Task-3871642
    Runbot-55365
    Runbot-55366
    Runbot-55367
    
    closes odoo/enterprise#60844
    
    Related: odoo/odoo#162022
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- test_mail_enterprise/tests/test_activity_performance.py
+++ test_mail_enterprise/tests/test_activity_performance.py
@@ -173,7 +173,7 @@ class TestActivityPerformance(BaseMailPerformance):
         in order to see difference with other activities (generic type). """
         test_records = self.test_records_voip.with_env(self.env)
 
-        with self.assertQueryCount(employee=169):
+        with self.assertQueryCount(employee=167):
             activities = test_records.activity_schedule(
                 activity_type_id=self.generic_activity.id,
                 automated=False,
@@ -182,7 +182,7 @@ class TestActivityPerformance(BaseMailPerformance):
             self.env.flush_all()
         self.assertEqual(len(activities), 10)
 
-        with self.assertQueryCount(employee=37):
+        with self.assertQueryCount(employee=35):
             activities[:3].write({'user_id': self.user_root.id})
             activities[3:6].write({'user_id': self.env.uid})
             activities[6:].write({'user_id': self.user_admin.id})
@@ -223,7 +223,7 @@ class TestActivityPerformance(BaseMailPerformance):
         # check business information (to benefits from this test)
         self.assertEqual(activities.activity_type_id, self.phonecall_activity)
 
-        with self.assertQueryCount(employee=112):
+        with self.assertQueryCount(employee=108):
             activities[:3].write({'user_id': self.user_root.id})
             activities[3:6].write({'user_id': self.env.uid})
             activities[6:].write({'user_id': self.user_admin.id})
