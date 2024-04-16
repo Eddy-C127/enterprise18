1af646722b3e7574248cc732f024b484f44568a3
Mélanie <mepe@odoo.com>
2024-04-16 11:00:10 +0200

    [IMP] account_fleet : add link to bill in fleet
    With this commit, the bill's amount is correctly computed.
    If the service log is generated by a bill, the cost is readonly to be
    consistent.
    If the service log is generated by a bill, it can't be deleted.
    
    The bill smartbutton's color is related to the bill's state.
    
    task-3783017
    
    closes odoo/enterprise#60818
    
    Related: odoo/odoo#157245
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- account_asset_fleet/models/account_move.py
+++ account_asset_fleet/models/account_move.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import models
@@ -14,14 +13,3 @@ class AccountMove(models.Model):
             for _command, _id, line_vals in move_vals['line_ids']:
                 line_vals['vehicle_id'] = vals['asset_id'].vehicle_id.id
         return move_vals
-
-
-class AccountMoveLine(models.Model):
-    _inherit = 'account.move.line'
-
-    def _prepare_fleet_log_service(self):
-        # Overridden in order to include the non deductible tax amount.
-        val = super()._prepare_fleet_log_service()
-        quantity = self.quantity if self.account_id.multiple_assets_per_line else 1
-        val['amount'] = self.currency_id.round((self.price_subtotal + self.non_deductible_tax_value) / quantity)
-        return val
