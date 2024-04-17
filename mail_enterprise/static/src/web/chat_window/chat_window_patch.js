aa501d7b3748c378ce3d08b294f26834f41206e6
Killian Frappart <kfr@odoo.com>
2024-04-17 09:46:16 +0200

    [IMP] mail_enterprise: add breadcrumbs to Discuss
    This commit adds breadcrumbs to Discuss page, so that users can easily
    browse backwards.
    
    Task-3734245
    
    closes odoo/enterprise#60896
    
    Related: odoo/odoo#153802
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- mail_enterprise/static/src/web/chat_window/chat_window_patch.js
+++ mail_enterprise/static/src/web/chat_window/chat_window_patch.js
@@ -1,5 +1,6 @@
 import { ChatWindow } from "@mail/core/common/chat_window";
 
+import { useService } from "@web/core/utils/hooks";
 import { patch } from "@web/core/utils/patch";
 
 import { useBackButton } from "@web_mobile/js/core/hooks";
@@ -8,5 +9,6 @@ patch(ChatWindow.prototype, {
     setup() {
         super.setup();
         useBackButton(() => this.props.chatWindow.close());
+        this.homeMenuService = useService("home_menu");
     },
 });
