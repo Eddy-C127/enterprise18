5c8f2c8da7728f8db4ffc04e94bd0c71485337f6
Maxime de Neuville <mane@odoo.com>
2024-04-23 17:25:11 +0200

    [IMP] sale_planning: test avoid opening plan dialog if no shift to plan
    In this commit, we test the feature implemented in this PR: https://github.com/odoo/enterprise/pull/50383
    
    task-3874263
    
    closes odoo/enterprise#61329
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- sale_planning/static/tests/test_planning_gantt.js
+++ sale_planning/static/tests/test_planning_gantt.js
@@ -2,11 +2,12 @@
 
 import { Component, useState, xml } from "@odoo/owl";
 import { Domain } from "@web/core/domain";
-import { getFixture, mount, nextTick, patchDate, patchWithCleanup } from "@web/../tests/helpers/utils";
+import { getFixture, mount, nextTick, patchDate, patchWithCleanup, click } from "@web/../tests/helpers/utils";
 import { makeTestEnv } from "@web/../tests/helpers/mock_env";
-import { setupViewRegistries } from "@web/../tests/views/helpers";
+import { makeView, setupViewRegistries } from "@web/../tests/views/helpers";
 import { PlanningGanttRenderer } from "@planning/views/planning_gantt/planning_gantt_renderer";
 import { View } from "@web/views/view";
+import { clickCell, hoverGridCell } from "@web_gantt/../tests/helpers";
 
 
 let serverData;
@@ -21,6 +22,7 @@ QUnit.module('SalePlanning > Views > GanttView', {
                 'planning.slot': {
                     fields: {
                         id: { string: 'ID', type: 'integer' },
+                        name: { string: 'Name', type: 'char' },
                         role_id: { string: 'Role', type: 'many2one', relation: 'planning.role' },
                         sale_line_id: { string: 'Sale Order Item', type: 'many2one', relation: 'sale.order.line' },
                         resource_id: { string: 'Resource', type: 'many2one', relation: 'resource.resource' },
@@ -31,6 +33,7 @@ QUnit.module('SalePlanning > Views > GanttView', {
                     records: [
                         {
                             id: 1,
+                            name: 'Shift 1',
                             role_id: 1,
                             sale_line_id: 1,
                             resource_id: false,
@@ -58,7 +61,14 @@ QUnit.module('SalePlanning > Views > GanttView', {
                     records: [
                         { id: 1, name: 'Computer Configuration' },
                     ],
-                }
+                },
+                'resource.resource': {
+                    fields: {
+                        id: { string: 'ID', type: 'integer' },
+                        name: { string: 'Name', type: 'char' },
+                    },
+                    records: [],
+                },
             },
         };
     }
@@ -144,4 +154,43 @@ QUnit.test('Process domain for plan dialog', async function (assert) {
         renderer.getPlanDialogDomain(),
         expectedDomain.toList()
     );
+});
+
+QUnit.test("Show shift form dialog only when shifts to plan", async function (assert) {
+    assert.expect(3);
+
+    // Additionally to 'Shift 1', we create a new unplanned shift 'Shift 2' which has a sale_line_id, no start_datetime and no end_datetime values
+    serverData.models["planning.slot"].records = [serverData.models["planning.slot"].records[0], { name: "Shift 2", id: 2, role_id: 1, sale_line_id: 1 }]
+    serverData.views = {
+        "planning.slot,false,form": `<form js_class="planning_form"><field name="name"/></form>`,
+        "planning.slot,false,list": `<tree><field name="name"/></tree>`,
+    };
+
+    await makeView({
+        type: "gantt",
+        resModel: "planning.slot",
+        serverData,
+        arch: `<gantt js_class="planning_gantt" date_start="start_datetime" date_stop="end_datetime" default_scale="week"/>`,
+        mockRPC(_, args) {
+            if (args.method === "gantt_resource_work_interval") {
+                return  [
+                    { false: [["2021-10-12 08:00:00", "2022-10-12 12:00:00"]] },
+                ];
+            }
+        },
+    });
+
+    // First, we click on a cell and check that the shift form dialog opens up with the shifts to plan (as 'Shift 2' is a shift to plan)
+    await hoverGridCell(1, 4);
+    await clickCell(1, 4);
+    assert.strictEqual(target.querySelector(".modal-title").textContent, "Plan");
+    assert.strictEqual(target.querySelector(".o_data_cell").textContent, "Shift 2");
+
+    // We then plan 'Shift 2'
+    await click(target, ".o_data_cell");
+
+    // There should be no more shifts to plan. So when we click again on the cell, only the create form view of shifts should open up
+    await hoverGridCell(1, 4);
+    await clickCell(1, 4);
+    assert.strictEqual(target.querySelector(".modal-title").textContent, "Add Shift");
 });
