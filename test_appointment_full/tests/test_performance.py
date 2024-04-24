fa0d123fa44775b1daccca4220e9a6ee8d72ad45
Antoine (ande) <ande@odoo.com>
2024-04-24 17:30:59 +0000

    [FIX] {test_}appointment{_full}: remove 1-min intervals
    Current behavior:
    When looking at an appointment availability,
    slots are missing (ie: 11pm-12am)
    
    Steps to reproduce:
    1. Install website_appointment
    2. Go to Appointment
    3. Select/Create a resource appointment
    4. Make sure resources use the default calendar
    5. In schedule, change 'To' to midnight (00:00)
    6. Click on 'Go to Website'
    7. Select the next day
    8. Should be available until midnight
    9. => slots missing
    
    Cause of the issue:
    When computing unavailabilities with _unavailable_intervals_batch,
    it takes all available slots: 12am-11:59am, 12pm-11:59pm
    then finds intervals between slots: 11:59am-12pm 11:59pm-12am
    These 1-minute intervals invalidate their respective slot
    => the slots 11am-12pm and 11pm-12am will show as unavailable
    
    Fix:
    Don't consider intervals that are one minute or less as unavailable
    
    opw-3861561
    
    closes odoo/enterprise#65003
    
    X-original-commit: 6a681ec9be0a1d1a21a1328ffad5ae4554467426
    Signed-off-by: Jérémy Hennecart (jeh) <jeh@odoo.com>

--- test_appointment_full/tests/test_performance.py
+++ test_appointment_full/tests/test_performance.py
@@ -87,7 +87,7 @@ class OnelineWAppointmentPerformance(AppointmentUIPerformanceCase, AppointmenHrP
         t0 = time.time()
         with freeze_time(self.reference_now):
             self.authenticate(None, None)
-            with self.assertQueryCount(default=32):
+            with self.assertQueryCount(default=34):
                 self._test_url_open('/appointment/%i' % self.test_apt_type_resource.id)
         t1 = time.time()
         _logger.info('Browsed /appointment/%i, time %.3f', self.test_apt_type_resource.id, t1 - t0)
@@ -99,7 +99,7 @@ class OnelineWAppointmentPerformance(AppointmentUIPerformanceCase, AppointmenHrP
         t0 = time.time()
         with freeze_time(self.reference_now):
             self.authenticate('staff_user_bxls', 'staff_user_bxls')
-            with self.assertQueryCount(default=43):  # apt only: 35 / +1 for no-demo
+            with self.assertQueryCount(default=44):  # apt only: 36 / +1 for no-demo
                 self._test_url_open('/appointment/%i' % self.test_apt_type_resource.id)
         t1 = time.time()
 
