ddf597b0e58e23cf9a8321bce50ee5cb0a47fa18
alsh-odoo <alsh@odoo.com>
2024-04-25 15:28:53 +0530

    [FIX] planning: fix traceback when click on employee avatar
    After the changes in the related community PR,
    Commit [2] is no longer needed as that use case in [2] will
    also resolved in the community PR mentioned below
    
    Community PR:- https://github.com/odoo/odoo/pull/163305
    
    [2]
    https://github.com/odoo/enterprise/pull/58964/commits/7845ff85cad0714a95866f9bfcc9d717960ecf3c
    
    sentry-3954475523
    
    closes odoo/enterprise#61592
    
    X-original-commit: 25611f86d9fb102f8d5042b66d4cdbfe6230abdb
    Related: odoo/odoo#163610
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>
    Signed-off-by: Altaf Shaik (alsh) <alsh@odoo.com>

--- planning/static/src/components/avatar_card_resource/avatar_card_resource_popover_patch.js
+++ planning/static/src/components/avatar_card_resource/avatar_card_resource_popover_patch.js
@@ -26,13 +26,11 @@ export const patchAvatarCardResourcePopover = {
     },
     get fieldNames() {
         const additionalFields = ["role_ids"];
-        const excludedFields = ["employee_ids"];
         if (this.hr_access) {
             additionalFields.push("default_role_id");
         }
-        const fields = super.fieldNames.filter((field) => !excludedFields.includes(field));
         return [
-            ...fields,
+            ...super.fieldNames,
             ...additionalFields,
         ];
     },
