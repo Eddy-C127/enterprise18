12806847267334b550df831705f07090851ec90b
agrabsi <angr@odoo.com>
2024-04-29 08:33:45 +0000

    [FIX] documents_fsm: post_init empty folder check
    Previous implementation checked if a folder was empty before removing it. However, it didn't take into account cases where one folder is empty but there are non-empty subfolders inside. Removing the parent folder triggered an error.
    
    The new implementation checks the subfolders to ensure their emptiness before trying to remove the parent folder.
    
    closes odoo/enterprise#64983
    
    X-original-commit: 3019682d5ece91d503bd46dfc553662f59e7f883
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- documents_fsm/__init__.py
+++ documents_fsm/__init__.py
@@ -6,21 +6,31 @@ from . import models
 
 def _documents_fsm_post_init(env):
     fsm_projects = env["project.project"].search([("is_fsm", "=", True), ("use_documents", "=", True)])
-    read_group_var = (
-        env["documents.document"]
-        .with_context(active_test=False)
-        ._read_group(
-            [("folder_id", "in", fsm_projects.documents_folder_id.ids)],
-            ["folder_id"],
-            ["__count"],
+
+    # Search for folders that are descendants of fsm_projects folders and have documents
+    subfolders_with_documents = env["documents.folder"].search([
+        ("id", "child_of", fsm_projects.documents_folder_id.ids),
+        ("document_ids", "!=", False),
+    ])
+
+    folders_with_non_empty_subfolders = {
+        folder.id
+        for folder in fsm_projects.documents_folder_id
+        if any(
+            subfolder.parent_path.startswith(folder.parent_path)
+            for subfolder in subfolders_with_documents
         )
-    )
-    workspaces_doc_count = {folder.id: count for folder, count in read_group_var}
+    }
+
     for project in fsm_projects:
         if project.document_count == 0:
             project.use_documents = False
 
-            if workspaces_doc_count.get(project.documents_folder_id.id, 0) == 0:
+            project_folder = project.documents_folder_id
+            if (
+                project_folder.document_count == 0
+                and project_folder.id not in folders_with_non_empty_subfolders
+            ):
                 project.documents_folder_id.unlink()
             else:
                 project.documents_folder_id = False
