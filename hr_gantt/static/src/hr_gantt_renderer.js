4134ed00f6fac5252c87bc6a60921eecc3ae784f
Levi Siuzdak (sile) <sile@odoo.com>
2024-05-02 11:31:04 +0200

    [FIX] hr_{gantt,work_entry_contract_enterprise}: split shift pills
    Versions
    --------
    - 17.0+
    
    Steps
    -----
    1. Go to an employee w/ a running contract;
    2. open their working schedule;
    3. change lunch breaks from the default 12:00 to 12:30;
    4. go to the Payroll app;
    5. check the work entries of the employee.
    
    Issue
    -----
    Each work entry has two rows:
    1. first half of working day, pill takes up the entire column;
    2. second half of working day, pill takes up half a column.
    
    Cause
    -----
    Commit 19c957264ed reworked how gantt pills get rounded, making it so
    that a if a record extends on a day/half-day/hour/month for even a
    minute, the pill includes it.
    
    Because the half-day is hardcoded to 12:00, this means that a lunch break
    that's one minute later than that will display the first shift as a full
    day in work entry gantt view.
    
    Solution
    --------
    Partially re-introduce the old pill rounding logic for this specific
    view/module. Add it to the hr_gantt module so the same visual issue gets
    resolved in hr_attendance_gantt views.
    
    opw-3895160
    
    closes odoo/enterprise#62390
    
    Signed-off-by: Levi Siuzdak <sile@odoo.com>

--- hr_gantt/static/src/hr_gantt_renderer.js
+++ hr_gantt/static/src/hr_gantt_renderer.js
@@ -3,6 +3,8 @@
 import { Avatar } from "@mail/views/web/fields/avatar/avatar";
 import { GanttRenderer } from "@web_gantt/gantt_renderer";
 
+const { DateTime } = luxon;
+
 export class HrGanttRenderer extends GanttRenderer {
     computeDerivedParams() {
         this.rowsWithAvatar = {};
@@ -27,6 +29,79 @@ export class HrGanttRenderer extends GanttRenderer {
         }
         return super.processRow(...arguments);
     }
+
+    /**
+     * Returns the colmn index closest to the given date and their diff in hours
+     * @param {DateTime} date
+     * @returns {[number, number]}
+     */
+    closestDateGridColumn(date) {
+        const dates = this.dateGridColumns;
+        let [left, right] = [0, dates.length - 1];
+        let mid, diff;
+        while (left <= right) {
+            mid = Math.floor((left + right) / 2);
+            diff = date.ts - dates[mid].ts;
+            if (Math.abs(diff) >= Math.abs(date.ts - dates[mid - 1]?.ts)) {
+                right = mid - 1;
+            } else if (Math.abs(diff) > Math.abs(date.ts - dates[mid + 1]?.ts)) {
+                left = mid + 1;
+            } else {
+                break;
+            }
+        }
+        return [mid, diff / 3.6e+6];
+    }
+
+    /**
+     * @param {RelationalRecord} record
+     * @returns {Partial<Pill>}
+     */
+    getPill(record) {
+        const pill = super.getPill(record);
+        switch (this.model.metaData.scale.id) {
+            case "week":
+            case "month": {
+                const {
+                    dateStartField,
+                    dateStopField,
+                    startDate,
+                    stopDate,
+                } = this.model.metaData;
+                const pillStartDate = DateTime.max(startDate, record[dateStartField]);
+                const pillStopDate = DateTime.min(stopDate, record[dateStopField]);
+
+                let [startIndex, diffStart] = this.closestDateGridColumn(pillStartDate);
+                let [stopIndex, diffStop] = this.closestDateGridColumn(pillStopDate);
+
+                if (startIndex === stopIndex) {
+                    if (diffStart < -2 && diffStop > 2) {
+                        startIndex -= 1;
+                        stopIndex += 1;
+                    } else if (diffStart < 0) {
+                        startIndex -= 1;
+                    } else {
+                        stopIndex += 1;
+                    }
+                } else {
+                    if (diffStart < -2) {
+                        startIndex -= 1;
+                    }
+                    if (diffStop > 2) {
+                        stopIndex += 1;
+                    }
+                }
+
+                const firstCol = startIndex + 1;
+                const span = stopIndex - startIndex;
+                pill.grid = { column: [firstCol, span] };
+                pill.startDate = this.dateGridColumns[startIndex];
+                pill.stopDate = this.dateGridColumns[stopIndex];
+            }
+        }
+        return pill;
+    }
 }
+
 HrGanttRenderer.rowHeaderTemplate = "hr.HrGanttRenderer.RowHeader";
 HrGanttRenderer.components = { ...GanttRenderer.components, Avatar };
--- hr_work_entry_contract_enterprise/static/src/work_entries_gantt_view.js
+++ hr_work_entry_contract_enterprise/static/src/work_entries_gantt_view.js
@@ -8,10 +8,24 @@ import { WorkEntriesGanttModel } from "./work_entries_gantt_model";
 const viewRegistry = registry.category("views");
 
 export const workEntriesGanttView = {
-    ...ganttView,
+    ...(viewRegistry.content.hr_gantt?.[1] ?? ganttView),
     Controller: WorkEntriesGanttController,
     Model: WorkEntriesGanttModel,
     buttonTemplate: "hr_work_entry_contract_enterprise.WorkEntriesGanttView.Buttons",
 };
 
 viewRegistry.add("work_entries_gantt", workEntriesGanttView);
+
+// hr_gantt should normally be installed unless manually removed,
+// so "stable" workaround to leave dependencies unchanged
+function updateWorkEntriesGanttView(evt) {
+    const { operation, key, value } = evt.detail;
+    if (key === "hr_gantt" && operation === "add") {
+        Object.assign(workEntriesGanttView, value);
+        viewRegistry.removeEventListener("UPDATE", updateWorkEntriesGanttView);
+    }
+}
+
+if (!viewRegistry.content.hr_gantt) {
+    viewRegistry.addEventListener("UPDATE", updateWorkEntriesGanttView);
+}
