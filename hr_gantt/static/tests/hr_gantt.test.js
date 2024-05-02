70627aedee8a43a1359b1e2e70ac467314f99f74
Levi Siuzdak (sile) <sile@odoo.com>
2024-05-02 09:31:04 +0000

    [FIX] hr_{gantt,work_entry_contract_enterprise}: split shift pills
    Versions
    --------
    - 17.0+
    
    Steps
    -----
    1. Go to an employee w/ a running contract;
    2. open their working schedule;
    3. change lunch breaks from the default 12:00 to 12:30;
    4. go to the Payroll app;
    5. check the work entries of the employee.
    
    Issue
    -----
    Each work entry has two rows:
    1. first half of working day, pill takes up the entire column;
    2. second half of working day, pill takes up half a column.
    
    Cause
    -----
    Commit 19c957264ed reworked how gantt pills get rounded, making it so
    that a if a record extends on a day/half-day/hour/month for even a
    minute, the pill includes it.
    
    Because the half-day is hardcoded to 12:00, this means that a lunch break
    that's one minute later than that will display the first shift as a full
    day in work entry gantt view.
    
    Solution
    --------
    Partially re-introduce the old pill rounding logic for this specific
    view/module. Add it to the hr_gantt module so the same visual issue gets
    resolved in hr_attendance_gantt views.
    
    For versions 17.3+:
    Because of the changes to gantt columns introduced by
    d18639785e622d97102227103d62ddb3a2716be6, the old pill rounding logic
    was no longer feasible, and new logic was introduced to halve pills
    spanning 2 half-days if they start after 11:00 or end before 14:00.
    
    opw-3895160
    
    closes odoo/enterprise#64215
    
    X-original-commit: 2240c28a664674014eb3fedb9c19caa0043f3beb
    Signed-off-by: Levi Siuzdak <sile@odoo.com>

--- hr_gantt/static/tests/hr_gantt.test.js
+++ hr_gantt/static/tests/hr_gantt.test.js
@@ -1,5 +1,6 @@
 import { defineMailModels } from "@mail/../tests/mail_test_helpers";
 import { beforeEach, describe, expect, test } from "@odoo/hoot";
+import { queryAll } from "@odoo/hoot-dom";
 import { mockDate } from "@odoo/hoot-mock";
 import { defineModels, fields, models } from "@web/../tests/web_test_helpers";
 import { mountGanttView } from "@web_gantt/../tests/web_gantt_test_helpers";
@@ -55,6 +56,22 @@ class Task extends models.Model {
             employee_id: 1,
             foo: "Foo 3",
         },
+        {
+            id: 5,
+            display_name: "Task 5",
+            start: "2018-12-21 10:30:00", // 11:30:00 CET
+            stop: "2018-12-21 11:29:59",  // 12:29:59 CET
+            employee_id: 1,
+            foo: "Foo 3",
+        },
+        {
+            id: 6,
+            display_name: "Task 6",
+            start: "2018-12-22 10:30:00", // 11:30:00 CET
+            stop: "2018-12-22 11:59:59",  // 12:59:59 CET
+            employee_id: 1,
+            foo: "Foo 3",
+        },
     ];
 }
 
@@ -87,4 +104,28 @@ test("hr gantt view grouped by foo > employee", async () => {
     expect(".o_gantt_row_header:not(.o_gantt_group) .o_gantt_row_title .o-mail-Avatar").toHaveCount(
         4
     );
+});
+
+test("hr gantt view pill rounding", async () => {
+    expect.assertions(7);
+    await mountGanttView({ resModel: "task", arch });
+    const [task4, task5, task6] = queryAll(".o_gantt_pill_wrapper.o_draggable").reduce(
+        (acc, node) => ["task,4", "task,5", "task,6"].includes(node.innerText)
+            ? acc.concat(node.getBoundingClientRect())
+            : acc,
+        [],
+    );
+    const [col19, col20, col21, col22] = queryAll(".o_gantt_header_cell").reduce(
+        (acc, node) => "19" <= node.innerText && node.innerText <= "22"
+            ? acc.concat(node.getBoundingClientRect())
+            : acc,
+        [],
+    );
+    expect(task4.width).toBe(col19.width, { message: "Task 4 should take up 2 half-days" });
+    expect(task5.width).toBe(col21.width / 2, { message: "Task 5 should take up 1 half-day" });
+    expect(task6.width).toBe(col22.width / 2, { message: "Task 6 should take up 1 half-day" });
+    expect(task4.left).toBeWithin(col19.x, col20.x, { message: "Task 4 should start the 19th" });
+    expect(task4.right).toBeWithin(col20.x, col21.x, { message: "Task 4 should end the 20th" });
+    expect(task5.left).toBe(col21.left, { message: "Task 5 should display before noon" });
+    expect(task6.right).toBe(col22.right, { message: "Task 6 should display after noon" });
 });
--- hr_work_entry_contract_enterprise/static/src/work_entries_gantt_view.js
+++ hr_work_entry_contract_enterprise/static/src/work_entries_gantt_view.js
@@ -1,4 +1,4 @@
-import { ganttView } from "@web_gantt/gantt_view";
+import { hrGanttView } from "@hr_gantt/hr_gantt_view";
 import { registry } from "@web/core/registry";
 import { WorkEntriesGanttController } from "./work_entries_gantt_controller";
 import { WorkEntriesGanttModel } from "./work_entries_gantt_model";
@@ -6,7 +6,7 @@ import { WorkEntriesGanttModel } from "./work_entries_gantt_model";
 const viewRegistry = registry.category("views");
 
 export const workEntriesGanttView = {
-    ...ganttView,
+    ...hrGanttView,
     Controller: WorkEntriesGanttController,
     Model: WorkEntriesGanttModel,
     buttonTemplate: "hr_work_entry_contract_enterprise.WorkEntriesGanttView.Buttons",
