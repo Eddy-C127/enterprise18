3317aba27eb9dcd9bcc45ea05cc5ba20d22fb931
Flotchet <mafl@odoo.com>
2024-05-03 15:10:50 +0200

    [FIX] l10n_ke_hr_payroll: NSSF number format
    the nssf number restriction should not be apply
    given the real world scenario where the number
    can be in other format (eg 000000000x)
    
    task-3908312
    
    closes odoo/enterprise#61947
    
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- l10n_ke_hr_payroll/models/res_company.py
+++ l10n_ke_hr_payroll/models/res_company.py
@@ -1,7 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError
+from odoo import fields, models
 
 
 class ResCompany(models.Model):
@@ -9,10 +8,3 @@ class ResCompany(models.Model):
 
     l10n_ke_nssf_number = fields.Char(string="NSSF Number", help="NSSF Number provided by the NSSF")
     l10n_ke_kra_pin = fields.Char(string="KRA PIN", help="KRA PIN provided by the KRA")
-
-    @api.constrains('l10n_ke_nssf_number', 'country_id')
-    def _check_l10n_ke_nssf_number(self):
-        for company in self:
-            number = company.l10n_ke_nssf_number
-            if number and (not number.isdigit() or len(number) > 10 or len(number) < 9):
-                raise UserError(_('The NSSF number must be a nine or ten digits number.'))
