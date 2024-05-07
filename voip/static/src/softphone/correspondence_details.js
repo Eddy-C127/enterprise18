439b14957cf4282b18fcf1ed959a5467ada90a17
Louis Wicket (wil) <wil@odoo.com>
2024-05-07 13:23:04 +0200

    [FIX] voip: open record from correspondence details
    Since the complete refactoring of the module in 17, the "open record"
    button of the CorrespondenceDetails no longer works: Clicking it causes
    the softphone to fold, but nothing more happens (the record is not
    displayed as expected).
    
    This is because we started retrieving the ORM service from the
    useService hook, whereas before we used to retrieve it from the env.
    useService untransparently wraps the ORM call in a "_protectMethod",
    which drops the return value of an RPC if the component that made it has
    been destroyed in the meantime. Since clicking the "open record" button
    also folds the softphone, it destroyed the Correspondence Details
    component, preventing it from ever receiving the RPC's return value.
    
    This commit changes the way the ORM service is retrieved in order not to
    use useService and to solve the problem.
    
    opw-3904519
    
    closes odoo/enterprise#62099
    
    Signed-off-by: Louis Wicket (wil) <wil@odoo.com>

--- voip/static/src/softphone/correspondence_details.js
+++ voip/static/src/softphone/correspondence_details.js
@@ -21,7 +21,7 @@ export class CorrespondenceDetails extends Component {
         this.activityService = useService("mail.activity");
         this.callService = useService("voip.call");
         this.dialog = useService("dialog");
-        this.orm = useService("orm");
+        this.orm = this.env.services.orm;
         this.userAgent = useState(useService("voip.user_agent"));
         this.voip = useService("voip");
         this.softphone = this.voip.softphone;
--- voip_crm/__manifest__.py
+++ voip_crm/__manifest__.py
@@ -1,5 +1,3 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 {
     "name": "VoIP for CRM",
     "summary": "VoIP integration with CRM module.",
@@ -10,4 +8,13 @@
     "auto_install": True,
     "data": ["views/crm_lead_views.xml"],
     "license": "OEEL-1",
+    "assets": {
+        "web.tests_assets": [
+            "voip_crm/static/tests/helpers/**/*.js",
+        ],
+        "web.qunit_suite_tests": [
+            "voip_crm/static/tests/**/*.js",
+            ("remove", "voip_crm/static/tests/helpers/**/*"),
+        ],
+    },
 }
