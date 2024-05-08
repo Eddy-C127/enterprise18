2ff59e2912c725517f27fc4ea19f3eb9ca7360ce
Ricardo Gomes Rodrigues <rigr@odoo.com>
2024-05-08 16:57:50 +0200

    [FIX] l10n_be_codabox: avoid override of accounting firm in demo
    Previous to this commit, in demo, when installing CodaBox, if a Belgian
    company already had an accounting firm set, it was overriden by a new one.
    
    This commit fixes this by checking if the Belgian company already has
    an accounting firm set. If so, we do not modify it.
    
    closes odoo/enterprise#60822
    
    Task-id: none
    Signed-off-by: John Laterre (jol) <jol@odoo.com>

--- l10n_be_codabox/__init__.py
+++ l10n_be_codabox/__init__.py
@@ -5,23 +5,22 @@ from odoo.exceptions import UserError
 
 
 def _l10n_be_codabox_pre_init_hook(env):
-    domain = [
+    companies = env['res.company'].search([
         ('partner_id.country_id.code', '=', 'BE'),
         ('vat', '!=', False),
-    ]
+    ])
     # The field is defined in account_reports module which this module does not depend on in 17.0
     if "account_representative_id" in env['res.company']._fields:
         # If we are in a demo db, create a demo accounting firm.
-        if bool(env['ir.module.module'].search_count([('demo', '=', True)])):
+        if bool(env['ir.module.module'].search_count([('demo', '=', True)])) and all(not company.account_representative_id.vat for company in companies):
             accounting_firm = env['res.partner'].create({
                 'name': 'Demo Accounting Firm',
                 'vat': 'BE0428759497',
                 'country_id': env.ref('base.be').id,
             })
-            env['res.company'].search(domain).write({
+            companies.write({
                 'account_representative_id': accounting_firm.id,
             })
-        domain.append(('account_representative_id.vat', '!=', False))
-    companies = env['res.company'].search(domain)
+        companies = companies.filtered(lambda c: c.account_representative_id.vat)
     if not companies:
         raise UserError(_lt("The CodaBox module must be installed and configured by an Accounting Firm."))
