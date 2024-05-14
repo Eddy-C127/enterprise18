c714cfeab86fc1a5a1850c2242163a7503806926
Flotchet <mafl@odoo.com>
2024-05-14 16:18:28 +0200

    [FIX] documents_hr_contract: ondele
    The ondelete was not in the right model.
    
    task-3928257
    
    closes odoo/enterprise#62466
    
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- documents_hr_contract/models/hr_contract.py
+++ documents_hr_contract/models/hr_contract.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, api, _
-from odoo.exceptions import UserError
+from odoo import api, models
 
 
 class HrContract(models.Model):
@@ -30,6 +28,5 @@ class HrContract(models.Model):
 
     @api.ondelete(at_uninstall=False)
     def _unlink_except_contract_signature_tag(self):
-        tag = self.env.ref('documents_hr_contract.document_tag_signature_request', raise_if_not_found=False)
-        if tag and tag in self:
-            raise UserError(_('You cannot delete this tag as it is used to link employee contracts and signatures.'))
+        # TODO: remove me in master
+        return
