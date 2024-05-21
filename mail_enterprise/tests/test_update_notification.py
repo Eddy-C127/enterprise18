460107cf076b43e89d9ebdbbad7bf47c1fca7be1
Thibault Francois <tfr@odoo.com>
2024-05-21 18:34:10 +0200

    [FIX] mail_enterprise: make store cloc test more resillient
    Since the commit https://github.com/odoo/enterprise/pull/52348/commits/5c11c94b972d1993de4147d96fc3faaaaddc7c57
    a test has been added that check the cloc result it stored on a ICP.
    
    But the result can be a bit different then expected during custom build
    if custom module are installed prior mail_enterprise.
    
    Solution: Check only the value of odoo/studio module that give the
    custom line in database that should be always empty during installation
    
    closes odoo/enterprise#63154
    
    X-original-commit: e337343594123bb1180114b3aca626648e518b0b
    Signed-off-by: Pierre Masereel (pim) <pim@odoo.com>

--- mail_enterprise/tests/test_update_notification.py
+++ mail_enterprise/tests/test_update_notification.py
@@ -1,6 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo.addons.base.tests.test_cloc import TestClocCustomization
+from ast import literal_eval
 
 
 class TestClocICP(TestClocCustomization):
@@ -10,4 +11,4 @@ class TestClocICP(TestClocCustomization):
         message = self.env["publisher_warranty.contract"]._get_message()
         self.assertTrue('maintenance' in message)
         store_cloc = self.env["ir.config_parameter"].get_param('publisher_warranty.cloc')
-        self.assertEqual(store_cloc, "{'version': 1, 'modules': {'odoo/studio': 1}}")
+        self.assertEqual(literal_eval(store_cloc)['modules']['odoo/studio'], 1)
