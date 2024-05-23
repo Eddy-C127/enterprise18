543021ccb1c2470de2ca0355b9c6a55e0c591e12
Levi Siuzdak (sile) <sile@odoo.com>
2024-05-23 11:57:34 +0000

    [FIX] helpdesk_timesheet: don't force UTC time on timesheets
    Versions
    --------
    - 15.0e+
    
    Steps
    -----
    1. If current UTC time is before 11, set user tz to Pacific/Niue;
    2. if current UTC time is after 11, set user tz to Pacific/Kiritimati;
    3. go to Helpdesk;
    4. enable timesheets for a helpdesk team;
    5. open a ticket of that helpdesk team;
    6. start timer;
    7. stop timer;
    8. create timesheet.
    
    Issue
    -----
    The date of the timesheet is set to the UTC date instead of the user
    date, which is one day off.
    
    Cause
    -----
    The wizard creating timesheets adds a `Datetime.now()` value to `date`,
    which defaults to UTC time.
    
    Solution
    --------
    Remove this line, allowing timesheets to be created with the default
    value, which does get adjusted for user timezone.
    
    opw-3834908
    
    closes odoo/enterprise#64059
    
    X-original-commit: 92fa312e86fdda56d82be04d881fd89013124b57
    Signed-off-by: Levi Siuzdak <sile@odoo.com>
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- helpdesk_timesheet/tests/test_timesheet.py
+++ helpdesk_timesheet/tests/test_timesheet.py
@@ -1,5 +1,7 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from datetime import date
+from freezegun import freeze_time
 
 from odoo.tests import tagged, Form
 from odoo.exceptions import ValidationError
@@ -21,11 +23,7 @@ class TestTimesheet(TestHelpdeskTimesheetCommon):
             2) Create timesheet with this ticket and task and check if an exception is raise.
         """
         # 1) Create ticket and a task
-        ticket = self.env['helpdesk.ticket'].create({
-            'name': 'Test Ticket',
-            'team_id': self.helpdesk_team.id,
-            'partner_id': self.partner.id,
-        })
+        ticket = self.helpdesk_ticket
         task = self.env['project.task'].create({
             'name': 'Test Task',
             'project_id': self.project.id,
@@ -47,11 +45,7 @@ class TestTimesheet(TestHelpdeskTimesheetCommon):
             'name': 'Customer ticket',
             'email': 'customer@ticket.com',
         })
-        helpdesk_ticket = self.env['helpdesk.ticket'].create({
-            'name': 'Test Ticket',
-            'team_id': self.helpdesk_team.id,
-            'partner_id': self.partner.id,
-        })
+        helpdesk_ticket = self.helpdesk_ticket
         timesheet_entry = self.env['account.analytic.line'].create({
             'name': 'the only timesheet. So lonely...',
             'helpdesk_ticket_id': helpdesk_ticket.id,
@@ -65,6 +59,37 @@ class TestTimesheet(TestHelpdeskTimesheetCommon):
 
         self.assertEqual(timesheet_entry.partner_id, partner2, "The timesheet entry's partner should still be equal to the ticket's partner/customer, after the change")
 
+    def test_helpdesk_timesheet_wizard_timezones(self):
+        user = self.user_employee
+        wizard = self.env['helpdesk.ticket.create.timesheet'].with_user(user).create({
+            'description': 'Create timesheet wizard',
+            'ticket_id': self.helpdesk_ticket.id,
+            'time_spent': 1.0,
+        })
+        timezones = [
+            'Pacific/Niue',        # UTC-11,
+            'Europe/Brussels',     # UTC+1
+            'Pacific/Kiritimati',  # UTC+14
+        ]
+        test_cases = {
+            '2024-01-24 08:30': (-1, +0, +0),
+            '2024-01-24 10:30': (-1, +0, +1),
+            '2024-01-24 16:30': (+0, +0, +1),
+        }
+
+        for utc_time, day_diffs in test_cases.items():
+            with freeze_time(utc_time):
+                day = date.today().day
+                expected = (date(2024, 1, day + diff) for diff in day_diffs)
+                for tz, local_date in zip(timezones, expected):
+                    user.tz = tz
+                    timesheet = wizard.action_generate_timesheet()
+                    self.assertEqual(
+                        timesheet.date,
+                        local_date,
+                        f"{utc_time} UTC should be {local_date} in {tz} time",
+                    )
+
     def test_log_timesheet_with_ticket_analytic_account(self):
         """ Test whether the analytic account of the project is set on the ticket.
 
@@ -74,11 +99,7 @@ class TestTimesheet(TestHelpdeskTimesheetCommon):
                 2) Check the default analytic account of the project and ticket
         """
 
-        helpdesk_ticket = self.env['helpdesk.ticket'].create({
-            'name': 'Test Ticket',
-            'team_id': self.helpdesk_team.id,
-            'partner_id': self.partner.id,
-        })
+        helpdesk_ticket = self.helpdesk_ticket
 
         self.assertEqual(helpdesk_ticket.analytic_account_id, self.project.analytic_account_id)
 
