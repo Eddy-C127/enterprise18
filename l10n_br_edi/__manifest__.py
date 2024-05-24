63974744a6bd4bca68e257ed2f4dc877dff030a3
Joren Van Onder <jov@odoo.com>
2024-05-24 11:28:30 -0700

    [IMP] l10n_br_edi: mark Odoo invoices as non-legal ones with watermark
    The official invoice will be retrieved via Avatax. Adding the official
    PDF in the portal is a bit complicated, so we instead we add a
    watermark to the standard Odoo one. We also add a message in the
    portal (/my/invoices/...) to inform the user that the official
    documents can be downloaded by clicking the "Download" button on the
    left.
    
    task-3719527
    
    closes odoo/enterprise#63946
    
    Signed-off-by: Josse Colpaert <jco@odoo.com>

--- l10n_br_edi/__manifest__.py
+++ l10n_br_edi/__manifest__.py
@@ -16,6 +16,8 @@ Provides electronic invoicing for Brazil through Avatax.
         "security/ir.model.access.csv",
         "views/account_move_view.xml",
         "views/res_country_views.xml",
+        "views/report_invoice.xml",
+        "views/account_portal_views.xml",
         "wizard/account_move_send_views.xml",
         "wizard/l10n_br_edi_invoice_update_views.xml",
         "wizard/l10n_br_edi_cancel_range_views.xml",
