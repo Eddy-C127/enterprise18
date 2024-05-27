a0b1f8c4cc74df96785a1b994a95e1f70e17a6ee
Yaroslav Soroko (yaso) <yaso@odoo.com>
2024-05-27 08:16:53 +0000

    [FIX] iot: OSError ctypes calls protection
    On Windows OS if a ctypes method causes a segmentation fault,
    it leads to an OSError instead of a crash like it does on a raspberry
    pi.
    
    Related Python ctypes documentation link:
    https://docs.python.org/3/library/ctypes.html#calling-functions
    
    We should protect every call of ctypes methods to easily catch such an
    error and log it
    
    task-3950652
    
    closes odoo/enterprise#64823
    
    X-original-commit: a3e60e3f6f66d31d0662207f607b7651800fbb07
    Signed-off-by: Quentin Lejeune (qle) <qle@odoo.com>
    Signed-off-by: Yaroslav Soroko (yaso) <yaso@odoo.com>

--- pos_iot_six/iot_handlers/drivers/SixDriver.py
+++ pos_iot_six/iot_handlers/drivers/SixDriver.py
@@ -67,50 +67,65 @@ class SixDriver(CtypesTerminalDriver):
 
         _logger.critical('SixDriver.processTransaction: %s', transaction)
         # Transaction
-        result = TIMAPI.six_perform_transaction(
-            ctypes.cast(self.dev, ctypes.c_void_p), # t_terminal_manager *terminal_manager
-            transaction['posId'].encode(), # char *pos_id
-            ctypes.c_int(transaction['userId']), # int user_id
-            ctypes.c_int(1) if transaction['transactionType'] == 'Payment' else ctypes.c_int(2), # int transaction_type
-            ctypes.c_int(transaction['amount']), # int amount
-            transaction['currency'].encode(), # char *currency_str
-            transaction_id, # char *transaction_id
-            ctypes_int_buffer_size,  # int transaction_id_size
-            merchant_receipt, # char *merchant_receipt
-            customer_receipt, # char *customer_receipt
-            ctypes_int_buffer_size,  # int receipt_size
-            card, # char *card
-            ctypes_int_buffer_size,  # int card_size
-            ctypes.byref(error_code), # int *error_code
-            error, # char *error
-            ctypes_int_buffer_size  # int error_size
-        )
-
-        # Transaction successful
-        if result == 1:
+        try:
+            result = TIMAPI.six_perform_transaction(
+                ctypes.cast(self.dev, ctypes.c_void_p),  # t_terminal_manager *terminal_manager
+                transaction['posId'].encode(),  # char *pos_id
+                ctypes.c_int(transaction['userId']),  # int user_id
+                ctypes.c_int(1) if transaction['transactionType'] == 'Payment' else ctypes.c_int(2),  # int transaction_type
+                ctypes.c_int(transaction['amount']),  # int amount
+                transaction['currency'].encode(),  # char *currency_str
+                transaction_id,  # char *transaction_id
+                ctypes_int_buffer_size,  # int transaction_id_size
+                merchant_receipt,  # char *merchant_receipt
+                customer_receipt,  # char *customer_receipt
+                ctypes_int_buffer_size,   # int receipt_size
+                card,  # char *card
+                ctypes_int_buffer_size,  # int card_size
+                ctypes.byref(error_code),  # int *error_code
+                error,  # char *error
+                ctypes_int_buffer_size  # int error_size
+            )
+            # Transaction successful
+            if result == 1:
+                self.send_status(
+                    response='Approved',
+                    ticket=customer_receipt.value,
+                    ticket_merchant=merchant_receipt.value,
+                    card=card.value,
+                    transaction_id=transaction_id.value,
+                    request_data=transaction,
+                )
+            # Transaction failed
+            elif result == 0:
+                # If cancelled by Odoo Pos
+                if error_code.value == CANCELLED_BY_POS:
+                    sleep(3)  # Wait a couple of seconds between cancel requests as per documentation
+                    self.send_status(stage='Cancel', request_data=transaction)
+                # If an error was encountered
+                else:
+                    error_message = f"{error_code.value}: {error.value.decode()}"
+                    self.send_status(error=error_message, request_data=transaction)
+            # Terminal disconnected
+            elif result == -1:
+                self.send_status(disconnected=True)
+        except OSError:
+            _logger.exception("Failed to perform Six transaction. Check for potential segmentation faults")
+            sleep(3)  # needed to space out transaction requests
             self.send_status(
-                response='Approved',
-                ticket=customer_receipt.value,
-                ticket_merchant=merchant_receipt.value,
-                card=card.value,
-                transaction_id=transaction_id.value,
+                error="An error has occured. Check the transaction result manually with the payment provider",
                 request_data=transaction,
             )
-        # Transaction failed
-        elif result == 0:
-            # If cancelled by Odoo Pos
-            if error_code.value == CANCELLED_BY_POS:
-                sleep(3) # Wait a couple of seconds between cancel requests as per documentation
-                self.send_status(stage='Cancel', request_data=transaction)
-            # If an error was encountered
-            else:
-                error_message = f"{error_code.value}: {error.value.decode()}"
-                self.send_status(error=error_message, request_data=transaction)
-        # Terminal disconnected
-        elif result == -1:
-            self.send_status(disconnected=True)
 
     def cancelTransaction(self, transaction):
         self.send_status(stage='waitingCancel', request_data=transaction)
-        if not TIMAPI.six_cancel_transaction(ctypes.cast(self.dev, ctypes.c_void_p)):
-            self.send_status(stage='Cancel', error='Transaction could not be cancelled', request_data=transaction)
+        try:
+            if not TIMAPI.six_cancel_transaction(ctypes.cast(self.dev, ctypes.c_void_p)):
+                self.send_status(stage='Cancel', error='Transaction could not be cancelled', request_data=transaction)
+        except OSError:
+            _logger.exception("Failed to cancel Six transaction. Check for potential segmentation faults.")
+            sleep(3)  # needed to space out cancellation requests
+            self.send_status(
+                error="An error has occured when cancelling Six transaction. Check the transaction result manually with the payment provider",
+                request_data=transaction,
+            )
