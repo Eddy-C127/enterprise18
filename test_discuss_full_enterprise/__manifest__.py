bbf44448b3076b85aff9a25c6c9fede05b6a4eea
Sébastien Theys <seb@odoo.com>
2024-05-28 17:39:42 +0200

    [FIX] test_discuss_full_enterprise: add auto install
    When `test_discuss_full` and at least `web_enterprise` are installed,
    `test_discuss_full_enterprise` must be installed for the tests in
    `test_discuss_full` to pass.
    
    runbot-65732
    
    closes odoo/enterprise#63401
    
    X-original-commit: 422594a235a59664cb2530d1d40bd13c8c8d5ef7
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- test_discuss_full_enterprise/__manifest__.py
+++ test_discuss_full_enterprise/__manifest__.py
@@ -4,6 +4,7 @@
     'name': 'Test Discuss Full Enterprise',
     'category': 'Hidden',
     'summary': 'Test Suite for Discuss Enterprise',
+    'auto_install': ['test_discuss_full', 'web_enterprise'],
     'depends': [
         'account_accountant',
         'account_invoice_extract',
@@ -15,6 +16,7 @@
         'test_discuss_full',
         'voip',
         'voip_onsip',
+        'web_enterprise',
         'website_helpdesk_livechat',
         'web_studio',
         'whatsapp',
