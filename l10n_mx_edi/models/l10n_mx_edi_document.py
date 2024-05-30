5bf014f5f0edd829deb38417bcbd197f1c736aeb
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2024-05-30 16:46:53 +0200

    [FIX] l10n_mx_edi: GI refund CFDI description
    - Create a credit note
    - Add a product as a "Discount" and add the proper information
    - Process it under the use "devoluciones, descuentos o bonificaciones" (G02)
    - Check the generated XML
    
    Issue: Under description, we report "devolucion de mercancias" which
    wrong, as the user is not returning anything, they´re only getting a discount.
    
    opw-3902858
    
    closes odoo/enterprise#63520
    
    Signed-off-by: Aliya Tastemirova (alta) <alta@odoo.com>

--- l10n_mx_edi/models/l10n_mx_edi_document.py
+++ l10n_mx_edi/models/l10n_mx_edi_document.py
@@ -1002,7 +1002,7 @@ class L10nMxEdiDocument(models.Model):
             if is_refund_gi:
                 product_unspsc_code = '84111506'
                 uom_unspsc_code = 'ACT'
-                description = "Devolución de mercancias"
+                description = "Devoluciones, descuentos o bonificaciones"
             else:
                 product_unspsc_code = product.unspsc_code_id.code
                 uom_unspsc_code = uom.unspsc_code_id.code
