42614390246a329bf312a94e54d2a12b0e770f83
dise <dise@odoo.com>
2024-05-31 18:10:27 +0200

    [IMP] helpdesk_stock_account: change action to allow partial refund
    Currently, if we do a refund after returning a partial quantity, it
    creates a credit note for all the quantity. This PR will override the
    "reverse_moves" action to allow partial refunding in helpdesk.
    
    task-3905637
    
    closes odoo/enterprise#64033
    
    Signed-off-by: Vincent Larcin (vila) <vila@odoo.com>

--- helpdesk_stock_account/wizard/account_move_reversal.py
+++ helpdesk_stock_account/wizard/account_move_reversal.py
@@ -13,6 +13,17 @@ class AccountMoveReversal(models.TransientModel):
     tracking = fields.Selection(related='product_id.tracking')
     suitable_product_ids = fields.Many2many(related='helpdesk_ticket_id.suitable_product_ids', export_string_translation=False)
 
+    def reverse_moves(self, is_modify=False):
+        action = super().reverse_moves(is_modify=is_modify)
+        if self.helpdesk_ticket_id:  # checking if the wizard was created from helpdesk
+            for line in self.new_move_ids.invoice_line_ids:  # self.new_move_ids is the reverse of the moves passed to the wizard
+                # the line could be a down payment, which has display_type == 'product'; we have to check if there is a product
+                if not line.product_id or line.display_type != 'product' or line.product_id.type != 'consu' or line.product_id != self.product_id:
+                    continue
+                # for helpdesk, we are guaranteed to always have a single sale line
+                line.quantity -= line.sale_line_ids.qty_delivered
+        return action
+
     def _get_default_so_domain(self, ticket):
         domain = super()._get_default_so_domain(ticket)
         if ticket.product_id:
