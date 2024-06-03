fff408418c1c5d52c36ab6b7503d1de5c11afb2b
Ricardo Gomes Rodrigues <rigr@odoo.com>
2024-06-03 10:01:52 +0200

    [FIX] l10n_be_codabox: singleton error when multiple companies
    When the `write` method is called with multiple lines
    belonging to multiple companies that have different countries,
    we get a `singleton error`.
    
    We should instead loop on all line records in self.
    
    Bug introduced in https://github.com/odoo/enterprise/pull/63356
    
    runbot-66564
    
    closes odoo/enterprise#63707
    
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>

--- l10n_be_codabox/models/account_move.py
+++ l10n_be_codabox/models/account_move.py
@@ -5,18 +5,17 @@ class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
     def write(self, vals):
-        suspense_account = self.company_id.account_journal_suspense_account_id
-        if (
-            self.company_id.account_fiscal_country_id.code == 'BE'
-            and 'account_id' in vals
-            and self.account_id == suspense_account
-        ):
-            if mapping := self.env['soda.account.mapping'].search([
-                ('company_id', '=', self.company_id.id),
-                ('name', '=', self.name),
-                '|',
-                    ('account_id', '=', False),
-                    ('account_id', '=', suspense_account.id),
-            ]):
-                mapping.account_id = vals['account_id']
+        if 'account_id' not in vals:
+            return super().write(vals)
+        for line in self.filtered(lambda l: l.company_id.account_fiscal_country_id.code == 'BE'):
+            suspense_account = line.company_id.account_journal_suspense_account_id
+            if line.account_id == suspense_account:
+                if mapping := self.env['soda.account.mapping'].search([
+                    ('company_id', '=', line.company_id.id),
+                    ('name', '=', line.name),
+                    '|',
+                        ('account_id', '=', False),
+                        ('account_id', '=', suspense_account.id),
+                ]):
+                    mapping.account_id = vals['account_id']
         return super().write(vals)
