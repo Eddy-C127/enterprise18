4531113e7d2b9b2c3fe208fab1c17586d6ee6817
Ricardo Gomes Rodrigues <rigr@odoo.com>
2024-06-05 10:03:33 +0200

    [IMP] l10n_be_soda: auto update Soda mapping when entry line with Suspense Account is updated
    If we upload a Soda and an account is not mapped, we'll fall back on the Suspense Account.
    
    However, when modifying the entry line with the Suspense Account, the general Soda Mapping is not updated.
    
    For UX reasons, we should update the general Soda Mapping too so that the user doesn't have to modify it twice.
    
    However, we shouldn't do the reverse. I.e. when modifying the general Soda Mapping,
    we shouldn't modify the existing entries.
    
    closes odoo/enterprise#63812
    
    Taks-id: 3954585
    X-original-commit: 52743720736d5466db1300bd71de9a70c749816c
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>
    Signed-off-by: Ricardo Gomes Rodrigues (rigr) <rigr@odoo.com>

--- l10n_be_soda/tests/test_import_soda_file.py
+++ l10n_be_soda/tests/test_import_soda_file.py
@@ -74,27 +74,59 @@ class TestSodaFile(AccountTestInvoicingCommon):
             ])
             self.assertRecordValues(result_move.line_ids.account_id, [{'code': '100000'}, {'code': '700000'}, {'code': '618000'}])
 
-    def test_soda_file_import_map_accounts_with_new(self):
+    def test_soda_file_import_auto_mapping(self):
+        """
+        If a mapping is not found for an imported Soda account, we should put it in the Suspense Account.
+        When the user manually updates the generated entry and modifies the Suspense Account to
+        an account of its choice, the global mapping should be updated too for future imports.
+        However, if the global mapping is updated, existing entries shouldn't.
+        """
         with file_open(self.soda_file_path_with_new, 'rb') as soda_file:
             wizard_action = self.misc_journal.create_document_from_attachment(self.env['ir.attachment'].create({
                 'mimetype': 'application/xml',
                 'name': 'soda_testing_file_with_new_account.xml',
                 'raw': soda_file.read(),
             }).ids)
+            company = self.misc_journal.company_id
             self.assertEqual(wizard_action['res_model'], 'soda.import.wizard')
-            wizard = self.env['soda.import.wizard'].search([('id', '=', wizard_action['res_id'])])
-            missing_name_line = wizard.soda_account_mapping_ids.filtered(lambda sam: not sam.name)
-            missing_name_line.name = "Label for Withholding Taxes line"
-            missing_name_line.account_id = self.env['account.account'].create({
-                'name': 'Withholding Taxes',
-                'code': missing_name_line.code,
-            })
+            wizard = self.env['soda.import.wizard'].browse(wizard_action['res_id'])
             result = wizard.action_save_and_import()
-            result_move = self.env['account.move'].search([('id', '=', result['res_id'])])
-            self.assertEqual(len(result_move.line_ids), 3)
-            self.assertRecordValues(result_move.line_ids, [
-                {'debit': 0, 'credit': 4000.00, 'name': 'Label for Withholding Taxes line'},
-                {'debit': 0, 'credit': 11655.10, 'name': 'Special remuneration'},
-                {'debit': 15655.10, 'credit': 0, 'name': 'Remuneration'},
+            move = self.env['account.move'].browse(result['res_id'])
+
+            suspense_account = company.account_journal_suspense_account_id
+            account_455 = self.env["account.chart.template"].ref('a455')
+            account_440 = self.env["account.chart.template"].ref('a440')
+
+            # Suspense account should be used for unmapped account 12345 (see test file)
+            self.assertRecordValues(move.line_ids, [
+                {'debit': 0, 'credit': 4000, 'name': '', 'account_id': suspense_account.id},
+                {'debit': 0, 'credit': 11655.1, 'name': 'Special remuneration', 'account_id': account_455.id},
+                {'debit': 15655.1, 'credit': 0, 'name': 'Remuneration', 'account_id': account_440.id},
             ])
-            self.assertRecordValues(result_move.line_ids.account_id, [{'code': '12345'}, {'code': '455000'}, {'code': '440000'}])
+
+            # Global Soda mapping should be updated
+            self.assertRecordValues(wizard.soda_account_mapping_ids, [
+                {"code": "12345", "account_id": False},
+                {"code": "4550", "account_id": account_455.id},
+                {"code": "440000", "account_id": account_440.id},
+            ])
+
+            # Now we manually update the entry to replace the temporary Suspense Account to another account
+            account_454 = self.env["account.chart.template"].ref('a454')
+            move.line_ids[0].account_id = account_454
+            self.assertRecordValues(move.line_ids, [
+                {'debit': 0, 'credit': 4000, 'name': '', 'account_id': account_454.id},
+                {'debit': 0, 'credit': 11655.1, 'name': 'Special remuneration', 'account_id': account_455.id},
+                {'debit': 15655.1, 'credit': 0, 'name': 'Remuneration', 'account_id': account_440.id},
+            ])
+
+            # Global Soda mapping should now be updated too
+            self.assertRecordValues(wizard.soda_account_mapping_ids, [
+                {"code": "12345", "account_id": account_454.id},
+                {"code": "4550", "account_id": account_455.id},
+                {"code": "440000", "account_id": account_440.id},
+            ])
+
+            # However, updating the mapping shouldn't affect already created entries
+            wizard.soda_account_mapping_ids[-1].account_id = account_455
+            self.assertEqual(move.line_ids[0].account_id, account_454)
