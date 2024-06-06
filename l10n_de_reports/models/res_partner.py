a6cff4e8baa977238733b0d85289d1b20e0969b2
Brice bib Bartoletti <bib@odoo.com>
2024-06-06 08:42:55 +0000

    [FIX] l10n_de_reports: datev constrains performance issues
    The aim of this commit is to improve the perfomance of the datev
    features constrains checks.
    
    Context:
    Performance issue spotted on odoo.com prod. DB.
    
    Before the commit:
    Creating a partner takes up to 10 min.
    
    After the commit:
    Creating a new partner reduces the creation time to 2 min.
    
    closes odoo/enterprise#64149
    
    Task-id: None
    X-original-commit: a0c801a5cdca43bcedbdd9081b8c48cc392ff34f
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>
    Signed-off-by: Brice Bartoletti (bib) <bib@odoo.com>

--- l10n_de_reports/models/res_partner.py
+++ l10n_de_reports/models/res_partner.py
@@ -10,6 +10,7 @@ class ResPartner(models.Model):
         string='DateV Vendor',
         copy=False,
         tracking=True,
+        index='btree_not_null',
         help="In the DateV export of the General Ledger, each vendor will be identified by this identifier. "
         "If this identifier is not set, the database id of the partner will be added to a multiple of ten starting by the number 7."
         "The account code's length can be specified in the company settings."
@@ -18,6 +19,7 @@ class ResPartner(models.Model):
         string='DateV Customer',
         copy=False,
         tracking=True,
+        index='btree_not_null',
         help="In the DateV export of the General Ledger, each customer will be identified by this identifier. "
         "If this identifier is not set, the database id of the partner will be added to a multiple of ten starting by the number 1."
         "The account code's length can be specified in the company settings."
@@ -27,10 +29,10 @@ class ResPartner(models.Model):
     def _check_datev_identifier(self):
         self.flush_model(['l10n_de_datev_identifier'])
         self.env.cr.execute("""
-            SELECT COUNT(id), l10n_de_datev_identifier FROM res_partner
+            SELECT 1 FROM res_partner
             WHERE l10n_de_datev_identifier != 0
             GROUP BY l10n_de_datev_identifier
-            HAVING COUNT(id) > 1
+            HAVING COUNT(*) > 1
         """)
 
         if self.env.cr.dictfetchone():
@@ -40,10 +42,10 @@ class ResPartner(models.Model):
     def _check_datev_identifier_customer(self):
         self.flush_model(['l10n_de_datev_identifier_customer'])
         self.env.cr.execute("""
-            SELECT COUNT(id), l10n_de_datev_identifier_customer FROM res_partner
+            SELECT 1 FROM res_partner
             WHERE l10n_de_datev_identifier_customer != 0
             GROUP BY l10n_de_datev_identifier_customer
-            HAVING COUNT(id) > 1
+            HAVING COUNT(*) > 1
         """)
 
         if self.env.cr.dictfetchone():
