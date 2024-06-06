c6977b8ace1368bc2b24e8fa194152c9dc1a5d29
Adrien Guilliams (adgu) <adgu@odoo.com>
2024-06-06 08:23:22 +0000

    [FIX] l10n_cl_edi_pos: put date on top of receipt
    The chilean law requires the date to be on top of the receipt.
    This is now the case.
    
    This commit also fixes the a bug where the voucher popup would not
    show when using bank payment method in Chile.
    
    closes odoo/enterprise#64445
    
    X-original-commit: 5ffa3e8c3ea5b08bf8cc108c8148057a35b99d07
    Related: odoo/odoo#169069
    Signed-off-by: David Monnom (moda) <moda@odoo.com>
    Signed-off-by: Adrien Guilliams (adgu) <adgu@odoo.com>

--- l10n_cl_edi_pos/models/pos_payment_method.py
+++ l10n_cl_edi_pos/models/pos_payment_method.py
@@ -10,4 +10,10 @@ class PosPaymentMethod(models.Model):
     @api.depends('journal_id')
     def _compute_is_card_payment(self):
         for pm in self:
-            pm.is_card_payment = True if pm.journal_id.type == 'bank' else False
+            pm.is_card_payment = pm.journal_id.type == 'bank'
+
+    def _load_pos_data_fields(self, config_id):
+        data = super()._load_pos_data_fields(config_id)
+        if self.env.company.country_id.code == 'CL':
+            data += ['is_card_payment']
+        return data
--- l10n_cl_edi_pos/static/src/overrides/models/pos_store.js
+++ l10n_cl_edi_pos/static/src/overrides/models/pos_store.js
@@ -51,6 +51,7 @@ patch(PosStore.prototype, {
             ];
         result.l10n_latam_document_type = order.account_move.l10n_latam_document_type_id.name;
         result.l10n_latam_document_number = order.account_move.l10n_latam_document_number;
+        result.date = order.receiptDate;
 
         return result;
     },
