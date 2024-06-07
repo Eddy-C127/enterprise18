d42486ad46327a0b225823fb937bd069117fa732
Rémi Rahir (rar) <rar@odoo.com>
2024-06-07 12:28:07 +0200

    [IMP] spreadsheet_dashboard_hr_referral: Improve dashboard data
    task-3964448
    
    closes odoo/enterprise#64104
    
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- spreadsheet_dashboard_hr_referral/__manifest__.py
+++ spreadsheet_dashboard_hr_referral/__manifest__.py
@@ -5,11 +5,11 @@
     'category': 'Hidden',
     'summary': 'Spreadsheet',
     'description': 'Spreadsheet',
-    'depends': ['spreadsheet_dashboard', 'hr_referral'],
+    'depends': ['spreadsheet_dashboard', 'hr_referral', 'hr_contract_salary'],
     'data': [
         "data/dashboards.xml",
     ],
     'installable': True,
-    'auto_install': ['hr_referral'],
+    'auto_install': ['hr_referral', 'hr_contract_salary'],
     'license': 'OEEL-1',
 }
