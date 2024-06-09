c38ffe2a476c28bae6d75f36d0c0eb9fcf7a046e
Brice bib Bartoletti <bib@odoo.com>
2024-06-09 19:08:42 +0200

    [FIX] account_disallowed_expenses_fleet: query is mixing apple and pear
    The aim of this commit is to make the report behaves correctly when
    facing a DB with a lot of vehicles and a lot of accounts by making the
    query `group by` and `order by` more robust.
    
    Context:
    - 1 account and 1 vehicle share the same number for the id
    - the account is tagged with disallowed expense tag
    - 1 `account.move.line` without vehicle in that account
    - 1 `account.move.line` with a vehicle (with a dna rate) in the same
      account
    
    Before the commit:
    - The computation is giving wrong result in the report: 2 `account.move.line`
      were aggregated together under the hierarchy of the account without vehicle
      and the vehicle doesn't appear in the report at all.
      cause: the `CASE WHEN` could return, for example `92` as a `vehicle_id` and
      then `92` as an `account_id` and thus have them regrouped together.
    
    - 2 lines of the report were sharing the same "line_id" (the id given by
      report framework id, see method `_build_line_id`) which are used in a
      t-key property in an OWL loop resulting in a crash for the front-end.
    
    After the commit:
    - The 2 lines are aggregated seperatly: The line without `vehicle_id` is under
      the hierarchy of the account and the line with `vehicle_id` is under the
      hierachy of the concerned vehicle.
    - The line doesn't share any line_id anymore
    
    Task-id: None (issue discovered in our own prod)
    X-original-commit: 359c907fc8ecea1484b3df1825d519b9045a2f97
    Part-of: odoo/enterprise#66548
    Signed-off-by: John Laterre (jol) <jol@odoo.com>
    Signed-off-by: Brice Bartoletti (bib) <bib@odoo.com>

--- account_disallowed_expenses_fleet/report/account_disallowed_expenses_report.py
+++ account_disallowed_expenses_fleet/report/account_disallowed_expenses_report.py
@@ -111,8 +111,15 @@ class DisallowedExpensesFleetCustomHandler(models.AbstractModel):
         if len(current) == 1 and current.get('category_id'):
             # Expanding a category
             if options.get('vehicle_split'):
-                group_by = SQL("%s, (CASE WHEN aml.vehicle_id IS NOT NULL THEN aml.vehicle_id ELSE aml.account_id END)", group_by)
-                order_by = SQL(" ORDER BY (CASE WHEN aml.vehicle_id IS NOT NULL THEN aml.vehicle_id ELSE aml.account_id END)")
+                # In the case of a split by `vehicle_id`, we want one report line per `vehicle_id`.
+                # For those without a `vehicle_id`, we want one report line per `account_id`.
+                # Thus, we first group based on the `vehicle_id` and then group by `account_id`
+                # for those without a vehicle (which means grouping again by `vehicle_id` for those having one).
+                # NOTE: We can't directly `GROUP BY COALESCE(aml.vehicle_id, aml.account_id)` because it could
+                # group rows having a `vehicle_id` with lines having an `account_id` if they share the same number as id.
+                # See `test_disallowed_expenses_account_id_and_vehicle_id_confusion_regression_test`.
+                group_by = SQL("%s, aml.vehicle_id, COALESCE(aml.vehicle_id, aml.account_id)", group_by)
+                order_by = SQL(" ORDER BY aml.vehicle_id, COALESCE(aml.vehicle_id, aml.account_id)")
             else:
                 group_by = SQL("%s, account.id", group_by)
                 order_by = SQL("ORDER BY account.id")
