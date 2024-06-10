2352757acb6573e5b656ee12a1e51c8b8dfd9f16
Lucas Perais <lpe@odoo.com>
2024-06-10 10:11:08 +0200

    [FIX] web_studio: reportEditor: improve visitNode complexity
    Before this commit, the function that recursively browses new nodes in the reportEditor
    was in O(n^2), now it is in O(n)
    
    Part-of: odoo/enterprise#66656
    Signed-off-by: Lucas Perais (lpe) <lpe@odoo.com>

--- web_studio/static/src/client_action/report_editor/report_editor_wysiwyg/report_editor_wysiwyg.js
+++ web_studio/static/src/client_action/report_editor/report_editor_wysiwyg/report_editor_wysiwyg.js
@@ -243,11 +243,16 @@ function computeTableLayout(table) {
 
 const CUSTOM_BRANDING_ATTR = ["ws-view-id", "ws-call-key", "ws-call-group-key", "ws-real-children"];
 function visitNode(el, callback) {
-    const els = [el];
-    while (els.length) {
-        const el = els.pop();
-        callback(el);
-        els.push(...el.children);
+    const iterators = [[el]];
+    while (iterators.length) {
+        const it = iterators.pop();
+        for (const _el of it) {
+            const doChildren = callback(_el);
+            if (doChildren === false) {
+                continue;
+            }
+            iterators.push(_el.children);
+        }
     }
 }
 
