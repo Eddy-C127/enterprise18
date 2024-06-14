e6dd61b6476d4a10d8876e49d95611d0cf9e6d29
akha <akha@odoo.com>
2024-06-14 06:40:37 +0000

    [IMP] l10n_be_hr_payroll: use exoneration in termination fees withholding taxes computation.
    A CP200 employee may be eligible for exoneration
    based on the number of dependent children they have.
    This change takes the value of exoneration into
    account in the computation of termination fees withholding
    taxes. It also refactors the computation of withholding taxes
    for double holiday pay, 13th month and termination fees into
    one function.
    
    task-3909012
    
    closes odoo/enterprise#73665
    
    X-original-commit: 4066e438062ab0f7be24ffd34f5cd6bfee5de06e
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- l10n_be_hr_payroll/tests/__init__.py
+++ l10n_be_hr_payroll/tests/__init__.py
@@ -5,3 +5,4 @@ from . import common
 from . import test_payroll_allocating_paid_time_off
 from . import test_payroll_credit_time_wizard
 from . import test_payroll_right_to_legal_leaves
+from . import test_payroll_withholding_taxes_with_child_allowances
