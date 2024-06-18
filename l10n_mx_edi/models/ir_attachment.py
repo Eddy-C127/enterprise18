7bd3fa2e340486b158f89fce5a8363b1c5f8e41b
Ali Alfie (alal) <alal@odoo.com>
2024-06-18 18:13:40 +0200

    [FIX] l10n_mx_edi: allow EDI document deletion for vendor bills
    In fc84a6e a fix was added to prevent deleting EDI documents as they're
    sent to the government. This is not the case for vendor bills and vendor
    bill refunds. This commit allows the deletion of the documents for those
    move types.
    
    task-3999152
    
    closes odoo/enterprise#65193
    
    X-original-commit: dd73a1d43cbe46803aeb8236f4832f212255f7fe
    Signed-off-by: Josse Colpaert <jco@odoo.com>
    Signed-off-by: Ali Alfie (alal) <alal@odoo.com>

--- l10n_mx_edi/models/ir_attachment.py
+++ l10n_mx_edi/models/ir_attachment.py
@@ -15,6 +15,9 @@ class IrAttachment(models.Model):
 
     @api.ondelete(at_uninstall=False)
     def _unlink_except_cfdi_document(self):
-        has_cfdi_document = self.env['l10n_mx_edi.document'].sudo().search_count([('attachment_id', 'in', self.ids)], limit=1)
+        has_cfdi_document = self.env['l10n_mx_edi.document'].sudo().search_count(
+            [('attachment_id', 'in', self.ids), ('move_id.move_type', 'not in', ['in_invoice', 'in_refund'])],
+            limit=1,
+        )
         if has_cfdi_document:
             raise UserError(_("You can't unlink an attachment being an EDI document sent to the government."))
