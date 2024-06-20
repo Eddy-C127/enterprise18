38d928d5d1606519907b041d96d205110b683877
yosa-odoo <yosa@odoo.com>
2024-06-20 14:09:07 +0000

    [FIX] l10n_de_reports: correct the tag for service datev
    Steps to reproduce:
    - set a customer with Belgian VAT and check one with German VAT
    - Create sales:
    	* one for the Belgian company using the 0% EU sale taxe
    	* another for the German company using the 0% tax for construction (provider §13b)
    - Go To Statement Reports > EC Sales List
    
    Issue:
    Only the invoice for the Belgian company should appear in the report EC Sales
    
    60 tag should not be present only tags in (41, 42, 21)
    source: https://apps.datev.de/help-center/documents/1017530
    
    opw-3923031
    
    closes odoo/enterprise#66062
    
    X-original-commit: eab783f86d2ce13a43253362b56568c153595dcb
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>
    Signed-off-by: Yolann Sabaux (yosa) <yosa@odoo.com>

--- l10n_de_reports/tests/test_sales_report.py
+++ l10n_de_reports/tests/test_sales_report.py
@@ -30,7 +30,7 @@ class GermanySalesReportTest(AccountSalesReportCommon):
             ('company_id', '=', self.company_data['company'].id)
         ])[0]
         s_tax = self.env['account.tax'].search([
-            ('name', '=', '0% MOB'),
+            ('name', '=', '0% EU'),
             ('company_id', '=', self.company_data['company'].id)
         ])[0]
         self._create_invoices([
