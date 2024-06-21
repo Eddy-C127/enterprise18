8b7829469907b39d048a328dd3a939f1c3e1d21c
yagp-odoo <yagp@odoo.com>
2024-06-21 15:00:18 +0530

    [FIX] whatsapp: composer sample values not updated on template change
    Before this Commit
    ===============
    In WhatsApp Composer, the Sample Values do not update when changing
    the WhatsApp Template. Instead, it display the sample values from the
    initially selected template.
    
    After this Commit
    ==============
    The Sample Values will now update according to the selected WhatsApp Template.
    
    Task-3996935
    
    closes odoo/enterprise#65198
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- whatsapp/wizard/whatsapp_composer.py
+++ whatsapp/wizard/whatsapp_composer.py
@@ -199,8 +199,7 @@ class WhatsAppComposer(models.TransientModel):
             if rec.wa_template_id.variable_ids:
                 free_text_count = 1
                 for param in rec.wa_template_id.variable_ids.filtered(lambda line: line.line_type == 'body' and line.field_type == 'free_text'):
-                    if not rec[f"free_text_{free_text_count}"]:
-                        rec[f"free_text_{free_text_count}"] = param.demo_value
+                    rec[f"free_text_{free_text_count}"] = param.demo_value
                     free_text_count += 1
 
     def _extract_digits(self, string):
