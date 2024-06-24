c629da0cb9f779e839813378a78ac2ea57bb9180
kpt-odoo <kpt@odoo.com>
2024-06-24 16:55:32 +0530

    [FIX] sign: fix automated mail spam
    Versions:
    ------------
    17.0
    
    Steps to reproduce:
    -------------------------
    1. Send a sign request with 2 signers, setting a reminder to be sent every day.
    2. Two emails will be sent to different signers.
    3. Change the local date to 2 days ahead.
    4. Run the "Send: Sign Reminder" cron.
        - Two more emails will be received, and one signer will complete their sign request.
    5. Change the dates again and run the cron job.
        - One email will be sent to the remaining signer.
    6. From the old sign request email, copy the link and try to resend it.
        - The user should not be able to resend the new link.
    
    Issue:
    -------
    Resending the link does not check the state of the sign request and items, allowing multiple sends.
    
    Solution:
    -----------
    Resending the link is only possible if the request and items are not already completed, canceled, or refused.
    
    closes odoo/enterprise#65853
    
    Task: 4002440
    Signed-off-by: Arnaud Joset (arj) <arj@odoo.com>

--- sign/static/tests/tours/sign_request_shared_tour.js
+++ sign/static/tests/tours/sign_request_shared_tour.js
@@ -79,3 +79,15 @@ registry.category("web_tour.tours").add("shared_sign_request_tour", {
         },
     ],
 });
+
+registry.category("web_tour.tours").add("sign_resend_expired_link_tour", {
+    test: true,
+    steps: () => [
+        {
+            trigger: ".btn.btn-primary",
+            content: "Click to resend the url",
+            extra_trigger: "a:contains(Send a new link)",
+            run() {},
+        },
+    ],
+});
