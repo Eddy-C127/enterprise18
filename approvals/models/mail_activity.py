e8bc2467c1d21679d32b397d4082df872406f6e3
Sébastien Theys <seb@odoo.com>
2024-06-25 13:50:26 +0200

    [REF] approvals: convert activity_format to store
    Part of task-3605717
    
    closes odoo/enterprise#65411
    
    Related: odoo/odoo#170709
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- approvals/models/mail_activity.py
+++ approvals/models/mail_activity.py
@@ -2,19 +2,28 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import models
+from odoo.addons.mail.tools.discuss import Store
 
 
 class MailActivity(models.Model):
-    _inherit = 'mail.activity'
+    _inherit = "mail.activity"
 
-    def activity_format(self):
-        result = super(MailActivity, self).activity_format()
-        activity_type_approval_id = self.env.ref('approvals.mail_activity_data_approval').id
-        for activity in result:
-            if activity['activity_type_id'] and activity['activity_type_id'][0] == activity_type_approval_id and \
-                    activity['res_model'] == 'approval.request':
-                request = self.env['approval.request'].browse(activity['res_id'])
-                approver = request.approver_ids.filtered(lambda approver: activity['user_id'][0] == approver.user_id.id)
-                activity['approver_id'] = approver.id
-                activity['approver_status'] = approver.status
-        return result
+    def _to_store(self, store: Store):
+        super()._to_store(store)
+        activity_type_approval_id = self.env.ref("approvals.mail_activity_data_approval")
+        for activity in self.filtered(
+            lambda activity: activity["res_model"] == "approval.request"
+            and activity.activity_type_id == activity_type_approval_id
+        ):
+            request = self.env["approval.request"].browse(activity["res_id"])
+            approver = request.approver_ids.filtered(
+                lambda approver: activity.user_id == approver.user_id
+            )
+            store.add(
+                "Activity",
+                {
+                    "id": activity.id,
+                    "approver_id": approver.id,
+                    "approver_status": approver.status,
+                },
+            )
--- approvals/static/tests/mock_server/mock_models/mail_activity.js
+++ approvals/static/tests/mock_server/mock_models/mail_activity.js
@@ -1,23 +1,25 @@
 import { mailModels } from "@mail/../tests/mail_test_helpers";
 
 export class MailActivity extends mailModels.MailActivity {
-
     /** @param {number[]} ids */
-    activity_format(ids) {
-        const activities = super.activity_format(...arguments);
-        for (const activity of activities) {
-            if (activity.res_model === 'approval.request') {
-                // check on activity type being approval not done here for simplicity
-                const [approver] = this.env["approval.approver"]._filter([
-                    ['request_id', '=', activity.res_id],
-                    ['user_id', '=', activity.user_id[0]],
-                ]);
-                if (approver) {
-                    activity.approver_id = approver.id;
-                    activity.approver_status = approver.status;
-                }
+    _to_store(ids, store) {
+        super._to_store(...arguments);
+        for (const activity of this._filter([
+            ["id", "in", ids],
+            ["res_model", "=", "approval.request"],
+        ])) {
+            // check on activity type being approval not done here for simplicity
+            const [approver] = this.env["approval.approver"]._filter([
+                ["request_id", "=", activity.res_id],
+                ["user_id", "=", activity.user_id],
+            ]);
+            if (approver) {
+                store.add("Activity", {
+                    id: activity.id,
+                    approver_id: approver.id,
+                    approver_status: approver.status,
+                });
             }
         }
-        return activities;
     }
 }
