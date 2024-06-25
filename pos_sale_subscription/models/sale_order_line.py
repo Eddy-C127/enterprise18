ef36066b260e4340fac3e72024b321ac8f197e31
roen-odoo <roen@odoo.com>
2024-06-25 11:24:38 +0200

    [FIX] pos_sale_subscription: fix access error
    When creating an invoice for a sales containing a recurring product,
    if the user doesn't have access to PoS you will have an access errorr
    
    Steps to reproduce:
    -------------------
    * Remove PoS right from Marc Demo
    * Create a recurring product
    * Connect as Marc Demo
    * Create a sale with the recurring product
    * Try to invoice the sale
    > Observation: You get an access error
    
    opw-4010753
    
    closes odoo/enterprise#65487
    
    X-original-commit: 1589feeac2e5fc5cfda7876675bc78aa7a8e6394
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>
    Signed-off-by: Robin Engels (roen) <roen@odoo.com>

--- pos_sale_subscription/models/sale_order_line.py
+++ pos_sale_subscription/models/sale_order_line.py
@@ -9,5 +9,5 @@ class SaleOrderLine(models.Model):
     def _get_subscription_qty_invoiced(self, last_invoice_date=None, next_invoice_date=None):
         result = super()._get_subscription_qty_invoiced(last_invoice_date, next_invoice_date)
         for sale_line in self:
-            result[sale_line.id] = result.get(sale_line.id, 0) + sum([self._convert_qty(sale_line, pos_line.qty, 'p2s') for pos_line in sale_line.pos_order_line_ids], 0)
+            result[sale_line.id] = result.get(sale_line.id, 0) + sum([self._convert_qty(sale_line, pos_line.qty, 'p2s') for pos_line in sale_line.sudo().pos_order_line_ids], 0)
         return result
