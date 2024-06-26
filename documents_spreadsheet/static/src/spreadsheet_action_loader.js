efafb3cf379121059a37831ad56e99933e500936
Lucas Lefèvre (lul) <lul@odoo.com>
2024-06-26 22:06:43 +0200

    [REF] spreadsheet_editions,*: factorize action loader
    *documents_spreadsheet,spreadsheet_dashboard_edition
    
    closes odoo/enterprise#65588
    
    Related: odoo/odoo#170967
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>

--- documents_spreadsheet/static/src/spreadsheet_action_loader.js
+++ documents_spreadsheet/static/src/spreadsheet_action_loader.js
@@ -1,34 +1,5 @@
-/** @odoo-module **/
-
-import { registry } from "@web/core/registry";
-import { loadSpreadsheetAction } from "@spreadsheet/assets_backend/spreadsheet_action_loader";
+import { addSpreadsheetActionLazyLoader } from "@spreadsheet/assets_backend/spreadsheet_action_loader";
 import { _t } from "@web/core/l10n/translation";
 
-const actionRegistry = registry.category("actions");
-
-const loadActionSpreadsheet = async (env, context) => {
-    await loadSpreadsheetAction(env, "action_open_spreadsheet", loadActionSpreadsheet);
-
-    return {
-        ...context,
-        target: "current",
-        tag: "action_open_spreadsheet",
-        type: "ir.actions.client",
-    };
-};
-loadActionSpreadsheet.path = "spreadsheet";
-loadActionSpreadsheet.displayName = _t("Spreadsheet");
-
-const loadActionSpreadsheetTemplate = async (env, context) => {
-    await loadSpreadsheetAction(env, "action_open_template", loadActionSpreadsheetTemplate);
-
-    return {
-        ...context,
-        target: "current",
-        tag: "action_open_template",
-        type: "ir.actions.client",
-    };
-};
-
-actionRegistry.add("action_open_spreadsheet", loadActionSpreadsheet);
-actionRegistry.add("action_open_template", loadActionSpreadsheetTemplate);
+addSpreadsheetActionLazyLoader("action_open_spreadsheet", "spreadsheet", _t("Spreadsheet"));
+addSpreadsheetActionLazyLoader("action_open_template");
--- spreadsheet_dashboard_edition/static/src/assets/dashboard_edit_action_loader.js
+++ spreadsheet_dashboard_edition/static/src/assets/dashboard_edit_action_loader.js
@@ -1,18 +1,3 @@
-/** @odoo-module **/
+import { addSpreadsheetActionLazyLoader } from "@spreadsheet/assets_backend/spreadsheet_action_loader";
 
-import { registry } from "@web/core/registry";
-import { loadSpreadsheetAction } from "@spreadsheet/assets_backend/spreadsheet_action_loader";
-
-const actionRegistry = registry.category("actions");
-
-const loadDashboardAction = async (env, context) => {
-    await loadSpreadsheetAction(env, "action_edit_dashboard", loadDashboardAction);
-    return {
-        ...context,
-        target: "current",
-        tag: "action_edit_dashboard",
-        type: "ir.actions.client",
-    };
-};
-
-actionRegistry.add("action_edit_dashboard", loadDashboardAction);
+addSpreadsheetActionLazyLoader("action_edit_dashboard");
