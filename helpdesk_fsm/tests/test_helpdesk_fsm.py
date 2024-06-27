a0456cf7f7c6bdd038ea8220a41250e4ba868204
Mylyna Hy <myhy@odoo.com>
2024-06-27 18:23:16 +0000

    [FIX] helpdesk_fsm: compute to correct default fsm project
    Problem: Upon installing helpdesk_fsm in a multi-company setup,
    the default fsm project when enabling "use_fsm" on helpdesk teams
    is incorrectly retrieving the fsm project from the wrong company.
    This leads to a multi-company access error when trying to navigate
    to the fsm project linked to the helpdesk teams.
    
    Purpose: Instead of incorrectly defaulting the fsm project from
    the wrong company upon installation using a default method,
    compute the default of fsm project based on the team's company
    when use_fsm is enabled on the team.
    
    Steps to Reproduce on Runbot:
    1. Install Project, Field Service, and any l10n module
    2. Switch to the localized company
    3. Create a helpdesk team
    4. Install the helpdesk_fsm module
    5. Navigate to the helpdesk team settings
    6. Enable "use_fsm" on the team
    7. Try to access the default fsm project
    8. multi-company AccessError is thrown
    
    opw-3989799
    
    closes odoo/enterprise#69577
    
    X-original-commit: dc4eae41c69624a3006ffca8370f7cae3f16bb34
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- helpdesk_fsm/tests/test_helpdesk_fsm.py
+++ helpdesk_fsm/tests/test_helpdesk_fsm.py
@@ -28,3 +28,20 @@ class TestHelpdeskFsm(HelpdeskCommon):
         last_message = ticket.message_ids[0].body
 
         self.assertTrue(task.name in last_message, 'Marking the task as done should be logged on the ticket.')
+
+    def test_fsm_project(self):
+        # Enable field service on the helpdesk team
+        self.test_team.use_fsm = True
+        # Find the first fsm project
+        fsm_project = self.env['project.project'].search([('is_fsm', '=', True), ('company_id', '=', self.test_team.company_id.id)], limit=1)
+        # Default fsm_project should be the first fsm project with the oldest id
+        self.assertEqual(self.test_team.fsm_project_id, fsm_project,
+                         "The default fsm project should be from the same company.")
+
+    def test_fsm_project_multicompany(self):
+        extra_company = self.env['res.company'].create({'name': 'Extra Company'})
+        fsm_project = self.env['project.project'].search([('is_fsm', '=', True), ('company_id', '=', extra_company.id)], limit=1)
+        self.test_team.write({'company_id': extra_company.id})
+        self.test_team.use_fsm = True
+        self.assertEqual(self.test_team.fsm_project_id, fsm_project,
+                         "The default fsm project should be from the same company.")
