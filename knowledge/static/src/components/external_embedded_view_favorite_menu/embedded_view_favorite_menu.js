31247b57779782fd3e978184fcdcee11702818a6
Mathieu Duckerts-Antoine <dam@odoo.com>
2024-06-28 11:05:13 +0200

    [REF] knowledge: InsertEmbeddedViewMenu without patching renderers
    We refactor the menu "knowledge" of the menu "Actions" so that we do no
    longer need to patch many renderers.
    
    closes odoo/enterprise#65730
    
    Signed-off-by: Damien Abeloos (abd) <abd@odoo.com>

--- knowledge/static/src/components/external_embedded_view_favorite_menu/embedded_view_favorite_menu.js
+++ knowledge/static/src/components/external_embedded_view_favorite_menu/embedded_view_favorite_menu.js
@@ -1,37 +1,210 @@
-/** @odoo-module */
-
+import { encodeDataBehaviorProps } from "@knowledge/js/knowledge_utils";
+import { Component } from "@odoo/owl";
 import { Dropdown } from "@web/core/dropdown/dropdown";
 import { DropdownItem } from "@web/core/dropdown/dropdown_item";
+import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
-import { supportedEmbeddedViews } from "@knowledge/components/external_embedded_view_insertion/views_renderers_patches";
-import { Component } from "@odoo/owl";
+import { user } from "@web/core/user";
+import { useOwnedDialogs, useService } from "@web/core/utils/hooks";
+import { omit } from "@web/core/utils/objects";
+import { renderToElement } from "@web/core/utils/render";
+import { SelectCreateDialog } from "@web/views/view_dialogs/select_create_dialog";
 
 const cogMenuRegistry = registry.category("cogMenu");
 
-export class InsertEmbeddedViewMenu extends Component {
+const supportedEmbeddedViews = new Set([
+    "calendar",
+    "graph",
+    "hierarchy",
+    "kanban",
+    "list",
+    "pivot",
+    "cohort",
+    "gantt",
+    "map",
+]);
+
+class InsertEmbeddedViewMenu extends Component {
     static props = {};
     static template = "knowledge.InsertEmbeddedViewMenu";
     static components = { Dropdown, DropdownItem };
 
-    _onInsertEmbeddedViewInArticle () {
-        this.env.searchModel.trigger('insert-embedded-view');
+    setup() {
+        this.orm = useService("orm");
+        this.actionService = useService("action");
+        this.addDialog = useOwnedDialogs();
+        this.knowledgeCommandsService = useService("knowledgeCommandsService");
     }
-    _onInsertViewLinkInArticle () {
-        this.env.searchModel.trigger('insert-view-link');
+
+    /**
+     * @param {string} isView TODO remove with upgrade, future improvements
+     *                 will use 'display_name' everywhere, currently we have to
+     *                 fetch the right name prop depending on embed type.
+     *                 view_link_behavior uses "name" and embedded_view_behavior
+     *                 uses "display_name".
+     * @returns {Object|null} Template props necessary to render an embedded
+     *                        view in Knowledge, or null if it is not possible
+     *                        to store this view as an embedded view.
+     */
+    extractCurrentViewEmbedTemplateProps(isView = true) {
+        const config = this.env.config;
+        const xmlId = this.actionService.currentController?.action?.xml_id;
+        const context = this.getViewContext();
+        const display_name = config.getDisplayName();
+        const nameProp = isView
+            ? {
+                  // used by embedded view behavior
+                  display_name: display_name,
+              }
+            : {
+                  // used by view link behavior TODO: remove with upgrade
+                  name: display_name,
+              };
+        if (xmlId) {
+            return {
+                behaviorProps: encodeDataBehaviorProps({
+                    action_xml_id: xmlId,
+                    context,
+                    view_type: config.viewType,
+                    ...nameProp,
+                }),
+            };
+        }
+        /**
+         * Recover the original action (before the service pre-processing). The
+         * raw action is needed because it will be pre-processed again as a
+         * "different" action, after being stripped of its id, in Knowledge.
+         * If there is no original action, it means that the action is not
+         * serializable, therefore it cannot be stored in the body of an
+         * article.
+         */
+        const originalAction = this.actionService.currentController?.action?._originalAction;
+        if (originalAction) {
+            const action = JSON.parse(originalAction);
+            // Don't keep the non-markup help (to not store it in
+            // `data-behavior-props`)
+            delete action.help;
+            // Recover the markup version of the act_window help field.
+            const help = this.actionService.currentController.action.help;
+            action.display_name = display_name;
+            return {
+                behaviorProps: encodeDataBehaviorProps({
+                    act_window: action,
+                    context,
+                    view_type: config.viewType,
+                    ...nameProp,
+                }),
+                action_help: help,
+            };
+        }
+        return null;
+    }
+
+    /**
+     * Returns the full context that will be passed to the embedded view.
+     * @returns {Object}
+     */
+    getViewContext() {
+        const context = {};
+        if (this.env.searchModel) {
+            // Store the context of the search model:
+            Object.assign(
+                context,
+                omit(this.env.searchModel.context, ...Object.keys(user.context))
+            );
+            // Store the state of the search model:
+            Object.assign(context, {
+                knowledge_search_model_state: JSON.stringify(this.env.searchModel.exportState()),
+            });
+        }
+        // Store the "local context" of the view:
+        const fns = this.env.__getContext__.callbacks;
+        const localContext = Object.assign({}, ...fns.map((fn) => fn()));
+        const extraContext = {};
+        this.env.searchModel.trigger("insert-embedded-view", extraContext);
+        Object.assign(context, localContext, extraContext);
+        return context;
+    }
+
+    /**
+     * Prepare a Behavior rendered in backend to be inserted in an article by
+     * the KnowledgeCommandsService.
+     * Allow to choose an article in a modal, redirect to that article and
+     * append the rendered template "blueprint" needed for the desired Behavior
+     *
+     * @param {string} template template name of the Behavior's blueprint to
+     *                 render.
+     */
+    insertCurrentViewInKnowledge(template) {
+        const config = this.env.config;
+        const templateProps = this.extractCurrentViewEmbedTemplateProps(
+            template === "knowledge.EmbeddedViewBehaviorBlueprint"
+        );
+        if (config.actionType !== "ir.actions.act_window" || !templateProps) {
+            throw new Error(
+                'This view can not be embedded in an article: the action is not an "ir.actions.act_window" or is not serializable.'
+            );
+        }
+        this.openArticleSelector(async (id) => {
+            this.knowledgeCommandsService.setPendingBehaviorBlueprint({
+                behaviorBlueprint: renderToElement(template, templateProps),
+                model: "knowledge.article",
+                field: "body",
+                resId: id,
+            });
+            this.actionService.doAction("knowledge.ir_actions_server_knowledge_home_page", {
+                additionalContext: {
+                    res_id: id,
+                },
+            });
+        });
+    }
+
+    onInsertEmbeddedViewInArticle() {
+        this.insertCurrentViewInKnowledge("knowledge.EmbeddedViewBehaviorBlueprint");
+    }
+    onInsertViewLinkInArticle() {
+        this.insertCurrentViewInKnowledge("knowledge.EmbeddedViewLinkBehaviorBlueprint");
+    }
+
+    /**
+     * @param {Function} onSelectCallback
+     */
+    openArticleSelector(onSelectCallback) {
+        this.addDialog(SelectCreateDialog, {
+            title: _t("Select an article"),
+            noCreate: false,
+            multiSelect: false,
+            resModel: "knowledge.article",
+            context: {},
+            domain: [
+                ["user_has_write_access", "=", true],
+                ["is_template", "=", false],
+            ],
+            onSelected: (resIds) => {
+                onSelectCallback(resIds[0]);
+            },
+            onCreateEdit: async () => {
+                const articleId = await this.orm.call("knowledge.article", "article_create", [], {
+                    is_private: true,
+                });
+                onSelectCallback(articleId);
+            },
+        });
     }
 }
 
-cogMenuRegistry.add(
-    'insert-embedded-view-menu',
-    {
-        Component: InsertEmbeddedViewMenu,
-        groupNumber: 10,
-        isDisplayed: (env) => {
-            // only support act_window with an id for now, but act_window
-            // object could potentially be used too (rework backend API to insert
-            // views in articles)
-            return env.config.actionId && !env.searchModel.context.knowledgeEmbeddedViewId &&
-                supportedEmbeddedViews.has(env.config.viewType);
-        },
+cogMenuRegistry.add("insert-embedded-view-menu", {
+    Component: InsertEmbeddedViewMenu,
+    groupNumber: 10,
+    isDisplayed: (env) => {
+        // only support act_window with an id for now, but act_window
+        // object could potentially be used too (rework backend API to insert
+        // views in articles)
+        return (
+            env.config.actionId &&
+            !env.searchModel.context.knowledgeEmbeddedViewId &&
+            supportedEmbeddedViews.has(env.config.viewType)
+        );
     },
-);
+});
