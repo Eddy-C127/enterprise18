6894af392331cc2d8ae2923eeba990698fe9b60a
Arthur Pierrot (arpi) <arpi@odoo.com>
2024-07-01 16:30:37 +0200

    [IMP] l10n_us_hr_payroll: add accrual hours to payslip
    For the US, it is mandatory to have the accrual gains, usage and balance on the
    payslip. This adds a new table with the information for the current period.
    
    closes odoo/enterprise#66563
    
    Task: 3993175
    Related: odoo/odoo#180288
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- l10n_us_hr_payroll/models/hr_payslip.py
+++ l10n_us_hr_payroll/models/hr_payslip.py
@@ -1,7 +1,8 @@
-# -*- coding:utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 from collections import defaultdict
 from datetime import date
+from dateutil.relativedelta import relativedelta
 
 from odoo import models, fields
 
@@ -55,3 +56,45 @@ class HrEmployee(models.Model):
         for payslip_line in grouped_payslip_lines:
             result[payslip_line['code']] = payslip_line['amount']
         return result
+
+    def _get_leave_lines(self):
+        self.ensure_one()
+        leaves_allocations = self.env['hr.leave.allocation'].search([
+            ('employee_id', '=', self.employee_id.id),
+            ('state', '=', 'validate'),
+            ('date_from', '<', self.date_to),
+            '|',
+            ('date_to', '=', False),
+            ('date_to', '>', self.date_from),
+        ])
+        if not leaves_allocations:
+            return []
+
+        day_before_period = self.date_from + relativedelta(days=-1)
+        before_period_durations_by_leave_type = self.env['hr.work.entry']._get_leaves_duration_between_two_dates(
+            self.employee_id, min(leaves_allocations.mapped('date_from')), day_before_period)
+        period_durations_by_leave_type = self.env['hr.work.entry']._get_leaves_duration_between_two_dates(
+            self.employee_id, self.date_from, self.date_to)
+
+        # Only get the leave types associated to valid allocations
+        leave_types = leaves_allocations.holiday_status_id
+        leave_lines = []
+        for leave_type in leave_types:
+            related_allocations = leaves_allocations.filtered(lambda a: a.holiday_status_id == leave_type)
+
+            allocated_before = related_allocations._get_total_allocated(day_before_period)
+            allocated_now = related_allocations._get_total_allocated(self.date_to)
+
+            total_used_before = before_period_durations_by_leave_type.get(leave_type, 0.0)
+            used = period_durations_by_leave_type.get(leave_type, 0.0)
+
+            gain = allocated_now - allocated_before
+            balance = allocated_now - total_used_before - used
+
+            leave_lines.append({
+                'type': leave_type.name,
+                'used': used,
+                'accrual': gain,
+                'balance': balance,
+            })
+        return leave_lines
