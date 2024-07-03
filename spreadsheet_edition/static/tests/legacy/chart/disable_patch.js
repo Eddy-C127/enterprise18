4f61f7b5643dd2262bc4f74af9da966d71163f79
Adrien Minne (adrm) <adrm@odoo.com>
2024-07-03 10:41:33 +0200

    [REM] spreadsheet_edition: remove legacy test helpers
    Now that all the tests have been migrated to the new test framework, we
    can remove the legacy test helpers.
    
    Note that we still need the unpatch helpers for the moment, as the
    qunit tests of ohter modules break without them (eg. Project). They
    should be removed once all the tests have been migrated.
    
    Task: 4028278
    Part-of: odoo/enterprise#65857
    Related: odoo/odoo#171723
    Signed-off-by: Pierre Rousseau (pro) <pro@odoo.com>

--- spreadsheet_edition/static/tests/legacy/chart/disable_patch.js
+++ spreadsheet_edition/static/tests/legacy/chart/disable_patch.js
@@ -2,4 +2,5 @@
 
 import { unpatchGraphSpreadsheet } from "@spreadsheet_edition/assets/graph_view/graph_view";
 
+// TODO: Remove this file once all the QUnit tests of all the modules are ported to Hoot
 unpatchGraphSpreadsheet();
