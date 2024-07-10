93a7567d360a231ef7b96fe2e4329874b146678d
william-andre <wan@odoo.com>
2024-07-10 07:28:07 +0000

    [FIX] {documents_,}account{,_accountant}: adapt tests for audit trail
    Since the audit trail is mandatory in Germany, we cannot remove moves or
    any documents related.
    We only need to remove the moves linked to the company of the tour.
    
    We also have more queries because we now need to check if the move is
    part of the audit trail before unlinking.
    
    closes odoo/enterprise#66430
    
    X-original-commit: ab81e11d325e246422decd1bf0a2d5c989074c7c
    Related: odoo/odoo#172660
    Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>
    Signed-off-by: William André (wan) <wan@odoo.com>

--- documents_account/tests/test_documents.py
+++ documents_account/tests/test_documents.py
@@ -301,8 +301,8 @@ class TestCaseDocumentsBridgeAccount(AccountTestInvoicingCommon):
         self.assertEqual(len(attachment2), 1)
 
         self.env.flush_all()
-        with self.assertQueryCount(57):
-            # Local: 51, runbot: 57
+        with self.assertQueryCount(59):
+            # Local: 53, runbot: 59
             (move1 | move2).unlink()
 
         self.assertTrue(attachment1.exists())
