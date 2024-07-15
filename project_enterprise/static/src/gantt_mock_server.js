82f266779c3ae53fd88f81491e44b531ab386264
Mathieu Duckerts-Antoine <dam@odoo.com>
2024-07-15 14:48:45 +0200

    [REM] project_enterprise: remove legacy mock server modif for gantt
    All gantt view tests have been converted to HOOT.
    
    Part-of: odoo/enterprise#66637
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- project_enterprise/static/src/gantt_mock_server.js
+++
@@ -1,7 +0,0 @@
-import { registry } from "@web/core/registry";
-
-function _mockWebGanttWrite(_, args) {
-    return this.mockWrite(args.model, args.args);
-}
-
-registry.category("mock_server").add("web_gantt_write", _mockWebGanttWrite);
