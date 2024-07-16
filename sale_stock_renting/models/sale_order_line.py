Eddy-C127/enterprise18

commit e3d7b0646755851611c0b9ed26f205b5996a433b

[FIX] sale_stock_renting: enforce rental transfer
**Current behavior:**
If a portal or public user causes a rental order to get
confirmed, rental transfer pickings won't get created if the DB
user has the setting enabled.

**Expected behavior:**
The transfer should be created with respect to the user of the
database, not the external users.

**Steps to reproduce:**
1. Enable rental transfers

2. Create a rental order to a portal user

3. In a fresh browser, open the preview link as the portal user

4. Sign & Pay for the order

5. See that no transfers got created

**Cause of the issue:**
We use the `env.user` to determine whether the transfers should
be generated.

**Fix:**
Use the creator of the order to decide to create the transfer.

opw-3933511

closes odoo/enterprise#66302

Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

sale_stock_renting/models/sale_order_line.py
@@ -298,7 +298,7 @@ class RentalOrderLine(models.Model):
 
         If the rental picking setting is activated:
         Process all lines at the same time. """
-        if self.env.user.has_group('sale_stock_renting.group_rental_stock_picking'):
+        if self.create_uid.has_group('sale_stock_renting.group_rental_stock_picking'):
             super()._action_launch_stock_rule(previous_product_uom_qty)
         else:
             other_lines = self.filtered(lambda sol: not sol.is_rental)

sale_stock_renting/tests/test_rental.py
@@ -556,3 +556,12 @@ class TestRentalPicking(TestRentalCommon):
         self.assertEqual(action_open_return_2.get('res_id'), rental_order_1.picking_ids[1].id)
         self.assertEqual(action_open_return_2.get('domain'), '')
         self.assertEqual(action_open_return_2.get('xml_id'), 'stock.action_picking_tree_all')
+
+    def test_create_rental_transfers(self):
+        """ E.g., a public/portal user signs & pays for an order via the portal
+        """
+        public_user = self.env.ref('base.public_user')
+        rental_order_1 = self.sale_order_id.copy()
+        rental_order_1.order_line.write({'product_uom_qty': 1, 'is_rental': True})
+        rental_order_1.with_user(public_user).sudo().action_confirm()
+        self.assertTrue(rental_order_1.picking_ids)

commit 4ced2ed8fb99147e40a06c1b2eb659bbd4d3397f

[FIX] sale_stock_renting: correctly check whether pickings are enabled
Previous commit e3d7b06 broke existing flows by checking
the group membership on empty/multi recordsets.

This commit makes sure we correctly check the group membership.
* on the SO level and not the SOL level (the creator of SOLines might be the public user)
* on one record only
* and fallback on the current user if there are no records

opw-3933511

closes odoo/enterprise#66748

Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>
Co-authored-by: Victor Feyens <vfe@odoo.com>

sale_stock_renting/models/sale_order_line.py
@@ -266,7 +266,7 @@ class RentalOrderLine(models.Model):
     def _prepare_procurement_values(self, group_id=False):
         """ Change the planned and deadline dates of rental delivery pickings. """
         values = super()._prepare_procurement_values(group_id)
-        if self.is_rental and self.env.user.has_group('sale_stock_renting.group_rental_stock_picking'):
+        if self.is_rental and self._are_rental_pickings_enabled():
             values.update({
                 'date_planned': self.order_id.rental_start_date,
                 'date_deadline': self.order_id.rental_start_date,
@@ -275,7 +275,7 @@ class RentalOrderLine(models.Model):
 
     def _get_qty_procurement(self, previous_product_uom_qty=False):
         qty = super()._get_qty_procurement(previous_product_uom_qty)
-        if self.is_rental and self.env.user.has_group('sale_stock_renting.group_rental_stock_picking'):
+        if self.is_rental and self._are_rental_pickings_enabled():
             outgoing_moves = self.move_ids.filtered(lambda m: m.location_dest_id == m.company_id.rental_loc_id and m.state != 'cancel' and not m.scrapped and self.product_id == m.product_id)
             for move in outgoing_moves:
                 qty += move.product_uom._compute_quantity(move.product_qty, self.product_uom, rounding_method='HALF-UP')
@@ -298,7 +298,7 @@ class RentalOrderLine(models.Model):
 
         If the rental picking setting is activated:
         Process all lines at the same time. """
-        if self.create_uid.has_group('sale_stock_renting.group_rental_stock_picking'):
+        if not self or self._are_rental_pickings_enabled():
             super()._action_launch_stock_rule(previous_product_uom_qty)
         else:
             other_lines = self.filtered(lambda sol: not sol.is_rental)
@@ -306,7 +306,7 @@ class RentalOrderLine(models.Model):
 
     def _get_outgoing_incoming_moves(self):
         outgoing_moves, incoming_moves = super()._get_outgoing_incoming_moves()
-        if self.is_rental and self.env.user.has_group('sale_stock_renting.group_rental_stock_picking'):
+        if self.is_rental and self._are_rental_pickings_enabled():
             for move in self.move_ids.filtered(lambda r: r.state != 'cancel' and not r.scrapped and self.product_id == r.product_id):
                 if move.location_dest_id == self.company_id.rental_loc_id:
                     outgoing_moves |= move
@@ -318,8 +318,11 @@ class RentalOrderLine(models.Model):
     def _compute_qty_delivered(self):
         super()._compute_qty_delivered()
 
+        if not self._are_rental_pickings_enabled():
+            return
+
         for line in self:
-            if line.is_rental and self.env.user.has_group('sale_stock_renting.group_rental_stock_picking'):
+            if line.is_rental:
                 qty = 0.0
                 outgoing_moves, dummy = line._get_outgoing_incoming_moves()
                 for move in outgoing_moves:
@@ -341,3 +344,10 @@ class RentalOrderLine(models.Model):
             padding_timedelta_before = timedelta(hours=line.product_id.preparation_time)
             line.reservation_begin = line.start_date and line.start_date - padding_timedelta_before
         (self - lines).reservation_begin = None
+
+    def _are_rental_pickings_enabled(self):
+        if self:
+            return self[0].order_id.create_uid.has_group(
+                'sale_stock_renting.group_rental_stock_picking'
+            )
+        return self.env.user.has_group('sale_stock_renting.group_rental_stock_picking')