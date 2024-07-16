07e4b30762e2f4a610b2721c52ad77ce255a86ee
Adrien Guilliams (adgu) <adgu@odoo.com>
2024-07-16 10:20:45 +0200

    [FIX] pos_barcodelookup: fix missing productScanner widget
    Runbot error: 71442
    
    The error "Missing widget: productScanner for field of type char"
    is raised in community builds. This is linked to the fact that
    the widget is called in community but declared in enterprise.
    We thus move its usage to the enterprise view override.
    
    closes odoo/enterprise#67308
    
    X-original-commit: c1a2708ab96aa6aa235476baa0cd90129c591ba1
    Related: odoo/odoo#174454
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- pos_barcodelookup/__manifest__.py
+++ pos_barcodelookup/__manifest__.py
@@ -4,6 +4,9 @@
     'category': 'Sales/Point of Sale',
     'summary': 'This module enables product creation by scanning barcodes with a camera, utilizing the Barcode Lookup API Key.',
     'depends': ['point_of_sale', 'product_barcodelookup'],
+    'data': [
+        'views/product_view.xml',
+    ],
     'assets': {
         'point_of_sale._assets_pos': [
             "pos_barcodelookup/static/src/**/*",
