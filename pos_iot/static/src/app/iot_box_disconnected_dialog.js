51cf1976d004b733126343d976ac01986a04f429
Louis Wicket (wil) <wil@odoo.com>
2024-07-30 15:20:27 +0200

    [FIX] make user text props translatable
    closes odoo/enterprise#67624
    
    Related: odoo/odoo#174914
    Signed-off-by: Vincent Larcin (vila) <vila@odoo.com>

--- pos_iot/static/src/app/iot_box_disconnected_dialog.js
+++ pos_iot/static/src/app/iot_box_disconnected_dialog.js
@@ -1,4 +1,4 @@
-import { Component, xml } from "@odoo/owl";
+import { Component } from "@odoo/owl";
 import { Dialog } from "@web/core/dialog/dialog";
 
 export class iotBoxDisconnectedDialog extends Component {
@@ -6,20 +6,5 @@ export class iotBoxDisconnectedDialog extends Component {
     static props = {
         url: String,
     };
-    static template = xml`
-        <Dialog title="'Network Error'">
-            <p>Please check if the IoT Box is still connected.</p>
-            <p>If you are on a secure server (HTTPS) check if you accepted the certificate:</p>
-            <ul>
-                <li>
-                    <span>Access your </span>
-                    <a t-attf-href="https://{{props.url}}" target="_blank">IoT Box Homepage</a>
-                </li>
-                <li>Click on Advanced/Show Details/Details/More information</li>
-                <li>Click on Proceed to .../Add Exception/Visit this website/Go on to the webpage</li>
-                <li>Firefox only: Click on Confirm Security Exception</li>
-                <li>Close this window and try again</li>
-            </ul>
-        </Dialog>
-    `;
+    static template = "pos_iot.IotBoxDisconnectedDialog";
 }
