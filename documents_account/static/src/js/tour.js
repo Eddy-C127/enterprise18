2b2b47ad9b9c533f5bf1fd648df6249fce1aee5c
Pierre Pulinckx (PIPU) <pipu@odoo.com>
2024-07-31 09:47:15 +0200

    [FIX] documents_account: fix documents_account_tour tour
    From https://github.com/odoo/odoo/pull/174548, tooltip has been renamed
    in tooltipPosition. This conversion for this tour has been missed.
    In https://github.com/odoo/odoo/pull/171756, the step schema is
    validated when a tour is added to the registry.
    This is why this bug was detected.
    
    task~3974087
    
    closes odoo/enterprise#67582
    
    Related: odoo/odoo#171756
    Signed-off-by: Mathieu Duckerts-Antoine (dam) <dam@odoo.com>

--- documents_account/static/src/js/tour.js
+++ documents_account/static/src/js/tour.js
@@ -12,12 +12,12 @@ registry.category("web_tour.tours").add("documents_account_tour", {
         {
     trigger: '.o_app[data-menu-xmlid="documents.menu_root"]',
     content: markup(_t("Want to become a <b>paperless company</b>? Let's discover Odoo Documents.")),
-    position: 'bottom',
+            tooltipPosition: "bottom",
         },
         {
     trigger: 'body:not(:has(.o-FileViewer)) img[src="https://img.youtube.com/vi/Ayab6wZ_U1A/0.jpg"]',
     content: markup(_t("Click on a thumbnail to <b>preview the document</b>.")),
-    position: 'bottom',
+            tooltipPosition: "bottom",
         },
         {
             isActive: ["auto"],
@@ -26,7 +26,7 @@ registry.category("web_tour.tours").add("documents_account_tour", {
         {
     trigger: '[title="Close (Esc)"]',
     content: markup(_t("Click the cross to <b>exit preview</b>.")),
-    position: 'left',
+            tooltipPosition: "left",
         },
         {
             isActive: ["auto"],
@@ -36,7 +36,7 @@ registry.category("web_tour.tours").add("documents_account_tour", {
             // equivalent to '.o_search_panel_filter_value:contains('Inbox')' but language agnostic.
     trigger: '.o_search_panel_filter_value:eq(0)',
     content: markup(_t("Let's process documents in your Inbox.<br/><i>Tip: Use Tags to filter documents and structure your process.</i>")),
-    position: 'bottom',
+            tooltipPosition: "bottom",
         },
         {
             trigger: ".o_search_panel_filter_value:eq(0) .o_search_panel_label_title",
@@ -49,13 +49,13 @@ registry.category("web_tour.tours").add("documents_account_tour", {
         {
     trigger: '.o_kanban_record:contains(mail.png)',
     content: markup(_t("Click on a card to <b>select the document</b>.")),
-    position: 'bottom',
+            tooltipPosition: "bottom",
         },
         {
             // equivalent to '.o_inspector_rule:contains('Send to Legal') .o_inspector_trigger_rule' but language agnostic.
     trigger: '.o_inspector_rule[data-id="3"] .o_inspector_trigger_rule',
     content: markup(_t("Let's tag this mail as legal<br/> <i>Tips: actions can be tailored to your process, according to the workspace.</i>")),
-    position: 'bottom',
+            tooltipPosition: "bottom",
         },
         {
             isActive: ["auto"],
@@ -65,7 +65,7 @@ registry.category("web_tour.tours").add("documents_account_tour", {
             // the nth(0) ensures that the filter of the preceding step has been applied.
     trigger: '.o_kanban_record:nth(0):contains(Mails_inbox.pdf)',
     content: _t("Let's process this document, coming from our scanner."),
-    position: 'bottom',
+            tooltipPosition: "bottom",
         },
         {
             isActive: ["auto"],
@@ -74,7 +74,7 @@ registry.category("web_tour.tours").add("documents_account_tour", {
         {
     trigger: '.o_inspector_split',
     content: _t("As this PDF contains multiple documents, let's split and process in bulk."),
-    position: 'bottom',
+            tooltipPosition: "bottom",
         },
         {
             isActive: ["auto"],
@@ -83,7 +83,7 @@ registry.category("web_tour.tours").add("documents_account_tour", {
         {
     trigger: '.o_page_splitter_wrapper:nth(3)',
     content: markup(_t("Click on the <b>page separator</b>: we don't want to split these two pages as they belong to the same document.")),
-    position: 'right',
+            tooltipPosition: "right",
         },
         {
             isActive: ["auto"],
@@ -92,7 +92,7 @@ registry.category("web_tour.tours").add("documents_account_tour", {
         {
     trigger: '.o_documents_pdf_page_selector:nth(5)',
     content: markup(_t("<b>Deselect this page</b> as we plan to process all bills first.")),
-    position: 'left',
+            tooltipPosition: "left",
         },
         {
             isActive: ["auto"],
@@ -102,7 +102,7 @@ registry.category("web_tour.tours").add("documents_account_tour", {
             // equivalent to '.o_pdf_manager_button:contains(Create Vendor Bill)' but language agnostic.
     trigger: '.o_pdf_manager_button:nth-last-child(2)',
     content: _t("Let's process these bills: turn them into vendor bills."),
-    position: 'bottom',
+            tooltipPosition: "bottom",
         },
         {
             isActive: ["auto"],
@@ -111,7 +111,7 @@ registry.category("web_tour.tours").add("documents_account_tour", {
         {
     trigger: '.o_documents_pdf_page_selector',
     content: markup(_t("<b>Select</b> this page to continue.")),
-    position: 'bottom',
+            tooltipPosition: "bottom",
         },
         {
             isActive: ["auto"],
@@ -121,7 +121,7 @@ registry.category("web_tour.tours").add("documents_account_tour", {
             // equivalent to '.o_pdf_manager_button:contains(Send to Legal)' but language agnostic.
     trigger: '.o_pdf_manager_button:nth-child(4)',
     content: _t("Send this letter to the legal department, by assigning the right tags."),
-    position: 'bottom',
+            tooltipPosition: "bottom",
         },
     ],
 });
