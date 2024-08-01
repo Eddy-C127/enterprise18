50b8b0189386ea3fa6a19b10de53fcbb656aab1c
akha <akha@odoo.com>
2024-08-01 11:58:56 +0300

    [FIX] hr_payroll_expense: Reset expense sheet to approved if slip is reset to draft
    If you generate a payslip with some expenses into it and set the
    payslip as "Done", the state of the Expense will be set to 'Done' too.
    If you set the payslip back to draft, it won't be possible to create draft
    entry again cause the linked expense state didn't reset back to 'approved'.
    Instead, it remained as 'Done'.
    
    To fix the issue, when the payslip is cancelled (before it is set back
    to draft), set the state of all linked expenses back to 'approved' and
    reverse all the account moves that were created when the expenses state
    was set to 'Done'.
    
    task-4045573
    
    closes odoo/enterprise#67779
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- hr_payroll_expense/models/hr_payslip.py
+++ hr_payroll_expense/models/hr_payslip.py
@@ -1,7 +1,12 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from collections import defaultdict
+import logging
+
 from odoo import api, fields, models, _, Command
+from odoo.exceptions import AccessError, UserError
+
+
+_logger = logging.getLogger(__name__)
 
 
 class HrPayslip(models.Model):
@@ -17,57 +22,92 @@ class HrPayslip(models.Model):
         for payslip in self:
             payslip.expenses_count = sum(payslip.mapped('expense_sheet_ids.nb_expense'))
 
+    def action_payslip_cancel(self):
+        # Remove the link to the cancelled payslip so it can be linked to another payslip
+        res = super().action_payslip_cancel()
+        if self.expense_sheet_ids.account_move_ids:
+            self.expense_sheet_ids._do_reverse_moves()
+        self.expense_sheet_ids.payslip_id = False
+        self._update_expense_input_line_ids()
+        return res
+
+    def action_payslip_draft(self):
+        # We can add the new or previously unlinked expenses to the payslip
+        res = super().action_payslip_draft()
+        self._link_expenses_to_payslip(clear_existing=False)  # Add the new expenses to the payslip, but keep the already linked ones
+        return res
+
     @api.model_create_multi
     def create(self, vals_list):
         payslips = super().create(vals_list)
         draft_slips = payslips.filtered(lambda p: p.employee_id and p.state == 'draft')
         if not draft_slips:
             return payslips
-        sheets = self.env['hr.expense.sheet'].search([
-            ('employee_id', 'in', draft_slips.mapped('employee_id').ids),
-            ('state', '=', 'approve'),
-            ('payment_mode', '=', 'own_account'),
-            ('refund_in_payslip', '=', True),
-            ('payslip_id', '=', False)])
-        # group by employee
-        sheets_by_employee = defaultdict(lambda: self.env['hr.expense.sheet'])
-        for sheet in sheets:
-            sheets_by_employee[sheet.employee_id] |= sheet
-        for slip in draft_slips:
-            payslip_sheets = sheets_by_employee[slip.employee_id]
-            if slip.expense_sheet_ids:
-                slip.expense_sheet_ids = [Command.set(payslip_sheets.ids)]
-            elif payslip_sheets:
-                slip.expense_sheet_ids = [Command.link(sheet.id) for sheet in payslip_sheets]
+        draft_slips._link_expenses_to_payslip()
         return payslips
 
     def write(self, vals):
         res = super().write(vals)
         if 'expense_sheet_ids' in vals:
-            self._compute_expense_input_line_ids()
+            self._update_expense_input_line_ids()
         if 'input_line_ids' in vals:
             self._update_expense_sheets()
         return res
 
     def _compute_expense_input_line_ids(self):
+        # DEPRECATED
+        return self._update_expense_input_line_ids()
+
+    def _link_expenses_to_payslip(self, clear_existing=True):
+        """
+        Link expenses to a payslip if the payslip is in draft state and the expense is not already linked to a payslip.
+        """
+        if not (self.env.is_superuser() or self.user_has_groups('hr_payroll.group_hr_payroll_user')):
+            raise AccessError(_(
+                "You don't have the access rights to link an expense report to a payslip. You need to be a payroll officer to do that.")
+            )
+
+        sheets_by_employee = dict(self.env['hr.expense.sheet'].sudo()._read_group([
+                ('employee_id', 'in', self.employee_id.ids),
+                ('state', '=', 'approve'),
+                ('payment_mode', '=', 'own_account'),
+                ('refund_in_payslip', '=', True),
+                ('payslip_id', '=', False)],
+                groupby=['employee_id'], aggregates=['id:recordset'])
+        )
+
+        for slip_sudo in self.sudo():
+            if clear_existing:
+                slip_sudo.expense_sheet_ids = [Command.clear()]
+            payslip_sheets = sheets_by_employee.get(slip_sudo.employee_id, self.env['hr.expense.sheet'])
+            if payslip_sheets:
+                slip_sudo.expense_sheet_ids = [Command.link(sheet.id) for sheet in payslip_sheets]
+
+    def _update_expense_input_line_ids(self):
         expense_type = self.env.ref('hr_payroll_expense.expense_other_input', raise_if_not_found=False)
+        if not expense_type:
+            _logger.warning("The 'hr_payroll_expense.expense_other_input' payslip input type is missing.")
+            return  # We cannot do anything without the expense type
         for payslip in self:
-            total = sum(payslip.expense_sheet_ids.mapped('total_amount'))
-            if not total or not expense_type:
-                continue
+            # Sudo to bypass access rights, as we just need to read the expense sheet's total amounts
+            total = sum(payslip.sudo().expense_sheet_ids.mapped('total_amount'))
             lines_to_remove = payslip.input_line_ids.filtered(lambda x: x.input_type_id == expense_type)
             input_lines_vals = [Command.delete(line.id) for line in lines_to_remove]
-            input_lines_vals.append(Command.create({
-                'amount': total,
-                'input_type_id': expense_type.id
-            }))
-            payslip.update({'input_line_ids': input_lines_vals})
+            if total:
+                input_lines_vals.append(Command.create({
+                    'amount': total,
+                    'input_type_id': expense_type.id
+                }))
+            payslip.input_line_ids = input_lines_vals
 
     def _update_expense_sheets(self):
         expense_type = self.env.ref('hr_payroll_expense.expense_other_input', raise_if_not_found=False)
-        for payslip in self:
-            if not payslip.input_line_ids.filtered(lambda line: line.input_type_id == expense_type):
-                payslip.expense_sheet_ids.write({'payslip_id': False})
+        if not expense_type:
+            return  # We cannot do anything without the expense type
+        for payslip_sudo in self.sudo():
+            if not payslip_sudo.input_line_ids.filtered(lambda line: line.input_type_id == expense_type):
+                # Sudo to bypass access rights, as we just need to unlink the two models
+                payslip_sudo.expense_sheet_ids.payslip_id = False
 
     def action_payslip_done(self):
         res = super(HrPayslip, self).action_payslip_done()
