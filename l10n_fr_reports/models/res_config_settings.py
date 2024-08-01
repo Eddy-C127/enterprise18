df546bf4ddc47c32de13a108304749d80dce13ef
Yosua Nicolaus <yoni@odoo.com>
2024-08-01 17:15:01 +0200

    [IMP] account_accountant,l10n_fr_reports: default accounts labels
    Improve/update some of the labels in the Default Accounts setting from
    `account_accountant` and `l10n_fr_reports`
    
    closes odoo/enterprise#67325
    
    Task-id: 4063678
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>

--- l10n_fr_reports/models/res_config_settings.py
+++ l10n_fr_reports/models/res_config_settings.py
@@ -9,13 +9,13 @@ class ResConfigSettings(models.TransientModel):
         comodel_name='account.account',
         related='company_id.l10n_fr_rounding_difference_loss_account_id',
         readonly=False,
-        string="VAT Rounding Loss Account",
+        string="Tax Rounding Loss Account",
         help="Account used for losses from rounding the lines of French tax reports",
     )
     l10n_fr_rounding_difference_profit_account_id = fields.Many2one(
         comodel_name='account.account',
         related='company_id.l10n_fr_rounding_difference_profit_account_id',
         readonly=False,
-        string="VAT Rounding Profit Account",
+        string="Tax Rounding Profit Account",
         help="Account used for profits from rounding the lines of French tax reports",
     )
