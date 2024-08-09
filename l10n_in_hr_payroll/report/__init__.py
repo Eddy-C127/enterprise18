ae03e5eabe06f96a44ea047dd6838aa62f8d72dc
arpr-odoo <arpr@odoo.com>
2024-08-09 11:52:09 +0530

    [FIX] l10n_in_hr_payroll: followup of remove salary by head
    commit: https://github.com/odoo/enterprise/pull/59417/commits/e8c87c0980745baeb117ff09f9c665e2a8f6160a
    
    task-4107188
    
    closes odoo/enterprise#68306
    
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- l10n_in_hr_payroll/report/__init__.py
+++ l10n_in_hr_payroll/report/__init__.py
@@ -2,5 +2,4 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import report_payroll_advice
-from . import report_hr_salary_employee_bymonth
 from . import report_hr_yearly_salary_detail
