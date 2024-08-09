683e89421df9abe70af279bb35f84081c740076b
JF Aubert <ajf@odoo.com>
2024-08-09 15:33:45 +0200

    [FIX] quality_mrp: shop floor fix
    test_shop_floor fails with only mrp_workorder installed.
    With quality_mrp (as in runbot), test succeeds.
    Origin is in _compute_lot_line_id which set lot_id even when no qty_done.
    
    closes odoo/enterprise#68226
    
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- quality_mrp/models/quality.py
+++ quality_mrp/models/quality.py
@@ -27,22 +27,6 @@ class QualityCheck(models.Model):
                 record_without_production |= qc
         return super(QualityCheck, record_without_production)._compute_qty_line()
 
-    @api.depends('production_id.lot_producing_id')
-    def _compute_lot_line_id(self):
-        op_level_comp_qc = self.env['quality.check']
-        for qc in self:
-            if qc.test_type in ('register_consumed_materials', 'register_byproducts'):
-                continue
-            if qc.product_id == qc.production_id.product_id and qc.production_id.lot_producing_id:
-                qc.lot_line_id = qc.production_id.lot_producing_id
-                qc.lot_id = qc.lot_line_id
-                continue
-            op_level_comp_qc |= qc
-        return super(QualityCheck, op_level_comp_qc)._compute_lot_line_id()
-
-    def _update_lot_from_lot_line(self):
-        return super()._update_lot_from_lot_line()
-
     def _can_move_line_to_failure_location(self):
         self.ensure_one()
         if self.production_id and self.quality_state == 'fail' and self.point_id.measure_on == 'move_line':
