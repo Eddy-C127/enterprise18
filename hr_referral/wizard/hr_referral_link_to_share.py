f484cdd06298b92cc05fa19b41bf152970b42b02
Flotchet <mafl@odoo.com>
2024-08-12 14:32:26 +0200

    [FIX] hr_referral: link to share multi
    The compute_url from the link to share wizard was ensure_one which
    is not in face on how odoo is supposed to work (multi record)
    
    This commit makes the compute method multi record compatible
    
    task-3786826
    
    Part-of: odoo/enterprise#65011
    Signed-off-by: Bertrand Dossogne (bedo) <bedo@odoo.com>

--- hr_referral/wizard/hr_referral_link_to_share.py
+++ hr_referral/wizard/hr_referral_link_to_share.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import uuid
 from werkzeug.urls import url_join
 
 from odoo import api, fields, models, _
@@ -24,41 +22,54 @@ class HrReferralLinkToShare(models.TransientModel):
 
     @api.depends('channel')
     def _compute_url(self):
-        self.ensure_one()
-
-        if not self.env.user.utm_source_id:
-            self.env.user.utm_source_id = self.env['utm.source'].create({
-                'name': self.env['utm.source']._generate_name(self, self.env.user.name),
-            }).id
-
-        if self.job_id and not self.job_id.utm_campaign_id:
-            self.job_id.utm_campaign_id = self.env['utm.campaign'].create({'name': self.job_id.name}).id
-
-        job_url = url_join(self.get_base_url(), (self.job_id.website_url or '/jobs'))
-        link_tracker_values = {
-            'title': _('Referral: %(job_url)s', job_url=job_url),
-            'url': job_url,
-            'campaign_id': self.job_id.utm_campaign_id.id,
-            'source_id': self.env.user.utm_source_id.id,
-        }
-
         channel_to_medium = {
             'direct': 'utm.utm_medium_direct',
             'facebook': 'utm.utm_medium_facebook',
             'twitter': 'utm.utm_medium_twitter',
             'linkedin': 'utm.utm_medium_linkedin',
         }
-        medium_reference = channel_to_medium.get(self.channel)
-        medium = self.env.ref(medium_reference, raise_if_not_found=False) if medium_reference else False
-        if medium:
-            link_tracker_values['medium_id'] = medium.id
 
-        link_tracker = self.env['link.tracker'].search_or_create([link_tracker_values])
-        if self.channel == 'direct':
-            self.url = link_tracker.short_url
-        elif self.channel == 'facebook':
-            self.url = 'https://www.facebook.com/sharer/sharer.php?u=%s' % link_tracker.short_url
-        elif self.channel == 'twitter':
-            self.url = 'https://twitter.com/intent/tweet?tw_p=tweetbutton&text=Amazing job offer for %s! Check it live: %s' % (self.job_id.name, link_tracker.short_url)
-        elif self.channel == 'linkedin':
-            self.url = 'https://www.linkedin.com/sharing/share-offsite?url=%s' % link_tracker.short_url
+        if not self.env.user.utm_source_id:
+            self.env.user.utm_source_id = self.env['utm.source'].create({
+                'name': self.env['utm.source']._generate_name(self.env.user, self.env.user.name),
+            }).id
+
+        jobs_without_campaign = [
+            wizard.job_id for wizard in self
+            if wizard.job_id and not wizard.job_id.utm_campaign_id
+        ]
+        if jobs_without_campaign:
+            utm_campaign_ids = self.env['utm.campaign'].create([
+                {'name': _('Referral: %(name)s', name=job.name)}
+                for job in jobs_without_campaign
+            ]).ids
+            for utm_campaign_id, job in zip(utm_campaign_ids, jobs_without_campaign):
+                job.utm_campaign_id = utm_campaign_id
+
+        link_trackers_values = []
+        for wizard in self:
+            url = url_join(self.get_base_url(), '/jobs') if not wizard.job_id else wizard.job_id.full_url
+            link_trackers_values.append({
+                'title': _('Referral: %(url)s', url=url),
+                'url': url,
+                'campaign_id': wizard.job_id.utm_campaign_id.id,
+                'source_id': wizard.env.user.utm_source_id.id,
+            })
+
+            medium_reference = channel_to_medium.get(wizard.channel)
+            medium = self.env.ref(medium_reference, raise_if_not_found=False) if medium_reference else False
+            if medium:
+                link_trackers_values[-1]['medium_id'] = medium.id
+
+        link_trackers = self.env['link.tracker'].search_or_create(link_trackers_values)
+
+        for wizard, link_tracker in zip(self, link_trackers):
+            if wizard.channel == 'direct':
+                wizard.url = link_tracker.short_url
+            elif wizard.channel == 'facebook':
+                wizard.url = 'https://www.facebook.com/sharer/sharer.php?u=%s' % link_tracker.short_url
+            elif wizard.channel == 'twitter':
+                wizard.url = 'https://twitter.com/intent/tweet?tw_p=tweetbutton&text=Amazing' + \
+                    ' job offer for %s! Check it live: %s' % (self.job_id.name, link_tracker.short_url)
+            elif wizard.channel == 'linkedin':
+                wizard.url = 'https://www.linkedin.com/sharing/share-offsite?url=%s' % link_tracker.short_url
