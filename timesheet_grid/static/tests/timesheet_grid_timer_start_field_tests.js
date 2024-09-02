ffcb6b6098ca320a7f7b3304cfc54ed5de76fa8a
odoo <angv@odoo.com>
2024-09-02 11:08:43 +0200

    [FIX] timer: Single app test
    Before this commit error was raised in Single app
    install tests as timer tests needed timesheet_grid
    module to be installed.
    In this commit we corrected this issue.
    
    closes odoo/enterprise#67789
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- timesheet_grid/static/tests/timesheet_grid_timer_start_field_tests.js
+++ timesheet_grid/static/tests/timesheet_grid_timer_start_field_tests.js
@@ -4,58 +4,37 @@ import { registry } from "@web/core/registry";
 import { serializeDateTime } from "@web/core/l10n/dates";
 import { timerService } from "@timer/services/timer_service";
 
-import { makeView, setupViewRegistries } from "@web/../tests/views/helpers";
+
+import { makeView } from "@web/../tests/views/helpers";
 import { getFixture } from "@web/../tests/helpers/utils";
+import { getServerData, updateArch, setupTestEnv, addFieldsInArch } from "@hr_timesheet/../tests/hr_timesheet_common_tests";
 
 const { DateTime } = luxon;
 
 
 QUnit.module("timer", (hooks) => {
     let target;
-    let serverData;
     let viewArguments;
     const now = DateTime.utc();
     hooks.beforeEach(async function (assert) {
+        setupTestEnv();
         registry.category("services").add("timer", timerService, { force: true });
-        serverData = {
-            models: {
-                "dummy": {
-                    fields: {
-                        timer_start: {
-                            string: "Timer Started",
-                            type: "datetime",
-                        },
-                        timer_pause: {
-                            string: "Timer Paused",
-                            type: "datetime",
-                        },
-                    },
-                    records: [
-                        { id: 1, timer_start: serializeDateTime(now.minus({ days: 1 })), timer_pause: false },
-                        { id: 2, timer_start: serializeDateTime(now.minus({ days: 1 })), timer_pause: serializeDateTime(now.minus({ hours: 1 })) },
-                    ],
-                },
-            },
-            views: {
-                "dummy,false,form": `
-                    <form>
-                        <group>
-                            <group>
-                                <field name="timer_start" widget="timer_start_field"/>
-                                <field name="timer_pause"/>
-                            </group>
-                        </group>
-                    </form>`,
-            },
-        };
 
-        setupViewRegistries();
-        target = getFixture();
+        let serverData = getServerData();
+        serverData.models["account.analytic.line"].fields["timer_start"] = { string: "Timer Started", type: 'datetime' };
+        serverData.models["account.analytic.line"].fields["timer_pause"] = { string: "Timer Paused", type: 'datetime' };
+        addFieldsInArch(serverData, ["timer_start", "timer_pause"], "unit_amount");
+        updateArch(serverData, { timer_start: "timer_start_field" });
+        for (let index = 0; index < serverData.models["account.analytic.line"].records.length; index++) {
+            const record = serverData.models["account.analytic.line"].records[index];
+            record.timer_pause = index % 2 ? serializeDateTime(now.minus({ hours: 1 })) : false;
+            record.timer_start = serializeDateTime(now.minus({ days: 1 }));
+        }
 
         viewArguments = {
             serverData,
             type: "form",
-            resModel: "dummy",
+            resModel: "account.analytic.line",
             resId: 1,
             mockRPC(route, { args, method }) {
                 if (method === "get_server_time") {
@@ -77,7 +56,7 @@ QUnit.module("timer", (hooks) => {
         if (expectedRunning) {
           assert.notEqual(originalValue, currentValue, `value should have been updated after 1 second interval`);
         } else {
-          assert.equal(originalValue, currentValue, `value should not have been updated after 1 second interval`);
+          assert.strictEqual(originalValue, currentValue, `value should not have been updated after 1 second interval`);
         }
     }
 
