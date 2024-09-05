Eddy-C127/enterprise18

commit e55597dde848ae349248e4381f812e516b839985

[IMP] documents: refactor the sharing flow
Purpose
=======
Avoid creating share records prematurely.

Share popup
-----------
We always create a `document.share`, and if we don't use it, we
manually make an unlink RPC call to remove the temporary record...

This is very inefficient because we make an RPC call to create the record,
one to get the action, one to remove the record if we cancel the modal, etc.

Instead, we can just not create the record at all, and refactor the
code in that direction (so the share URL needs to be just based on
the token, not on the ID, since we need to copy the URL before the
record is created).

Spreadsheet
-----------
When we share a spreadsheet, we make a snapshot of it. This snapshot
is saved in the model `documents.shared.spreadsheet`, and it can not
be done in python. Before it used the fact that the share was created
to not make an RPC call, but it can not be used anymore.

We can still avoid making an RPC call by creating a new non-stored field,
with an inverse, that way the performance remain the same.

So, we have a lot of code with RPC call to create those snapshot records,

Task-3924439

closes odoo/enterprise#63153

Signed-off-by: Patrick Hoste (pko) <pko@odoo.com>

documents_spreadsheet/models/documents_share.py
@@ -1,5 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import ast
 import base64
 from odoo import api, Command, models, fields
 
@@ -10,6 +11,14 @@ class DocumentShare(models.Model):
         "documents.shared.spreadsheet", "share_id"
     )
 
+    # technical field, used to create freezed_spreadsheet_ids
+    spreadsheet_shares = fields.Char(inverse="_inverse_spreadsheet_shares", store=False)
+
+    def _inverse_spreadsheet_shares(self):
+        for share in self:
+            create_commands = self._create_spreadsheet_share_commands(ast.literal_eval(share.spreadsheet_shares))
+            share.write({"freezed_spreadsheet_ids": create_commands})
+
     def _get_documents_and_check_access(self, access_token, document_ids=None, operation="write"):
         available_documents = super()._get_documents_and_check_access(access_token, document_ids, operation)
         if operation == "write" and available_documents and not self.env.user._is_internal():
@@ -17,21 +26,6 @@ class DocumentShare(models.Model):
         return available_documents
 
     @api.model
-    def action_get_share_url(self, vals):
-        if "spreadsheet_shares" in vals:
-            spreadsheet_shares = vals.pop("spreadsheet_shares")
-            create_commands = self._create_spreadsheet_share_commands(spreadsheet_shares)
-            vals["freezed_spreadsheet_ids"] = create_commands
-        return super().action_get_share_url(vals)
-
-    @api.model
-    def open_share_popup(self, vals):
-        if "spreadsheet_shares" in vals:
-            spreadsheet_shares = vals.pop("spreadsheet_shares")
-            create_commands = self._create_spreadsheet_share_commands(spreadsheet_shares)
-            vals["freezed_spreadsheet_ids"] = create_commands
-        return super().open_share_popup(vals)
-
     def _create_spreadsheet_share_commands(self, spreadsheet_shares):
         create_commands = []
         for share in spreadsheet_shares:

documents_spreadsheet/tests/test_spreadsheet_share.py
@@ -81,13 +81,7 @@ class SpreadsheetSharing(SpreadsheetTestCommon):
                 }
             ]
         }
-        action = self.env["documents.share"].open_share_popup(share_vals)
-        share = self.env["documents.share"].search(
-            [("document_ids", "in", document.id)]
-        )
-        self.assertEqual(action["type"], "ir.actions.act_window")
-        self.assertEqual(action["res_id"], share.id)
-        self.assertEqual(action["res_model"], "documents.share")
+        share = self.env["documents.share"].create(share_vals)
         spreadsheet_share = share.freezed_spreadsheet_ids
         self.assertEqual(len(spreadsheet_share), 1)
         self.assertEqual(spreadsheet_share.document_id, document)

documents/models/documents_share.py
@@ -20,7 +20,7 @@ class DocumentShare(models.Model):
     include_sub_folders = fields.Boolean(default=True)
     name = fields.Char(string="Name", compute="_compute_name", store=True, readonly=False)
 
-    access_token = fields.Char(required=True, default=lambda x: str(uuid.uuid4()), groups="documents.group_documents_user")
+    access_token = fields.Char(required=True, default=lambda __: str(uuid.uuid4()), groups="documents.group_documents_user", index=True)
     full_url = fields.Char(string="URL", compute='_compute_full_url')
     links_count = fields.Integer(string="Number of Links", compute='_compute_links_count')
     date_deadline = fields.Date(string="Valid Until")
@@ -155,7 +155,7 @@ class DocumentShare(models.Model):
         else:
             return documents
 
-    @api.depends('type')
+    @api.depends('type', 'folder_id')
     def _compute_name(self):
         for record in self:
             if record.type == 'ids':
@@ -163,6 +163,7 @@ class DocumentShare(models.Model):
             else:
                 record.name = "-".join([record.folder_id.name, *record.tag_ids.mapped('name')])
 
+    @api.depends('folder_id')
     def _compute_can_upload(self):
         for record in self:
             folder = record.folder_id
@@ -170,6 +171,7 @@ class DocumentShare(models.Model):
             in_write_group = set(folder.group_ids.ids) & set(record.create_uid.groups_id.ids)
             record.can_upload = in_write_group or not folder_has_groups
 
+    @api.depends('date_deadline')
     def _compute_state(self):
         """
         changes the state based on the expiration date,
@@ -192,8 +194,7 @@ class DocumentShare(models.Model):
     @api.depends('access_token')
     def _compute_full_url(self):
         for record in self:
-            record.full_url = (f'{record.get_base_url()}/document/share/'
-                               f'{record._origin.id or record.id}/{record.access_token}')
+            record.full_url = f'{record.get_base_url()}/document/share/{record.access_token}'
 
     @api.depends('type', 'document_ids', 'domain', 'tag_ids')
     def _compute_links_count(self):
@@ -225,41 +226,11 @@ class DocumentShare(models.Model):
             })
         return values
 
-    def _get_share_popup(self, context, vals):
-        view_id = self.env.ref('documents.share_view_form_popup').id
-        return {
-            'context': context,
-            'res_model': 'documents.share',
-            'target': 'new',
-            'name': _('Share documents') if vals.get('type') == 'ids' else _('Share workspace'),
-            'res_id': self.id if self else False,
-            'type': 'ir.actions.act_window',
-            'views': [[view_id, 'form']], 
-        }
-
     def send_share_by_mail(self, template_xmlid):
         self.ensure_one()
         request_template = self.env.ref(template_xmlid, raise_if_not_found=False)
         if request_template:
             self.message_mail_with_source(request_template)
-
-    @api.model
-    def open_share_popup(self, vals):
-        """
-        returns a view.
-        :return: a form action that opens the share window to display the settings.
-        """
-        new_context = dict(self.env.context)
-        # TOOD: since the share is created directly do we really need to set the context?
-        new_context.update({
-            'default_owner_id': self.env.user.partner_id.id,
-            'default_folder_id': vals.get('folder_id'),
-            'default_tag_ids': vals.get('tag_ids'),
-            'default_type': vals.get('type', 'domain'),
-            'default_domain': vals.get('domain') if vals.get('type', 'domain') == 'domain' else False,
-            'default_document_ids': vals.get('document_ids', False),
-        })
-        return self.create(vals)._get_share_popup(new_context, vals)
 
     @api.model
     def action_get_share_url(self, vals):

documents/controllers/documents.py
@@ -455,27 +455,30 @@ class ShareRoute(http.Controller):
     # Frontend portals #############################################################################
 
     # share portals route.
-    @http.route(['/document/share/<int:share_id>/<token>'], type='http', auth='public')
+    @http.route([
+        '/document/share/<int:share_id>/<token>',
+        '/document/share/<token>'], type='http', auth='public')
     def share_portal(self, share_id=None, token=None):
-        """
-        Leads to a public portal displaying downloadable files for anyone with the token.
+        """Leads to a public portal displaying downloadable files for anyone with the token.
 
-        :param share_id: id of the share link
+        :param share_id: id of the share link (deprecated, to support old URLs)
         :param token: share access token
         """
-        try:
-            share = http.request.env['documents.share'].sudo().browse(share_id)
-            available_documents = share._get_documents_and_check_access(token, operation='read')
-            if available_documents is False:
-                if share._check_token(token):
-                    options = {
-                        'expiration_date': share.date_deadline,
-                        'author': share.create_uid.name,
-                    }
-                    return request.render('documents.not_available', options)
-                else:
-                    return request.not_found()
+        share = http.request.env['documents.share'].sudo().search([('access_token', '=', token)])
+        if not share:
+            return request.not_found()
 
+        available_documents = share._get_documents_and_check_access(token, operation='read')
+        if available_documents is False:
+            return request.render('documents.not_available', {
+                'expiration_date': share.date_deadline,
+                'author': share.create_uid.name,
+            })
+
+        return self._share_portal(share, token, available_documents)
+
+    def _share_portal(self, share, token, available_documents):
+        try:
             shareable_documents = available_documents.filtered(lambda r: r.type != 'url')
             options = {
                 'name': share.name,

documents/models/documents_document.py
@@ -436,12 +436,18 @@ class Document(models.Model):
 
     def create_share(self):
         self.ensure_one()
-        vals = {
-            'type': 'ids',
-            'document_ids': [(6, 0, self.ids)],
-            'folder_id': self.folder_id.id,
+        return {
+            'type': 'ir.actions.act_window',
+            'name': _("Share documents"),
+            'res_model': 'documents.share',
+            'views': [(False, 'form')],
+            'target': 'new',
+            'context': {
+                'default_type': 'ids',
+                'default_folder_id': self.folder_id.id,
+                'default_document_ids': [(6, 0, self.ids)],
+            }
         }
-        return self.env['documents.share'].open_share_popup(vals)
 
     def open_resource(self):
         self.ensure_one()

documents/static/src/views/hooks.js
@@ -88,33 +88,21 @@ export function useDocumentView(helpers) {
 
     // Opens Share Dialog
     const _openShareDialog = async (vals) => {
-        const act = await orm.call("documents.share", "open_share_popup", [vals]);
-        const shareResId = act.res_id;
-        let saved = false;
-        dialogService.add(
-            FormViewDialog,
-            {
-                title: act.name,
-                resModel: "documents.share",
-                resId: shareResId,
-                onRecordSaved: async (record) => {
-                    saved = true;
-                    // Copy the share link to the clipboard
-                    navigator.clipboard.writeText(record.data.full_url);
-                    // Show a notification to the user about the copy to clipboard
-                    notification.add(_t("The share url has been copied to your clipboard."), {
-                        type: "success",
-                    });
-                },
-            },
-            {
-                onClose: async () => {
-                    if (!saved) {
-                        await orm.unlink("documents.share", [shareResId]);
-                    }
-                },
-            }
+        const context = Object.fromEntries(
+            Object.entries(vals).map(([name, value]) => [`default_${name}`, value])
         );
+        dialogService.add(FormViewDialog, {
+            title: vals.type === "domains" ? _t("Share workspace") : _t("Share documents"),
+            resModel: "documents.share",
+            context,
+            onRecordSaved: async (record) => {
+                // Copy the share link to the clipboard
+                navigator.clipboard.writeText(record.data.full_url);
+                notification.add(_t("The share URL has been copied to your clipboard."), {
+                    type: "success",
+                });
+            },
+        });
     };
 
     // Keep selection between views

documents/static/tests/documents_kanban_tests.js
@@ -847,11 +847,17 @@ QUnit.module("documents", {}, function () {
             );
 
             QUnit.test("can share current domain", async function (assert) {
-                assert.expect(2);
+                assert.expect(6);
+
+                patchWithCleanup(navigator.clipboard, {
+                    async writeText(text) {
+                        assert.step("copy");
+                    },
+                });
 
                 const [resUsersId1] = pyEnv["res.users"].search([["display_name", "=", "Lukaku"]]);
                 const domain = ["owner_id", "=", resUsersId1];
-                const kanban = await createDocumentsView({
+                await createDocumentsView({
                     type: "kanban",
                     resModel: "documents.document",
                     arch: `<kanban js_class="documents_kanban"><templates><t t-name="kanban-box">
@@ -861,36 +867,28 @@ QUnit.module("documents", {}, function () {
                 </t></templates></kanban>`,
                     domain: [domain],
                     mockRPC: async function (route, args) {
-                        if (args.method === "open_share_popup") {
-                            assert.deepEqual(args.args, [
-                                {
-                                    document_ids: false,
-                                    domain: [
-                                        "&",
-                                        domain,
-                                        "&",
-                                        ["folder_id", "child_of", 1],
-                                        ["res_model", "in", ["res.fake"]],
-                                    ],
-                                    folder_id: 1,
-                                    tag_ids: [[6, false, []]],
-                                    type: "domain",
-                                },
-                            ]);
-                            return { ignore: true };
+                        if (args.method === "web_save") {
+                            assert.step("web_save");
+                            assert.deepEqual(args.kwargs.context, {
+                                default_document_ids: false,
+                                default_domain: [
+                                    "&",
+                                    domain,
+                                    "&",
+                                    ["folder_id", "child_of", 1],
+                                    ["res_model", "in", ["res.fake"]],
+                                ],
+                                default_folder_id: 1,
+                                default_tag_ids: [[6, false, []]],
+                                default_type: "domain",
+                                lang: "en",
+                                tz: "taht",
+                                uid: 2,
+                            });
                         }
                     },
                 });
 
-                const oldDoAction = kanban.env.services.action.doAction;
-                patchWithCleanup(kanban.env.services.action, {
-                    doAction(action) {
-                        if (action.ignore) {
-                            return;
-                        }
-                        return oldDoAction.call(this, action);
-                    },
-                });
                 await legacyClick(
                     target.querySelectorAll(".o_search_panel_category_value header")[1]
                 );
@@ -906,6 +904,9 @@ QUnit.module("documents", {}, function () {
 
                 await legacyClick($(".o_cp_buttons .dropdown-toggle:visible").get(0));
                 await legacyClick($(".o_documents_kanban_share_domain:visible").get(0));
+                assert.verifySteps([]);
+                await legacyClick($(".o_form_button_save").get(0));
+                assert.verifySteps(["web_save", "copy"]);
             });
 
             QUnit.test("can upload from URL", async function (assert) {
@@ -5534,7 +5535,13 @@ QUnit.module("documents", {}, function () {
             );
 
             QUnit.test("SearchPanel: can share workspace", async function (assert) {
-                assert.expect(6);
+                assert.expect(8);
+
+                patchWithCleanup(navigator.clipboard, {
+                    async writeText(text) {
+                        assert.step("copy");
+                    },
+                });
 
                 await createDocumentsView({
                     type: "kanban",
@@ -5545,16 +5552,16 @@ QUnit.module("documents", {}, function () {
                                 </div>
                             </t></templates></kanban>`,
                     mockRPC: async function (route, args) {
-                        if (args.method === "open_share_popup") {
-                            assert.deepEqual(args.args, [
-                                {
-                                    domain: [["folder_id", "child_of", 1]],
-                                    folder_id: 1,
-                                    type: "domain",
-                                },
-                            ]);
-                            assert.step("share popup");
-                            return { ignore: true };
+                        if (args.method === "web_save") {
+                            assert.deepEqual(args.kwargs.context, {
+                                default_domain: [["folder_id", "child_of", 1]],
+                                default_folder_id: 1,
+                                default_type: "domain",
+                                lang: "en",
+                                tz: "taht",
+                                uid: 2,
+                            });
+                            assert.step("web_save");
                         }
                     },
                 });
@@ -5568,7 +5575,9 @@ QUnit.module("documents", {}, function () {
                 await nextTick();
                 assert.containsOnce(target, ".o_search_panel_item_settings_popover");
                 await legacyClick(target, ".o_search_panel_value_share");
-                assert.verifySteps(["share popup"]);
+                assert.verifySteps([]);
+                await legacyClick($(".o_form_button_save").get(0));
+                assert.verifySteps(["web_save", "copy"]);
             });
         }
     );

documents/tests/test_documents_document.py
@@ -348,21 +348,13 @@ class TestCaseDocuments(TransactionCase):
         })
         share_folder.document_ids[0].tag_ids = [Command.set(share_tag.ids)]
         domain = [('folder_id', 'in', share_folder.id)]
-        action = self.env['documents.share'].open_share_popup({
+        share = self.env['documents.share'].create({
             'domain': domain,
             'folder_id': share_folder.id,
             'tag_ids': [[6, 0, [share_tag.id]]],
             'type': 'domain',
         })
-        share = self.env['documents.share'].browse(action['res_id'])
         self.assertEqual(share.links_count, 0, "There should be no links counted in this share")
-        action_context = action['context']
-        self.assertTrue(action_context)
-        self.assertEqual(action_context['default_owner_id'], self.env.user.partner_id.id, "the action should open a view with the current user as default owner")
-        self.assertEqual(action_context['default_folder_id'], share_folder.id, "the action should open a view with the right default folder")
-        self.assertEqual(action_context['default_tag_ids'], [[6, 0, [share_tag.id]]], "the action should open a view with the right default tags")
-        self.assertEqual(action_context['default_type'], 'domain', "the action should open a view with the right default type")
-        self.assertEqual(action_context['default_domain'], domain, "the action should open a view with the right default domain")
 
     def test_default_res_id_model(self):
         """

commit 78897ec42a97a3cf5ad85f0e7ebfa8ed44518385

[FIX] documents_spreadsheet: share large spreadsheet
- create a new spreadsheet
- fill all cell with content (add rows and content until it crashes)
- click on the Share button

=> boom

We have a monkey-patched version of `literal_eval` which only accepts
an input size of maximum 100KiB.

But spreadsheet files can be larger than that.

Bug introduced by e55597dde848

opw-4139087
opw-4082093

closes odoo/enterprise#69535

X-original-commit: b1190b908420e0c6ebfc81a883ef79e8bbc7b562
Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>
Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

documents_spreadsheet/models/documents_share.py
@@ -1,7 +1,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import ast
 import base64
+import json
+
 from odoo import api, Command, models, fields
 
 class DocumentShare(models.Model):
@@ -16,7 +17,7 @@ class DocumentShare(models.Model):
 
     def _inverse_spreadsheet_shares(self):
         for share in self:
-            create_commands = self._create_spreadsheet_share_commands(ast.literal_eval(share.spreadsheet_shares))
+            create_commands = self._create_spreadsheet_share_commands(json.loads(share.spreadsheet_shares))
             share.write({"freezed_spreadsheet_ids": create_commands})
 
     def _get_documents_and_check_access(self, access_token, document_ids=None, operation="write"):

documents_spreadsheet/static/src/bundle/actions/spreadsheet_action.js
@@ -99,13 +99,13 @@ export class SpreadsheetAction extends AbstractSpreadsheetAction {
             document_ids: [x2ManyCommands.set([this.resId])],
             folder_id: this.data.folder_id,
             type: "ids",
-            spreadsheet_shares: [
+            spreadsheet_shares: JSON.stringify([
                 {
                     document_id: this.resId,
                     spreadsheet_data: JSON.stringify(data),
                     excel_files: excelExport.files,
                 },
-            ],
+            ]),
         };
         const url = await this.orm.call("documents.share", "action_get_share_url", [vals]);
         return url;

documents_spreadsheet/static/src/documents_view/documents_spreadsheet_controller_mixin.js
@@ -59,7 +59,7 @@ export const DocumentsSpreadsheetControllerMixin = () => ({
         }
         return {
             ...documentShareVals,
-            spreadsheet_shares: spreadsheetShares,
+            spreadsheet_shares: JSON.stringify(spreadsheetShares),
         };
     },
 

documents_spreadsheet/static/src/documents_view/search/documents_search_model.js
@@ -24,7 +24,7 @@ patch(DocumentsSearchModel.prototype, {
             })
         );
         return {
-            spreadsheet_shares: spreadsheetShares,
+            spreadsheet_shares: JSON.stringify(spreadsheetShares),
         };
     },
 });

documents_spreadsheet/static/tests/action/spreadsheet_cp.test.js
@@ -165,13 +165,13 @@ test("share spreadsheet from control panel", async function () {
                     document_ids: [x2ManyCommands.set([spreadsheetId])],
                     folder_id: 1,
                     type: "ids",
-                    spreadsheet_shares: [
+                    spreadsheet_shares: JSON.stringify([
                         {
-                            spreadsheet_data: JSON.stringify(model.exportData()),
                             document_id: spreadsheetId,
+                            spreadsheet_data: JSON.stringify(model.exportData()),
                             excel_files: excel,
                         },
-                    ],
+                    ]),
                 });
                 return "localhost:8069/share/url/132465";
             }

documents_spreadsheet/static/tests/documents/kanban_view.test.js
@@ -113,13 +113,13 @@ test("share spreadsheet from the document inspector", async function () {
                     document_ids: [x2ManyCommands.set([1])],
                     folder_id: 1,
                     type: "ids",
-                    spreadsheet_shares: [
+                    spreadsheet_shares: JSON.stringify([
                         {
                             spreadsheet_data: JSON.stringify(model.exportData()),
-                            document_id: 1,
                             excel_files: excel,
+                            document_id: 1,
                         },
-                    ],
+                    ]),
                 });
                 return "localhost:8069/share/url/132465";
             }
@@ -160,13 +160,13 @@ test("share a selected spreadsheet from the share button", async function () {
                 expect(shareVals.default_document_ids).toEqual([x2ManyCommands.set([1])]);
                 expect(shareVals.default_folder_id).toBe(1);
                 expect(shareVals.default_type).toBe("ids");
-                expect(shareVals.default_spreadsheet_shares).toEqual([
+                expect(shareVals.default_spreadsheet_shares).toEqual(JSON.stringify([
                     {
                         spreadsheet_data: JSON.stringify(model.exportData()),
-                        document_id: 1,
                         excel_files: JSON.parse(JSON.stringify(model.exportXLSX().files)),
+                        document_id: 1,
                     },
-                ]);
+                ]));
             }
         },
     });
@@ -205,13 +205,13 @@ test("share the full workspace from the share button", async function () {
                 expect(shareVals.default_folder_id).toBe(1);
                 expect(shareVals.default_type).toBe("domain");
                 expect(shareVals.default_domain).toEqual([["folder_id", "=", 1]]);
-                expect(shareVals.default_spreadsheet_shares).toEqual([
+                expect(shareVals.default_spreadsheet_shares).toEqual(JSON.stringify([
                     {
                         spreadsheet_data: JSON.stringify(model.exportData()),
-                        document_id: 1,
                         excel_files: JSON.parse(JSON.stringify(model.exportXLSX().files)),
+                        document_id: 1,
                     },
-                ]);
+                ]));
             }
         },
     });

documents_spreadsheet/tests/test_spreadsheet_share.py
@@ -1,5 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import json
+
 
 from .common import SpreadsheetTestCommon
 from odoo.exceptions import AccessError
@@ -20,13 +22,13 @@ class SpreadsheetSharing(SpreadsheetTestCommon):
             "document_ids": [(6, 0, [document.id])],
             "folder_id": document.folder_id.id,
             "type": "ids",
-            "spreadsheet_shares": [
+            "spreadsheet_shares": json.dumps([
             {
                 "spreadsheet_data": document.spreadsheet_data,
                 "document_id": document.id,
                 "excel_files": EXCEL_FILES,
             }
-        ]
+        ])
         }
         url = self.env["documents.share"].action_get_share_url(share_vals)
         share = self.env["documents.share"].search(
@@ -46,7 +48,7 @@ class SpreadsheetSharing(SpreadsheetTestCommon):
             "document_ids": [(6, 0, documents.ids)],
             "folder_id": document1.folder_id.id,
             "type": "ids",
-            "spreadsheet_shares": [
+            "spreadsheet_shares": json.dumps([
             {
                 "spreadsheet_data": document1.spreadsheet_data,
                 "document_id": document1.id,
@@ -57,7 +59,7 @@ class SpreadsheetSharing(SpreadsheetTestCommon):
                 "document_id": document2.id,
                 "excel_files": EXCEL_FILES,
             },
-        ]
+        ])
         }
         url = self.env["documents.share"].action_get_share_url(share_vals)
         share = self.env["documents.share"].search(
@@ -73,13 +75,13 @@ class SpreadsheetSharing(SpreadsheetTestCommon):
             "document_ids": [(6, 0, [document.id])],
             "folder_id": document.folder_id.id,
             "type": "ids",
-            "spreadsheet_shares": [
+            "spreadsheet_shares": json.dumps([
                 {
                     "spreadsheet_data": document.spreadsheet_data,
                     "document_id": document.id,
                     "excel_files": EXCEL_FILES,
                 }
-            ]
+            ])
         }
         share = self.env["documents.share"].create(share_vals)
         spreadsheet_share = share.freezed_spreadsheet_ids