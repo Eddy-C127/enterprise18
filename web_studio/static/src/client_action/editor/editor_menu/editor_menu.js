3e75686543de2ad536f7c9008fb9851be5bdcc6d
Lucas Perais <lpe@odoo.com>
2024-09-10 14:03:40 +0000

    [FIX] web_studio: editorMenu updates when tab changes
    Before this commit, in studio, the editorMenu was not updated when clicking on a different tab
    (views, automations etc...)
    
    After this commit, the menu is updated.
    
    closes odoo/enterprise#69840
    
    X-original-commit: 5ca4793370a6fe09136fbf60f544b07861d3d52a
    Signed-off-by: Lucas Perais (lpe) <lpe@odoo.com>

--- web_studio/static/src/client_action/editor/editor_menu/editor_menu.js
+++ web_studio/static/src/client_action/editor/editor_menu/editor_menu.js
@@ -1,10 +1,10 @@
 /** @odoo-module */
-import { useService } from "@web/core/utils/hooks";
 import { _t } from "@web/core/l10n/translation";
 import { localization } from "@web/core/l10n/localization";
 import { registry } from "@web/core/registry";
 
 import { Component, useState } from "@odoo/owl";
+import { useStudioServiceAsReactive } from "@web_studio/studio_service";
 const editorTabRegistry = registry.category("web_studio.editor_tabs");
 
 class Breadcrumbs extends Component {
@@ -105,7 +105,7 @@ export class EditorMenu extends Component {
     static components = { Breadcrumbs };
     setup() {
         this.l10n = localization;
-        this.studio = useService("studio");
+        this.studio = useStudioServiceAsReactive();
         this.editionFlow = useState(this.env.editionFlow);
     }
 
