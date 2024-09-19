8217b6a43a8ff1f78cd720574251124df6549e7f
Hesham <hsal@odoo.com>
2024-09-19 13:14:53 +0000

    [IMP] l10n_tr_reports: Renamed Turkey to Türkiye
    In 2022, Republic of Turkey officially renamed itself to Republic of Türkiye.
    So, its name needed to be changed in its Odoo modules.
    Moreover, updated Turkish modules descriptions according to this excalidraw:
    https://link.excalidraw.com/readonly/rbesZEAkXUS8rVgXplgm?darkMode=true
    
    task-4182931
    
    closes odoo/enterprise#71444
    
    X-original-commit: cdd55dd0ab3afcbe1279e8006b69442632a72534
    Related: odoo/odoo#182743
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>
    Signed-off-by: Hesham Saleh (hsal) <hsal@odoo.com>

--- l10n_tr_reports/__manifest__.py
+++ l10n_tr_reports/__manifest__.py
@@ -2,12 +2,15 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Turkey - Accounting Reports',
+    'name': 'Türkiye - Accounting Reports',
     'countries': ['tr'],
     'version': '1.0',
     'category': 'Accounting/Localizations/Reporting',
     'description': """
-Accounting reports for Turkey
+Accounting reports for Türkiye
+
+- Balance Sheet
+- Profit and Loss
     """,
     'depends': [
         'l10n_tr', 'account_reports'
@@ -18,6 +21,5 @@ Accounting reports for Turkey
     ],
     'installable': True,
     'auto_install': True,
-    'website': 'https://www.odoo.com/app/accounting',
     'license': 'OEEL-1',
 }
