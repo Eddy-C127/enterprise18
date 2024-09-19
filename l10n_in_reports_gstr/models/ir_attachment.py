33cfbf21c36f8d62e532ae3e8637b6156b448140
Zeel Patel <zepa@odoo.com>
2024-09-19 14:40:56 +0530

    [FIX] l10n_in_reports_gstr: Handle multiple records in _unwrap_edi_attachments
    The method `_unwrap_edi_attachments` previously failed when `self` contained
    more than one record. This caused issues, particularly in the test
    `test_extend_with_attachments_multi_pdf` when both `l10n_in` and
    `account_accountant` modules were installed.
    
    The issue has been fixed by adding a loop to process each record in `self`
    individually and checking the `res_model` for each attachment within the file
    data.
    
    closes odoo/enterprise#70282
    
    Fixes: https://runbot.odoo.com/web/#id=98056&menu_id=424&cids=1&action=573&model=runbot.build.error&view_type=form
    Signed-off-by: Josse Colpaert (jco) <jco@odoo.com>

--- l10n_in_reports_gstr/models/ir_attachment.py
+++ l10n_in_reports_gstr/models/ir_attachment.py
@@ -10,8 +10,9 @@ class IrAttachment(models.Model):
     def _unwrap_edi_attachments(self, *args, **kwargs):
         file_data_list = super()._unwrap_edi_attachments(*args, **kwargs)
         for file_data in file_data_list:
-            if file_data['type'] == 'binary' and self.res_model == "account.move":
-                move = self.env['account.move'].browse(self.res_id)
+            attachment = file_data['attachment']
+            if file_data['type'] == 'binary' and attachment.res_model == "account.move":
+                move = self.env['account.move'].browse(attachment.res_id)
                 if move and move.l10n_in_irn_number:
                     file_data['process_if_existing_lines'] = True
         return file_data_list
