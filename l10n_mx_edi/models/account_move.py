6005bc76ec009e9fc7d03c0f793b9cb1b6368232
Anh Thao Pham (pta) <pta@odoo.com>
2024-09-19 09:32:47 +0200

    [FIX] l10n_mx_edi: display matching usage description
    Steps to reproduce:
    - Install l10n_mx_edi
    - Switch to a Mexican company (e.g. ESCUELA KEMPER URGATE)
    - Create an invoice:
      * Customer: [a Mexican customer] (e.g. INMOBILIARIA CVA)
      * Usage: General expenses
      * CFDI to public: [checked]
    - Confirm the invoice
    - Generate CFDI and the invoice via "Send & Print" button
    
    Issue:
    On the invoice, "Usage" has an incorrect value:
    "S01 - General expenses"
    which is a mix of "S01 - Without fiscal effects" and
    "G03 - General expenses".
    The description should be the one linked to the code.
    
    opw-4118647
    
    closes odoo/enterprise#70274
    
    Signed-off-by: Ricardo Gomes Rodrigues (rigr) <rigr@odoo.com>

--- l10n_mx_edi/models/account_move.py
+++ l10n_mx_edi/models/account_move.py
@@ -374,6 +374,7 @@ class AccountMove(models.Model):
         if payment_way:
             payment_method = self.env['l10n_mx_edi.payment.method'].search([('code', '=', payment_way)])
             cfdi_infos['payment_way'] = f'{payment_way} - {payment_method.name}'
+        cfdi_infos['usage_desc'] = dict(USAGE_SELECTION).get(cfdi_infos['usage'])
 
         return cfdi_infos
 
