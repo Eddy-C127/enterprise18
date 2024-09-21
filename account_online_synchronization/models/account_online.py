Eddy-C127/enterprise18

commit 08a818bef6150c2877cdf64a7f4aa05ea14449e9

[IMP] account: rework lock dates and their wizard
In the associated community PR the lock dates were reworked.
I.e.
- The `period_lock_date` was replaced by `sale_lock_date` and `purchase_lock_date`.
- (Temporary) Lock Date Exceptions were added (new model)
  They can be used to make changes to a locked period
- A `hard_lock_date` was added.
  - It locks everything (like all the lock dates were set to that date).
  - It does not allow exceptions.
  - It cannot be decreased or removed.
See the community PR for details.

In this commit we adapt the enterprise code to the changes in community.

I.e. we change the lock date wizard:
- All the lock dates can be seen and set in the wizard.
- Exceptions are created when decreasing a lock date
  - No exceptions are possible for the `hard_lock_date`
  - When a lock date value is decreased in the wizard a line on the button appears.
    There it can be selected
    - who the exception applies to: me or everyone
    - how long the exception is valid: 5 min, 15 min, 1 h, 24 h or forever
    - (optional) reason
  - In case an exception for everyone and forever is selected we change the lock date instead of creating an exception.
- In case there are active exceptions for a lock date there is a revoke button on the right side of the lock date.
  It can be used to revoke the most permissive (minimal date) active exception for that lock date.
- In case there are draft entries in the period the user wants to lock a warning is displayed.
  It is only blocking for the `hard_lock_date`.

task-3891414

closes odoo/enterprise#67090

Related: odoo/odoo#174004
Related: odoo/upgrade#6295
Signed-off-by: William André (wan) <wan@odoo.com>

account_online_synchronization/models/account_online.py
@@ -127,7 +127,7 @@ class AccountOnlineAccount(models.Model):
         self.account_online_link_id._get_consent_expiring_date()
 
         # Set last_sync date (date of latest statement or accounting lock date or False)
-        last_sync = self.env.company.fiscalyear_lock_date
+        last_sync = self.env.company._get_user_fiscal_lock_date(journal)
         bnk_stmt_line = self.env['account.bank.statement.line'].search([('journal_id', 'in', self.journal_ids.ids)], order="date desc", limit=1)
         if bnk_stmt_line:
             last_sync = bnk_stmt_line.date

account_accountant/tests/__init__.py
@@ -8,3 +8,4 @@ from . import test_account_reconcile_wizard
 from . import test_deferred_management
 from . import test_ui
 from . import test_signature
+from . import test_change_lock_date_wizard

account_accountant/tests/test_change_lock_date_wizard.py
@@ -1,0 +1,124 @@
+from odoo import fields
+from odoo.addons.account.tests.common import AccountTestInvoicingCommon
+from odoo.addons.account_accountant.wizard.account_change_lock_date import SOFT_LOCK_DATE_FIELDS
+from odoo.exceptions import UserError
+from odoo.tests import tagged
+
+
+@tagged('post_install', '-at_install')
+class TestChangeLockDateWizard(AccountTestInvoicingCommon):
+
+    def test_exception_generation(self):
+        """
+        Test the exception generation from the wizard.
+        Note that exceptions for 'everyone' and 'forever' are not tested here.
+        They do not create an exception (no 'account.lock_exception' object), but just change the lock date.
+        (See `test_everyone_forever_exception`.)
+        """
+        self.env['account.lock_exception'].search([]).sudo().unlink()
+
+        for lock_date_field in SOFT_LOCK_DATE_FIELDS:
+            with self.subTest(lock_date_field=lock_date_field), self.cr.savepoint() as sp:
+                # We can set the lock date if there is none.
+                self.env['account.change.lock.date'].create({lock_date_field: '2010-01-01'}).change_lock_date()
+                self.assertEqual(self.env.company[lock_date_field], fields.Date.from_string('2010-01-01'))
+
+                # We can increase the lock date if there is one.
+                self.env['account.change.lock.date'].create({lock_date_field: '2011-01-01'}).change_lock_date()
+                self.assertEqual(self.env.company[lock_date_field], fields.Date.from_string('2011-01-01'))
+
+                # We cannot remove the lock date; not even with an exception.
+                wizard = self.env['account.change.lock.date'].create({
+                    lock_date_field: False,
+                    'exception_applies_to': 'everyone',
+                    'exception_duration': '1h',
+                    'exception_reason': ':TestChangeLockDateWizard.test_exception_generation; remove',
+                })
+                with self.assertRaises(UserError), self.env.cr.savepoint():
+                    wizard.change_lock_date()
+
+                # Ensure we have not created any exceptions yet
+                self.assertEqual(self.env['account.lock_exception'].search_count([]), 0)
+
+                # We cannot decrease the lock date; but we can create an exception
+                self.env['account.change.lock.date'].create({lock_date_field: '2009-01-01'}).change_lock_date()
+                self.assertEqual(self.env.company[lock_date_field], fields.Date.from_string('2011-01-01'))
+                exception = self.env['account.lock_exception'].search([])
+                self.assertEqual(len(exception), 1)
+                # Check lock date and default values on exception
+                self.assertRecordValues(exception, [{
+                    lock_date_field: fields.Date.from_string('2009-01-01'),
+                    'company_id': self.env.company.id,
+                    'user_id': self.env.user.id,
+                    'create_uid': self.env.user.id,
+                    'end_datetime': False,
+                    'reason': False,
+                }])
+
+                sp.close()  # Rollback to ensure all subtests start in the same situation
+
+    def test_hard_lock_date(self):
+        self.env['account.lock_exception'].search([]).sudo().unlink()
+
+        # We can set the hard lock date if there is none.
+        self.env['account.change.lock.date'].create({'hard_lock_date': '2010-01-01'}).change_lock_date()
+        self.assertEqual(self.env.company.hard_lock_date, fields.Date.from_string('2010-01-01'))
+
+        # We can increase the hard lock date if there is one.
+        self.env['account.change.lock.date'].create({'hard_lock_date': '2011-01-01'}).change_lock_date()
+        self.assertEqual(self.env.company.hard_lock_date, fields.Date.from_string('2011-01-01'))
+
+        # We cannot decrease the hard lock date; not even with an exception.
+        wizard = self.env['account.change.lock.date'].create({
+            'hard_lock_date': '2009-01-01',
+            'exception_applies_to': 'everyone',
+            'exception_duration': '1h',
+            'exception_reason': ':TestChangeLockDateWizard.test_hard_lock_date',
+        })
+        with self.assertRaises(UserError), self.env.cr.savepoint():
+            wizard.change_lock_date()
+        self.assertEqual(self.env.company.hard_lock_date, fields.Date.from_string('2011-01-01'))
+
+        # We cannot remove the hard lock date; not even with an exception.
+        wizard = self.env['account.change.lock.date'].create({
+            'hard_lock_date': False,
+            'exception_applies_to': 'everyone',
+            'exception_duration': '1h',
+            'exception_reason': ':TestChangeLockDateWizard.test_hard_lock_date',
+        })
+        with self.assertRaises(UserError), self.env.cr.savepoint():
+            wizard.change_lock_date()
+        self.assertEqual(self.env.company.hard_lock_date, fields.Date.from_string('2011-01-01'))
+
+        self.assertEqual(self.env['account.lock_exception'].search_count([]), 0)
+
+    def test_everyone_forever_exception(self):
+        self.env['account.lock_exception'].search([]).sudo().unlink()
+
+        for lock_date_field in SOFT_LOCK_DATE_FIELDS:
+            with self.subTest(lock_date_field=lock_date_field), self.cr.savepoint() as sp:
+                self.env['account.change.lock.date'].create({lock_date_field: '2010-01-01'}).change_lock_date()
+                self.assertEqual(self.env.company[lock_date_field], fields.Date.from_string('2010-01-01'))
+
+                # We can decrease the lock date with a 'forever' / 'everyone' exception.
+                self.env['account.change.lock.date'].create({
+                    lock_date_field: '2009-01-01',
+                    'exception_applies_to': 'everyone',
+                    'exception_duration': 'forever',
+                    'exception_reason': ':TestChangeLockDateWizard.test_everyone_forever_exception; remove',
+                }).change_lock_date()
+                self.assertEqual(self.env.company[lock_date_field], fields.Date.from_string('2009-01-01'))
+
+                # We can remove the lock date with a 'forever' / 'everyone' exception.
+                self.env['account.change.lock.date'].create({
+                    lock_date_field: False,
+                    'exception_applies_to': 'everyone',
+                    'exception_duration': 'forever',
+                    'exception_reason': ':TestChangeLockDateWizard.test_everyone_forever_exception; remove',
+                }).change_lock_date()
+                self.assertEqual(self.env.company[lock_date_field], False)
+
+                # Ensure we have not created any exceptions
+                self.assertEqual(self.env['account.lock_exception'].search_count([]), 0)
+
+                sp.close()  # Rollback to ensure all subtests start in the same situation

account_accountant/wizard/account_change_lock_date.py
@@ -1,8 +1,12 @@
-from odoo import models, fields, _
+from odoo import api, models, fields, _
 from odoo.exceptions import UserError
+from odoo.osv import expression
 from odoo.tools import date_utils
 
-from datetime import timedelta
+from odoo.addons.account.models.company import SOFT_LOCK_DATE_FIELDS, LOCK_DATE_FIELDS
+
+from datetime import date, timedelta
+
 
 class AccountChangeLockDate(models.TransientModel):
     """
@@ -11,31 +15,245 @@ class AccountChangeLockDate(models.TransientModel):
     _name = 'account.change.lock.date'
     _description = 'Change Lock Date'
 
-    period_lock_date = fields.Date(
-        string='Journal Entries Lock Date',
-        default=lambda self: self.env.company.period_lock_date,
-        help='Prevents Journal entries creation up to the defined date inclusive. Except for Accountant users.')
+    company_id = fields.Many2one(
+        comodel_name='res.company',
+        required=True,
+        readonly=True,
+        default=lambda self: self.env.company,
+    )
+
     fiscalyear_lock_date = fields.Date(
-        string='All Users Lock Date',
+        string='Lock Everyone',
         default=lambda self: self.env.company.fiscalyear_lock_date,
         help='Prevents Journal Entry creation or modification up to the defined date inclusive for all users. '
-             'As a closed period, all accounting operations are prohibited.')
+             'As a closed period, all accounting operations are prohibited.',
+    )
+    min_fiscalyear_lock_date_exception = fields.Many2one(
+        comodel_name='account.lock_exception',
+        compute='_compute_lock_date_exceptions',
+    )
+
     tax_lock_date = fields.Date(
-        string="Tax Return Lock Date",
+        string="Lock Tax Return",
         default=lambda self: self.env.company.tax_lock_date,
         help='Prevents Tax Returns modification up to the defined date inclusive (Journal Entries involving taxes). '
-             'The Tax Return Lock Date is automatically set when the corresponding Journal Entry is posted.')
+             'The Tax Return Lock Date is automatically set when the corresponding Journal Entry is posted.',
+    )
+    min_tax_lock_date_exception = fields.Many2one(
+        comodel_name='account.lock_exception',
+        compute='_compute_lock_date_exceptions',
+    )
 
-    def _prepare_lock_date_values(self):
+    sale_lock_date = fields.Date(
+        string='Lock Sales',
+        default=lambda self: self.env.company.sale_lock_date,
+        help='Prevents creation and modification of entries in sales journals up to the defined date inclusive',
+    )
+    min_sale_lock_date_exception = fields.Many2one(
+        comodel_name='account.lock_exception',
+        compute='_compute_lock_date_exceptions',
+    )
+
+    purchase_lock_date = fields.Date(
+        string='Lock Purchases',
+        default=lambda self: self.env.company.purchase_lock_date,
+        help='Prevents creation and modification of entries in purchase journals up to the defined date inclusive',
+    )
+    min_purchase_lock_date_exception = fields.Many2one(
+        comodel_name='account.lock_exception',
+        compute='_compute_lock_date_exceptions',
+    )
+
+    hard_lock_date = fields.Date(
+        string='Hard Lock',
+        default=lambda self: self.env.company.hard_lock_date,
+        help='Like the "Everyone Lock", but no exceptions are possible.',
+    )
+    current_hard_lock_date = fields.Date(
+        string='Current Hard Lock',
+        related='company_id.hard_lock_date',
+        readonly=True,
+    )
+
+    exception_needed = fields.Boolean(
+        string='Exception needed',
+        compute='_compute_exception_needed',
+    )
+    exception_applies_to = fields.Selection(
+        string='Exception applies to',
+        selection=[
+            ('me', "me"),
+            ('everyone', "everyone"),
+        ],
+        default='me',
+        required=True,
+    )
+    exception_duration = fields.Selection(
+        string='Exception Duration',
+        selection=[
+            ('5min', "5 minutes"),
+            ('15min', "15 minutes"),
+            ('1h', "1 hour"),
+            ('24h', "24 hours"),
+            ('forever', "forever"),
+        ],
+        default='5min',
+        required=True,
+    )
+    exception_reason = fields.Char(
+        string='Exception Reason',
+    )
+
+    show_draft_entries_warning = fields.Text(
+        string="Show Draft Entries Warning",
+        compute='_compute_show_draft_entries_warning',
+    )
+
+    def _get_active_exceptions_domain(self, soft_lock_date_fields):
+        self.ensure_one()
+        return [
+            '|',
+                ('user_id', '=', None),
+                ('user_id', '=', self.env.user.id),
+            *expression.OR([(field, '<', self.env.company[field])] for field in soft_lock_date_fields),
+            ('company_id', '=', self.env.company.id),
+            ('state', '=', 'active'),  # checks the datetime
+        ]
+
+    @api.depends('company_id')
+    @api.depends_context('user', 'company')
+    def _compute_lock_date_exceptions(self):
+        for wizard in self:
+            exceptions = self.env['account.lock_exception'].search(
+                self._get_active_exceptions_domain(SOFT_LOCK_DATE_FIELDS)
+            )
+            for field in SOFT_LOCK_DATE_FIELDS:
+                field_exceptions = exceptions.filtered(lambda e: e[field])
+                min_exception = min(field_exceptions, key=lambda e: e[field]) if field_exceptions else False
+                wizard[f"min_{field}_exception"] = min_exception
+
+    def _get_draft_moves_in_locked_period_domain(self):
+        self.ensure_one()
+        lock_date_domains = []
+        if self.hard_lock_date:
+            lock_date_domains.append([('date', '<=', self.hard_lock_date)])
+        if self.fiscalyear_lock_date:
+            lock_date_domains.append([('date', '<=', self.fiscalyear_lock_date)])
+        if self.sale_lock_date:
+            lock_date_domains.append([
+                ('date', '<=', self.sale_lock_date),
+                ('journal_id.type', '=', 'sale')])
+        if self.purchase_lock_date:
+            lock_date_domains.append([
+                ('date', '<=', self.purchase_lock_date),
+                ('journal_id.type', '=', 'purchase')])
+        return [
+            ('company_id', 'child_of', self.env.company.id),
+            ('state', '=', 'draft'),
+            *expression.OR(lock_date_domains),
+        ]
+
+    @api.depends('fiscalyear_lock_date', 'tax_lock_date', 'sale_lock_date', 'purchase_lock_date', 'hard_lock_date')
+    def _compute_show_draft_entries_warning(self):
+        for wizard in self:
+            draft_entries = self.env['account.move'].search(self._get_draft_moves_in_locked_period_domain(), limit=1)
+            wizard.show_draft_entries_warning = bool(draft_entries)
+
+    def _get_changes_needing_exception(self):
+        self.ensure_one()
         return {
-            'period_lock_date': self.period_lock_date,
-            'fiscalyear_lock_date': self.fiscalyear_lock_date,
-            'tax_lock_date': self.tax_lock_date,
+            field: self[field]
+            for field in SOFT_LOCK_DATE_FIELDS
+            if self.env.company[field] and (not self[field] or self[field] < self.env.company[field])
         }
+
+    @api.depends(*SOFT_LOCK_DATE_FIELDS)
+    def _compute_exception_needed(self):
+        for wizard in self:
+            wizard.exception_needed = bool(wizard._get_changes_needing_exception())
+
+    def _prepare_lock_date_values(self, exception_values=None):
+        """
+        Return a dictionary (lock date field -> field value)
+        It only contains lock dates which are changed and for which no exception is added
+        """
+        self.ensure_one()
+        if self.env.company.hard_lock_date and (not self.hard_lock_date or self.hard_lock_date < self.env.company.hard_lock_date):
+            raise UserError(_('It is not possible to decrease or remove the Hard Lock Date.'))
+
+        lock_date_values = {
+            field: self[field]
+            for field in LOCK_DATE_FIELDS
+            if self[field] != self.env.company[field]
+        }
+
+        for field, lock_date in lock_date_values.items():
+            if lock_date and lock_date > fields.Date.context_today(self):
+                raise UserError(_('You cannot set a Lock Date in the future.'))
+
+        # We do not change fields for which we add an exception
+        if exception_values:
+            for field in LOCK_DATE_FIELDS:
+                if field in exception_values:
+                    lock_date_values.pop(field, None)
+
+        return lock_date_values
+
+    def _prepare_exception_values(self):
+        self.ensure_one()
+        changes_needing_exception = self._get_changes_needing_exception()
+
+        if not changes_needing_exception:
+            return False
+
+        # Exceptions for everyone and forever are just "normal" changes to the lock date.
+        if self.exception_applies_to == 'everyone' and self.exception_duration == 'forever':
+            return False
+
+        exception_values = {
+            'company_id': self.env.company.id,
+            **changes_needing_exception
+        }
+
+        exception_errors = []
+        if {field: value for field, value in changes_needing_exception.items() if not value}:
+            exception_errors.append(_('You cannot remove a lock date with an exception.'))
+        if not self.exception_applies_to:
+            exception_errors.append(_('You need to select who the exception applies to.'))
+        if not self.exception_duration:
+            exception_errors.append(_('You need to select a duration for the exception.'))
+        if exception_errors:
+            raise UserError('\n'.join(exception_errors))
+
+        exception_values['user_id'] = {
+            'me': self.env.user.id,
+            'everyone': False,
+        }[self.exception_applies_to]
+
+        exception_duration = {
+            'me': 'forever',
+            'everyone': self.exception_duration,
+        }[self.exception_applies_to]
+
+        exception_timedelta = {
+            '5min': timedelta(minutes=5),
+            '15min': timedelta(minutes=15),
+            '1h': timedelta(hours=1),
+            '24h': timedelta(hours=24),
+            'forever': False,
+        }[exception_duration]
+        if exception_timedelta:
+            exception_values['end_datetime'] = fields.Datetime.now() + exception_timedelta
+
+        if self.exception_reason:
+            exception_values['reason'] = self.exception_reason
+
+        return exception_values
 
     def _get_current_period_dates(self, lock_date_field):
         """ Gets the date_from - either the previous lock date or the start of the fiscal year.
         """
+        self.ensure_one()
         company_lock_date = self.env.company[lock_date_field]
         if company_lock_date:
             date_from = company_lock_date + timedelta(days=1)
@@ -47,22 +265,85 @@ class AccountChangeLockDate(models.TransientModel):
         # to be overriden
         pass
 
+    def _change_lock_date(self, lock_date_values=None):
+        self.ensure_one()
+        if lock_date_values is None:
+            lock_date_values = self._prepare_lock_date_values()
+
+        # Possibly create default report external values for tax
+        tax_lock_date = lock_date_values.get('tax_lock_date', None)
+        if tax_lock_date and tax_lock_date != self.env.company['tax_lock_date']:
+            self._create_default_report_external_values('tax_lock_date')
+
+        # Possibly create default report external values for fiscal year
+        fiscalyear_lock_date = lock_date_values.get('fiscalyear_lock_date', None)
+        hard_lock_date = lock_date_values.get('hard_lock_date', None)
+        if fiscalyear_lock_date or hard_lock_date:
+            fiscal_lock_date, field = max([
+                (fiscalyear_lock_date, 'fiscalyear_lock_date'),
+                (hard_lock_date, 'hard_lock_date'),
+            ], key=lambda t: t[0] or date.min)
+            company_fiscal_lock_date = max(
+                self.env.company.fiscalyear_lock_date or date.min,
+                self.env.company.hard_lock_date or date.min,
+            )
+            if fiscal_lock_date != company_fiscal_lock_date:
+                self._create_default_report_external_values(field)
+
+        self.env.company.sudo().write(lock_date_values)
+
     def change_lock_date(self):
+        self.ensure_one()
         if self.env.user.has_group('account.group_account_manager'):
-            if any(
-                    lock_date > fields.Date.context_today(self)
-                    for lock_date in (
-                            self.fiscalyear_lock_date,
-                            self.tax_lock_date,
-                    )
-                    if lock_date
-            ):
-                raise UserError(_('You cannot set a lock date in the future.'))
-            if self.fiscalyear_lock_date and self.fiscalyear_lock_date != self.env.company.fiscalyear_lock_date:
-                self._create_default_report_external_values('fiscalyear_lock_date')
-            if self.tax_lock_date and self.tax_lock_date != self.env.company.tax_lock_date:
-                self._create_default_report_external_values('tax_lock_date')
-            self.env.company.sudo().write(self._prepare_lock_date_values())
+            exception_values = self._prepare_exception_values()
+            changed_lock_date_values = self._prepare_lock_date_values(exception_values=exception_values)
+
+            if exception_values:
+                self.env['account.lock_exception'].create(exception_values)
+
+            self._change_lock_date(changed_lock_date_values)
         else:
             raise UserError(_('Only Billing Administrators are allowed to change lock dates!'))
         return {'type': 'ir.actions.act_window_close'}
+
+    def action_show_draft_moves_in_locked_period(self):
+        self.ensure_one()
+        return {
+            'view_mode': 'tree',
+            'name': _('Draft Entries'),
+            'res_model': 'account.move',
+            'type': 'ir.actions.act_window',
+            'domain': self._get_draft_moves_in_locked_period_domain(),
+            'search_view_id': [self.env.ref('account.view_account_move_filter').id, 'search'],
+            'views': [[self.env.ref('account.view_move_tree_multi_edit').id, 'list'], [self.env.ref('account.view_move_form').id, 'form']],
+        }
+
+    def action_reopen_wizard(self):
+        # This action can be used to keep the wizard open after doing something else
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': self._name,
+            'res_id': self.id,
+            'view_mode': 'form',
+            'target': 'new',
+        }
+
+    def _action_revoke_min_exception(self, exception_field):
+        self.ensure_one()
+        exception = self[exception_field]
+        if exception:
+            exception.action_revoke()
+            self._compute_lock_date_exceptions()
+        return self.action_reopen_wizard()
+
+    def action_revoke_min_sale_lock_date_exception(self):
+        return self._action_revoke_min_exception('min_sale_lock_date_exception')
+
+    def action_revoke_min_purchase_lock_date_exception(self):
+        return self._action_revoke_min_exception('min_purchase_lock_date_exception')
+
+    def action_revoke_min_tax_lock_date_exception(self):
+        return self._action_revoke_min_exception('min_tax_lock_date_exception')
+
+    def action_revoke_min_fiscalyear_lock_date_exception(self):
+        return self._action_revoke_min_exception('min_fiscalyear_lock_date_exception')

commit b4012a6bc98234d16a6e038308b9068e9bee5565

[FIX] account_online_synchronization: Fix bank connection
Commit https://github.com/odoo/enterprise/commit/08a818bef6150c2877cdf64a7f4aa05ea14449e9 introduced a bug by setting journals last_sync to 1-1-1 when no lockdate is set.
This would cause the first synchronization to fail because:
- a start date would be set which is not wanted for first sync
- the start date would be 1-1-1 which can cause error with some providers

Reproduce bug:
- Connect Saltedge "Fake Bank OAuth with Pending Transactions"
- Select Account

closes odoo/enterprise#70360

Signed-off-by: Florian Gilbert (flg) <flg@odoo.com>

account_online_synchronization/models/account_online.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import base64
+import datetime
 import requests
 import logging
 import re
@@ -129,7 +130,8 @@ class AccountOnlineAccount(models.Model):
         self.account_online_link_id._get_consent_expiring_date()
 
         # Set last_sync date (date of latest statement or accounting lock date or False)
-        last_sync = self.env.company._get_user_fiscal_lock_date(journal)
+        lock_date = self.env.company._get_user_fiscal_lock_date(journal)
+        last_sync = lock_date if lock_date and lock_date > datetime.date.min else None
         bnk_stmt_line = self.env['account.bank.statement.line'].search([('journal_id', 'in', self.journal_ids.ids)], order="date desc", limit=1)
         if bnk_stmt_line:
             last_sync = bnk_stmt_line.date