Raniani-lab/enterpriise

commit 93d09071a08bd5de948886729d827b1c46e907d9

[FIX] website_sale_renting: fix renting dates on product page
Before this commit, some tools of website_sale_renting were broken due
to a loading problem of the overrides of the Variant mixin. For
instance, the price on the product page was not depending on the rental
period anymore. We now force them to be executed by prepending the js
files in the manifest.

Furthermore, the dates were not always given in the context. In this
case, the fallback is on the cart dates, if any.

task-3570178

closes odoo/enterprise#49610

Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

website_sale_renting/__manifest__.py
@@ -24,7 +24,7 @@ appropriate views and selling choices.
     'assets': {
         'web.assets_frontend': [
             'website_sale_renting/static/src/scss/*.scss',
-            'website_sale_renting/static/src/js/*.js',
+            ('prepend', 'website_sale_renting/static/src/js/*.js'),
         ],
         'web.assets_tests': [
             'website_sale_renting/static/tests/tours/**/*',

website_sale_renting/models/product_template.py
@@ -49,8 +49,9 @@ class ProductTemplate(models.Model):
             return res
 
         # Compute best pricing rule or set default
-        start_date = self.env.context.get('start_date')
-        end_date = self.env.context.get('end_date')
+        order = website.sale_get_order() if website and request else self.env['sale.order']
+        start_date = self.env.context.get('start_date') or order.rental_start_date
+        end_date = self.env.context.get('end_date') or order.rental_return_date
         if start_date and end_date:
             current_pricing = product_or_template._get_best_pricing_rule(
                 start_date=start_date,
@@ -81,7 +82,7 @@ class ProductTemplate(models.Model):
         )
 
         default_start_date, default_end_date = self._get_default_renting_dates(
-            start_date, end_date, website, current_duration, current_unit
+            start_date, end_date, current_duration, current_unit
         )
 
         ratio = ceil(current_duration) / pricing.recurrence_id.duration if pricing.recurrence_id.duration else 1
@@ -145,14 +146,11 @@ class ProductTemplate(models.Model):
         }
 
     @api.model
-    def _get_default_renting_dates(
-        self, start_date, end_date, website, duration, unit
-    ):
+    def _get_default_renting_dates(self, start_date, end_date, duration, unit):
         """ Get default renting dates to help user
 
         :param datetime start_date: a start_date which is directly returned if defined
         :param datetime end_date: a end_date which is directly returned if defined
-        :param Website website: the website currently browsed by the user
         :param int duration: the duration expressed in int, in the unit given
         :param string unit: The duration unit, which can be 'hour', 'day', 'week' or 'month'
         """
@@ -161,11 +159,6 @@ class ProductTemplate(models.Model):
 
         if start_date or end_date:
             return start_date, end_date
-
-        if website and request:
-            order = website.sale_get_order()
-            if order.has_rented_products:
-                return order.rental_start_date, order.rental_return_date
 
         default_date = self._get_default_start_date()
         return default_date, self._get_default_end_date(default_date, duration, unit)

Eddy-C127/enterprise18

commit cd0ccd642f50c216eebf8dbec332cc81063caa53

[FIX] website_sale_renting: correctly add files to bundle
Current behaviour:
When you click on the "request a document" link,
then upload a document, you get a CSRF error

Steps to reproduce:
1. Install documents
2. In documents, click the arrow next to "new", then "request"
3. Add a name, and a "request to", then click "request"
4. Go to messages, copy the link in the generated email
5. Paste the link in an incognito window
6. Upload a document, CSRF error

Cause of the issue:
The js file that adds the CSRF token (documents_public_pages.js)
could not be loaded due to an error caused by website_sale_renting:
TypeError: odoo.define is not a function

The mixin file was loaded before the odoo module loader.
Caused by: https://github.com/odoo/enterprise/commit/93d09071a08bd5de948886729d827b1c46e907d9

Fix:
Put js code from website_sale_renting before owl.js
which is right after assets_frontend_minimal,
containing module_loader.js

opw-4138333

closes odoo/enterprise#70577

Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

website_sale_renting/__manifest__.py
@@ -24,7 +24,7 @@ appropriate views and selling choices.
     'assets': {
         'web.assets_frontend': [
             'website_sale_renting/static/src/scss/*.scss',
-            ('prepend', 'website_sale_renting/static/src/js/*.js'),
+            ('before', 'website_sale/static/src/js/website_sale.js', 'website_sale_renting/static/src/js/*.js'),
         ],
         'web.assets_tests': [
             'website_sale_renting/static/tests/tours/**/*',