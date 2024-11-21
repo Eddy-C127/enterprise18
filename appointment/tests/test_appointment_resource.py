Eddy-C127/enterprise18

commit 064e0da976edd7ef501e4fb33d4d579eaab85a3d

[IMP] appointment: gantt view grouped by appointment_resource_ids
Backport of dea2f65fcb106848c9f1985e3d49f177c597fe71
The appointment_resource_id field has been kept and made unused fields
in view invisible to avoid breaking custom views and be stable compliant.

We now group the gantt view by resource_ids to manage
appointment type where multiple resources can be chosen for the
booking.

It is now possible to manage appointment types of restaurant type
(aka with manage capacity and multi resources).

Computations are now managed with the computed resource_ids field
and store with the appointment_resource_ids field through the
appointment_booking_line table.

The capacity distribution between resources is simplified and takes
the shared resources in last.

task-3812927

closes odoo/enterprise#59003

Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>

appointment/models/calendar_event.py
@@ -6,6 +6,7 @@ import logging
 from datetime import datetime, timedelta
 
 from odoo import _, api, Command, fields, models, SUPERUSER_ID
+from odoo.exceptions import ValidationError
 from odoo.tools import html2plaintext, email_normalize, email_split_tuples
 from odoo.addons.resource.models.utils import Intervals, timezone_datetime
 from ..utils import interval_from_events, intervals_overlap
@@ -27,12 +28,12 @@ class CalendarEvent(models.Model):
         if res.get('stop') and isinstance(res['stop'], datetime) and res['stop'].second != 0:
             res['stop'] = datetime.min + round((res['stop'] - datetime.min) / timedelta(minutes=1)) * timedelta(minutes=1)
         user_id = res.get('user_id')
-        resource_id = res.get('appointment_resource_id') or self.env.context.get('default_appointment_resource_id')
+        resource_ids = self.env.context.get('default_resource_ids', [])
         # get a relevant appointment type for ease of use when coming from a view that groups by resource
         if not res.get('appointment_type_id') and 'appointment_type_id' in fields_list:
             appointment_types = False
-            if resource_id:
-                appointment_types = self.env['appointment.resource'].browse(resource_id).appointment_type_ids
+            if resource_ids:
+                appointment_types = self.env['appointment.resource'].browse(resource_ids).appointment_type_ids
             elif user_id:
                 appointment_types = self.env['appointment.type'].search([('staff_user_ids', 'in', user_id)])
             if appointment_types:
@@ -54,10 +55,19 @@ class CalendarEvent(models.Model):
     appointment_type_schedule_based_on = fields.Selection(related="appointment_type_id.schedule_based_on")
     appointment_type_manage_capacity = fields.Boolean(related="appointment_type_id.resource_manage_capacity")
     appointment_invite_id = fields.Many2one('appointment.invite', 'Appointment Invitation', readonly=True, ondelete='set null')
+    # currently unused but kept because of stable constraint, properly removed by: https://github.com/odoo/enterprise/commit/dea2f65fcb106848c9f1985e3d49f177c597fe71
     appointment_resource_id = fields.Many2one('appointment.resource', string="Appointment Resource",
                                               compute="_compute_appointment_resource_id", inverse="_inverse_appointment_resource_id_or_capacity",
                                               store=True, group_expand="_read_group_appointment_resource_id")
-    appointment_resource_ids = fields.Many2many('appointment.resource', string="Appointment Resources", compute="_compute_resource_ids")
+    appointment_resource_ids = fields.Many2many('appointment.resource', 'appointment_booking_line', 'calendar_event_id', 'appointment_resource_id',
+                                                string="Appointment Resources", group_expand="_read_group_appointment_resource_id",
+                                                depends=['booking_line_ids'], readonly=True)
+    # This field is used in the form view to create/manage the booking lines based on the resource_total_capacity_reserved
+    # selected. This allows to have the appointment_resource_ids field linked to the appointment_booking_line model and
+    # thus avoid the duplication of information.
+    resource_ids = fields.Many2many('appointment.resource', string="Resources",
+                                    compute="_compute_resource_ids", inverse="_inverse_resource_ids_or_capacity",
+                                    group_expand="_read_group_appointment_resource_id")
     booking_line_ids = fields.One2many('appointment.booking.line', 'calendar_event_id', string="Booking Lines")
     partner_ids = fields.Many2many('res.partner', group_expand="_read_group_partner_ids")
     resource_total_capacity_reserved = fields.Integer('Total Capacity Reserved', compute="_compute_resource_total_capacity", inverse="_inverse_appointment_resource_id_or_capacity")
@@ -71,6 +81,12 @@ class CalendarEvent(models.Model):
          "CHECK(appointment_resource_id IS NULL OR (appointment_resource_id IS NOT NULL AND appointment_type_id IS NOT NULL))",
          "An event cannot book resources without an appointment type.")
     ]
+
+    @api.constrains('appointment_resource_ids', 'appointment_type_id')
+    def _check_resource_and_appointment_type(self):
+        for event in self:
+            if event.appointment_resource_ids and not event.appointment_type_id:
+                raise ValidationError(_("The event %s cannot book resources without an appointment type.", event.name))
 
     @api.depends('appointment_type_id')
     def _compute_alarm_ids(self):
@@ -86,10 +102,10 @@ class CalendarEvent(models.Model):
             else:
                 event.appointment_resource_id = False
 
-    @api.depends('booking_line_ids')
+    @api.depends('booking_line_ids', 'booking_line_ids.appointment_resource_id')
     def _compute_resource_ids(self):
         for event in self:
-            event.appointment_resource_ids = event.booking_line_ids.appointment_resource_id
+            event.resource_ids = event.booking_line_ids.appointment_resource_id
 
     @api.depends('start', 'stop', 'appointment_resource_ids', 'appointment_resource_id')
     def _compute_resources_on_leave(self):
@@ -179,20 +195,68 @@ class CalendarEvent(models.Model):
                 })
             elif len(event.booking_line_ids) == 1 and event.appointment_resource_id:
                 event.booking_line_ids.appointment_resource_id = event.appointment_resource_id
-                event.booking_line_ids.capacity_reserved = event.resource_total_capacity_reserved
+                event.booking_line_ids.capacity_reserved = min(
+                    event.resource_total_capacity_reserved or event.booking_line_ids.capacity_reserved,
+                    event.appointment_resource_id.capacity
+                )
             elif len(event.booking_line_ids) == 1:
                 event.booking_line_ids.unlink()
+
+    def _inverse_resource_ids_or_capacity(self):
+        """Update booking lines as inverse of both resource capacity and resource_ids.
+
+        As both values are related to the booking line and resource capacity is dependant
+        on resources existing in the first place. They need to both use the same inverse
+        field to ensure there is no ordering conflict.
+        """
+        booking_lines = []
+        for event in self:
+            resources = event.resource_ids
+            if resources:
+                if event.appointment_type_manage_capacity and self.resource_total_capacity_reserved:
+                    capacity_to_reserve = self.resource_total_capacity_reserved
+                else:
+                    capacity_to_reserve = sum(event.booking_line_ids.mapped('capacity_reserved')) or sum(resources.mapped('capacity'))
+                event.booking_line_ids.unlink()
+                for resource in resources.sorted("shareable"):
+                    if capacity_to_reserve <= 0:
+                        break
+                    booking_lines.append({
+                        'appointment_resource_id': resource.id,
+                        'calendar_event_id': event.id,
+                        'capacity_reserved': min(resource.capacity, capacity_to_reserve),
+                    })
+                    capacity_to_reserve -= min(resource.capacity, capacity_to_reserve)
+            else:
+                event.booking_line_ids.unlink()
+        self.env['appointment.booking.line'].create(booking_lines)
+
+    @api.model
+    def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
+        """ Simulate group_by on resource_ids by using appointment_resource_ids.
+            appointment_resource_ids is only used to store the data through the appointment_booking_line
+            table. All computation on the resources and the capacity reserved is done with capacity_reserved.
+            Simulating the group_by on resource_ids also avoids to do weird override in JS on appointment_resource_ids.
+            This is needed because when simply writing on the field, it tries to create the corresponding booking line
+            with the field capacity_reserved required leading to ValidationError.
+        """
+        groupby = [group_element if group_element != "resource_ids" else "appointment_resource_ids" for group_element in groupby]
+        read_group_data = super().read_group(domain, fields, groupby, offset, limit, orderby, lazy)
+        for data in read_group_data:
+            if 'appointment_resource_ids' in data:
+                data['resource_ids'] = data['appointment_resource_ids']
+            if 'appointment_resource_ids_count' in data:
+                data['resource_ids_count'] = data['appointment_resource_ids_count']
+        return read_group_data
 
     def _read_group_appointment_resource_id(self, resources, domain, order):
         if not self.env.context.get('appointment_booking_gantt_show_all_resources'):
             return resources
-        # Assume shared resources will be used with multi-resource bookings -> hide
-        filter_shared_resources = [('appointment_type_ids.resource_manage_capacity', '=', False)]
         # If we have a default appointment type, we only want to show those resources
         default_appointment_type = self.env.context.get('default_appointment_type_id')
         if default_appointment_type:
-            return self.env['appointment.type'].browse(default_appointment_type).resource_ids.filtered_domain(filter_shared_resources)
-        return self.env['appointment.resource'].search(filter_shared_resources)
+            return self.env['appointment.type'].browse(default_appointment_type).resource_ids
+        return self.env['appointment.resource'].search([])
 
     def _read_group_partner_ids(self, partners, domain, order):
         """Show the partners associated with relevant staff users in appointment gantt context."""
@@ -290,6 +354,7 @@ class CalendarEvent(models.Model):
             'appointment_resource_id',
             'appointment_resource_ids',
             'appointment_type_id',
+            'resource_ids',
             'resource_total_capacity_reserved',
             'resource_total_capacity_used',
         }
@@ -351,7 +416,7 @@ class CalendarEvent(models.Model):
     def gantt_unavailability(self, start_date, end_date, scale, group_bys=None, rows=None):
         # skip if not dealing with appointments
         resource_ids = [row['resId'] for row in rows if row.get('resId')]  # remove empty rows
-        if not group_bys or group_bys[0] not in ('appointment_resource_id', 'partner_ids') or not resource_ids:
+        if not group_bys or group_bys[0] not in ('resource_ids', 'partner_ids') or not resource_ids:
             return super().gantt_unavailability(start_date, end_date, scale, group_bys=group_bys, rows=rows)
 
         start_datetime = fields.Datetime.from_string(start_date)

appointment/tests/test_appointment_resource.py
@@ -69,7 +69,7 @@ class AppointmentResource(AppointmentCommon):
         for resource, default_type, expected_type_id in states:
             context = {
                 'booking_gantt_create_record': True,
-                'default_appointment_resource_id': resource.id
+                'default_resource_ids': resource.ids
             }
             if default_type:
                 context.update(default_appointment_type_id=default_type.id)

appointment/i18n/appointment.pot
@@ -2531,12 +2531,14 @@ msgstr ""
 #. module: appointment
 #: model:ir.actions.act_window,name:appointment.appointment_resource_action
 #: model:ir.model.fields,field_description:appointment.field_appointment_invite__resource_ids
+#: model:ir.model.fields,field_description:appointment.field_calendar_event__resource_ids
 #: model:ir.model.fields.selection,name:appointment.selection__appointment_type__schedule_based_on__resources
 #: model:ir.ui.menu,name:appointment.menu_appointment_resource
 #: model_terms:ir.ui.view,arch_db:appointment.appointment_type_view_form
 #: model_terms:ir.ui.view,arch_db:appointment.appointment_type_view_tree
 #: model_terms:ir.ui.view,arch_db:appointment.appointment_type_view_tree_invitation
 #: model_terms:ir.ui.view,arch_db:appointment.appointment_validated
+#: model_terms:ir.ui.view,arch_db:appointment.calendar_event_view_form_gantt_booking
 #: model_terms:ir.ui.view,arch_db:appointment.calendar_event_view_gantt_booking_resource
 msgid "Resources"
 msgstr ""
@@ -2981,6 +2983,13 @@ msgstr ""
 #. module: appointment
 #: model:ir.model.constraint,message:appointment.constraint_appointment_slot_check_start_and_end_hour
 msgid "The end time must be later than the start time."
+msgstr ""
+
+#. module: appointment
+#. odoo-python
+#: code:addons/appointment/models/calendar_event.py:0
+#, python-format
+msgid "The event %s cannot book resources without an appointment type."
 msgstr ""
 
 #. module: appointment

appointment/models/appointment_type.py
@@ -386,10 +386,7 @@ class AppointmentType(models.Model):
                 management_views = ['gantt']
         else:
             action = self.env["ir.actions.actions"]._for_xml_id("appointment.calendar_event_action_view_bookings_resources")
-            if self.resource_manage_capacity:
-                management_views = ['tree']
-            else:
-                management_views = ['gantt']
+            management_views = ['gantt']
         appointments = self.meeting_ids.filtered_domain([
             ('start', '>=', datetime.today())
         ])
@@ -491,7 +488,7 @@ class AppointmentType(models.Model):
         now = datetime.utcnow()
         last_meeting_values = self.env['calendar.event'].search_read([
             ('appointment_type_id', 'in', self.ids),
-            ('appointment_resource_id', '!=', False),
+            ('appointment_resource_ids', '!=', False),
             ('stop', '>=', now.date()),
         ], ['stop'], limit=1, order='stop desc')
         last_meeting_end = last_meeting_values[0]['stop'] if last_meeting_values else False

appointment/static/src/views/gantt/gantt_model.js
@@ -75,13 +75,27 @@ export class AppointmentBookingGanttModel extends GanttModel {
             this.metaData.groupedBy &&
             this.metaData.groupedBy[0] === "partner_ids" &&
             schedule.partner_ids &&
-            data.partner_ids[0] != schedule.originId // attendee_ids will be messed up without this check
+            data.partner_ids[0] !== schedule.originId // attendee_ids will be messed up without this check
         ) {
             return {
                 ...data,
                 partner_ids: [
                     [3, schedule.originId, 0],
                     [4, data.partner_ids[0], 0],
+                ],
+            };
+        }
+        if (
+            this.metaData.groupedBy &&
+            this.metaData.groupedBy[0] === "resource_ids" &&
+            schedule.resource_ids &&
+            data.resource_ids[0] !== schedule.originId
+        ) {
+            return {
+                ...data,
+                resource_ids: [
+                    [3, schedule.originId, 0],
+                    [4, data.resource_ids[0], 0],
                 ],
             };
         }

appointment/static/src/views/gantt/gantt_renderer.js
@@ -91,7 +91,7 @@ export class AppointmentBookingGanttRenderer extends GanttRenderer {
      * Display 'Add Leaves' action button if grouping by appointment resources.
      */
     get showAddLeaveButton() {
-        return !!(this.model.metaData.groupedBy && this.model.metaData.groupedBy[0] === 'appointment_resource_id');
+        return !!(this.model.metaData.groupedBy && this.model.metaData.groupedBy[0] === 'resource_ids');
     }
 
     /**
@@ -102,7 +102,7 @@ export class AppointmentBookingGanttRenderer extends GanttRenderer {
      */
     async dragPillDrop({ pill, cell, diff }) {
         let unpatch = null;
-        if (this.model.metaData.groupedBy && this.model.metaData.groupedBy[0] === "partner_ids") {
+        if (this.model.metaData.groupedBy && (this.model.metaData.groupedBy[0] === "partner_ids" || this.model.metaData.groupedBy[0] === "resource_ids")) {
             const originResId = this.rows.find((row) => {
                 return row.pills.some(
                     (rowPill) => rowPill.id === this.pills[pill.dataset.pillId].id,

appointment/tests/test_appointment_ui.py
@@ -207,7 +207,7 @@ class AppointmentUITest(AppointmentUICommon):
             'default_partner_ids': [],
             'initial_date': now,
         }, {
-            'appointment_booking_gantt_domain': [('appointment_resource_id', '!=', False)],
+            'appointment_booking_gantt_domain': [('appointment_resource_ids', '!=', False)],
             'appointment_default_add_organizer_to_attendees': False,
             'default_scale': 'day',
             'default_appointment_type_id': appointment_types[2].id,

appointment/views/calendar_event_views.xml
@@ -8,11 +8,9 @@
         <field name="arch" type="xml">
             <field name="categ_ids" position="after">
                 <field name="appointment_type_id"/>
-                <field name="appointment_resource_id"
-                       invisible="not appointment_type_id or not appointment_resource_id"
+                <field name="appointment_resource_id" invisible="1"
                        domain="[('appointment_type_ids', 'in', appointment_type_id)]"/>
-                <field name="resource_total_capacity_reserved"
-                       invisible="not appointment_resource_id"/>
+                <field name="resource_total_capacity_reserved" invisible="1"/>
             </field>
             <xpath expr="//page[@name='page_invitations']" position="before">
                 <page name="page_appointment" string="Appointment Details"
@@ -121,9 +119,12 @@
                             <field name="appointment_type_manage_capacity" invisible="1"/>
                             <field name="appointment_type_schedule_based_on" invisible="1"/>
                             <field name="appointment_resource_id" string="Resource" options="{'no_create': True}"
+                                    domain="[('appointment_type_ids', 'in', appointment_type_id)]"/>
+                            <field name="appointment_resource_ids" invisible="1"/>
+                            <field name="resource_ids" string="Resources" options="{'no_create': True}"
                                    invisible="not appointment_type_id or appointment_type_schedule_based_on != 'resources'"
-                                   domain="[('appointment_type_ids', 'in', appointment_type_id)]"/>
-                            <div invisible="not appointment_resource_id or not appointment_type_manage_capacity or appointment_type_schedule_based_on != 'resources'" colspan="2">
+                                   domain="[('appointment_type_ids', 'in', appointment_type_id)]" widget="many2many_tags"/>
+                            <div invisible="not resource_ids or not appointment_type_manage_capacity or appointment_type_schedule_based_on != 'resources'" colspan="2">
                                 <label for="resource_total_capacity_reserved" class="me-5"/>
                                 <field name="resource_total_capacity_reserved" string="Total Reserved" class="oe_inline"/>
                                 <span>people</span>
@@ -144,7 +145,7 @@
                 create="1" delete="1" plan="0"
                 date_start="start"
                 date_stop="stop"
-                default_group_by="appointment_resource_id"
+                default_group_by="resource_ids"
                 default_scale="day"
                 display_unavailability="1"
                 form_view_id="%(calendar_event_view_form_gantt_booking)d"
@@ -215,7 +216,7 @@
             (0, 0, {'view_mode': 'tree', 'view_id': ref('appointment.calendar_event_view_tree_booking')}),
         ]"/>
         <field name="view_mode">gantt,tree,form</field>
-        <field name="context">{'appointment_booking_gantt_domain': [('appointment_resource_id', '!=', False)],
+        <field name="context">{'appointment_booking_gantt_domain': [('appointment_resource_ids', '!=', False)],
                                'appointment_default_add_organizer_to_attendees': False,
                                'default_partner_ids': [],
                                'default_resource_total_capacity_reserved': 1}</field>

commit 2bd752f912c74c720ae7aeb8f9ec07bac6edcef6

[FIX] appointment: cannot duplicate resource bookings
Current behaviour:
When trying to duplicate a resource booking containing
a resource, you get a Validation Error for a missing field.

Expected behaviour:
Ability to duplicate a resource booking

Steps to reproduce:
1. Go to Appointments
2. Select a resource appointments (ie. Tennis Court)
3. Click on new (To make a new resource booking)
4. Set a name, attendee, AND resource (ie. Court 1)
5. Go to list view, and open newly made booking
6. Click on the cog > Duplicate
7. Validation Error

Cause of the issue:
Caused by: https://github.com/odoo/enterprise/commit/064e0da976edd7ef501e4fb33d4d579eaab85a3d
When duplicating a calendar.event, at the field appointment_resource_ids
the relation table appointment_booking_line is used, on the fields
calendar_event_id and appointment_resource_id. However, the model
appointment_booking_line has another required field capacity_reserved.
So when inserting into the table:
https://github.com/odoo/odoo/blob/b64a507697381fd7bb205f4a2b2217322d31811a/odoo/fields.py#L4945
Only the two specified columns/fields are set, but not the third
required field capacity_reserved.
Which causes a SQL error "violates not-null constraint"

opw-4187159

closes odoo/enterprise#71489

Signed-off-by: Antoine Demany (ande) <ande@odoo.com>

appointment/models/calendar_event.py
@@ -67,19 +67,20 @@ class CalendarEvent(models.Model):
     # currently unused but kept because of stable constraint, properly removed by: https://github.com/odoo/enterprise/commit/dea2f65fcb106848c9f1985e3d49f177c597fe71
     appointment_resource_id = fields.Many2one('appointment.resource', string="Appointment Resource",
                                               compute="_compute_appointment_resource_id", inverse="_inverse_appointment_resource_id_or_capacity",
-                                              store=True, group_expand="_read_group_appointment_resource_id")
+                                              store=True, group_expand="_read_group_appointment_resource_id", copy=False)
     appointment_resource_ids = fields.Many2many('appointment.resource', 'appointment_booking_line', 'calendar_event_id', 'appointment_resource_id',
                                                 string="Appointment Resources", group_expand="_read_group_appointment_resource_id",
-                                                depends=['booking_line_ids'], readonly=True)
+                                                depends=['booking_line_ids'], readonly=True, copy=False)
     # This field is used in the form view to create/manage the booking lines based on the resource_total_capacity_reserved
     # selected. This allows to have the appointment_resource_ids field linked to the appointment_booking_line model and
     # thus avoid the duplication of information.
     resource_ids = fields.Many2many('appointment.resource', string="Resources",
                                     compute="_compute_resource_ids", inverse="_inverse_resource_ids_or_capacity",
-                                    group_expand="_read_group_appointment_resource_id")
-    booking_line_ids = fields.One2many('appointment.booking.line', 'calendar_event_id', string="Booking Lines")
+                                    group_expand="_read_group_appointment_resource_id", copy=False)
+    booking_line_ids = fields.One2many('appointment.booking.line', 'calendar_event_id', string="Booking Lines", copy=True)
     partner_ids = fields.Many2many('res.partner', group_expand="_read_group_partner_ids")
-    resource_total_capacity_reserved = fields.Integer('Total Capacity Reserved', compute="_compute_resource_total_capacity", inverse="_inverse_appointment_resource_id_or_capacity")
+    resource_total_capacity_reserved = fields.Integer('Total Capacity Reserved', compute="_compute_resource_total_capacity",
+                                                      inverse="_inverse_appointment_resource_id_or_capacity", copy=True)
     resource_total_capacity_used = fields.Integer('Total Capacity Used', compute="_compute_resource_total_capacity")
     user_id = fields.Many2one('res.users', group_expand="_read_group_user_id")
     videocall_redirection = fields.Char('Meeting redirection URL', compute='_compute_videocall_redirection')

appointment/tests/test_appointment_resource.py
@@ -1117,6 +1117,45 @@ class AppointmentResourceBookingTest(AppointmentCommon):
         self.assertEqual(len(resource_slots), 0, "Once a resource is booked on a slot, it should not be available anymore to other appointment types.")
 
     @users('apt_manager')
+    def test_appointment_resources_duplicate(self):
+        """ Check that we can correctly duplicate resource bookings """
+        table, other_table = self.env["appointment.resource"].create([{
+            'appointment_type_ids': self.apt_type_resource.ids,
+            'capacity': 2,
+            'name': 'Table for 2',
+            'sequence': 1,
+        }, {
+            'appointment_type_ids': self.apt_type_resource.ids,
+            'capacity': 4,
+            'name': 'Table for 4',
+            'sequence': 2,
+        }])
+
+        start = datetime(2022, 2, 14, 15, 0, 0)
+        end = start + timedelta(hours=1)
+        booking = self.env['calendar.event'].with_context(self._test_context).create({
+            'appointment_type_id': self.apt_type_resource.id,
+            'booking_line_ids': [
+                (0, 0, {'appointment_resource_id': table.id, 'capacity_reserved': 2, 'capacity_used': 2}),
+                (0, 0, {'appointment_resource_id': other_table.id, 'capacity_reserved': 3}),
+            ],
+            'name': 'Resource booking',
+            'start': start,
+            'stop': end,
+        })
+
+        duplicate = booking.copy()
+        self.assertEqual(booking.name, duplicate.name, "Resource booking should have duplicated correctly")
+        self.assertListEqual(booking.resource_ids.ids, duplicate.resource_ids.ids, "Resources should be the same")
+        self.assertEqual(booking.resource_total_capacity_used, duplicate.resource_total_capacity_used, "Capacity used should be the same")
+        self.assertEqual(booking.resource_total_capacity_reserved, duplicate.resource_total_capacity_reserved, "Capacity reserved should be the same")
+        self.assertListEqual(
+            [bl.capacity_reserved for bl in booking.booking_line_ids],
+            [bl.capacity_reserved for bl in duplicate.booking_line_ids],
+            "Capacity reserved should be the same for each booking line",
+        )
+
+    @users('apt_manager')
     def test_appointment_resources_without_capacity_management(self):
         """ Check use case where capacity management is not activated """
 