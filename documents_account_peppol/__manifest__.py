ab12976668fe0229274f5145ff0635a24d59e849
Claire (clbr) <clbr@odoo.com>
2024-11-22 08:55:15 +0100

    [IMP] documents_account_peppol: Peppol - remove auto install of the document bridge
    Will be removed in master, in the meantime we stop auto-installing the module.
    
    task-4332306
    
    closes odoo/enterprise#74524
    
    Related: odoo/odoo#188500
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- documents_account_peppol/__manifest__.py
+++ documents_account_peppol/__manifest__.py
@@ -12,5 +12,4 @@ within the Documents app.
     "license": "OEEL-1",
     "depends": ["account_peppol", "documents"],
     "data": ["views/res_config_settings_views.xml"],
-    "auto_install": True,
 }
