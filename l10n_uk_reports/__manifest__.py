9f837afb5a092725d3a9e9cc786d476973a760b7
Logan Staelens <lost@odoo.com>
2024-12-09 15:52:13 +0000

    [FIX] l10n_uk_reports: Missing fields in the fraud prevention headers
    Before when sending request to hmrc we had a fraud prevention headers creation but only with part of the data needed, client_data was provided for one of the two request.
    Another problem was the ips. If we are in a private network we should still send the public IP.
    
    task-4387562
    
    closes odoo/enterprise#78310
    
    X-original-commit: 3a894c915d06b985859e1519b7614830dcb1259e
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>
    Signed-off-by: Logan Staelens (lost) <lost@odoo.com>

--- l10n_uk_reports/__manifest__.py
+++ l10n_uk_reports/__manifest__.py
@@ -30,7 +30,7 @@ MTD-VAT API to HMRC.
     ],
     'assets': {
         'web.assets_backend': [
-            "l10n_uk_reports/static/src/components/**/*",
+            "l10n_uk_reports/static/**/*",
             ],
     },
     'installable': True,
--- l10n_uk_reports/static/src/components/send_hmrc/send_hmrc.js
+++ l10n_uk_reports/static/src/components/send_hmrc/send_hmrc.js
@@ -3,11 +3,11 @@
 import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
+import { retrieveHMRCClientInfo } from "../../hmrc_api";
 
 import { Component } from "@odoo/owl";
 
 export class SendHmrcButton extends Component {
-
     setup() {
         this.orm = useService("orm");
         this.actionService = useService("action");
@@ -19,31 +19,20 @@ export class SendHmrcButton extends Component {
         this.env.services.ui.block();
         try {
             if (!localStorage.getItem('hmrc_gov_client_device_id')) {
-                localStorage.setItem('hmrc_gov_client_device_id', this.hmrc_gov_client_device_id);
+                localStorage.setItem('hmrc_gov_client_device_id', this.hmrcGovClientDeviceIdentifier);
             }
-
-            const clientInfo = {
-                'screen_width': screen.width,
-                'screen_height': screen.height,
-                'screen_scaling_factor': window.devicePixelRatio,
-                'screen_color_depth': screen.colorDepth,
-                'window_width': window.outerWidth,
-                'window_height': window.outerHeight,
-                'hmrc_gov_client_device_id': localStorage.getItem('hmrc_gov_client_device_id'),
-            }
-
+            let clientData = retrieveHMRCClientInfo();
+            clientData.hmrc_gov_client_device_id = localStorage.getItem('hmrc_gov_client_device_id');
             await this.orm.call(
                 'l10n_uk.vat.obligation',
                 'action_submit_vat_return',
-                [this.props.record.data.obligation_id[0], clientInfo]
+                [this.props.record.data.obligation_id[0], clientData]
             );
             this.actionService.doAction({'type': 'ir.actions.act_window_close'})
         } finally {
             this.env.services.ui.unblock();
         }
     }
-
-
 }
 
 export const sendHmrcButton = {
