049d07212e2a7f10b7237275b167f03de2be0041
Gauthier Wala (gawa) <gawa@odoo.com>
2024-12-19 15:17:24 +0000

    [FIX] l10n-de_reports: datev export attachments
    The 2 exports have the same name, which is impractical.
    Let's differentiate them
    
    task-4414223
    
    closes odoo/enterprise#75957
    
    X-original-commit: 9270e740ef51c02d210e2eefd59be10348307cdf
    Signed-off-by: John Laterre (jol) <jol@odoo.com>
    Signed-off-by: Wala Gauthier (gawa) <gawa@odoo.com>

--- l10n_de_reports/models/datev_export_csv.py
+++ l10n_de_reports/models/datev_export_csv.py
@@ -113,8 +113,10 @@ class GeneralLedgerCustomHandler(models.AbstractModel):
                                 self._l10n_de_datev_get_partner_list(options, set_move_line_ids, customer=False))
             buf.seek(0)
             content = buf.read()
+
+        filename, extension = report.get_default_report_filename(options, 'ZIP').split('.')
         return {
-            'file_name': report.get_default_report_filename(options, 'ZIP'),
+            'file_name': f'{filename}_atch.{extension}' if options.get('add_attachments') else f'{filename}_data.{extension}',
             'file_content': content,
             'file_type': 'zip'
         }
