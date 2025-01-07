95e939951dfe9e0c441c92e5793ed5e00e871ab0
Renaud Thiry <reth@odoo.com>
2025-01-07 12:54:05 +0000

    [IMP] website_appointment: support turnstile
    The previous commit implements captcha for this form
    
    Turnstile can be implemented on top of it by just invoking the turnstile script in js.
    
    As it's implicitly checked when recaptcha is checked, this also avoids any issue with the form potentially not working if turnstile is enabled.
    
    task-4335141
    
    closes odoo/enterprise#80613
    
    X-original-commit: 845b33e9073ff34b59da568cb32f65b8b4f24ea3
    Related: odoo/odoo#200049
    Signed-off-by: Jérémy Hennecart (jeh) <jeh@odoo.com>
    Signed-off-by: Renaud Thiry (reth) <reth@odoo.com>

--- website_appointment/static/src/js/appointment_frontend/appointment_form.js
+++ website_appointment/static/src/js/appointment_frontend/appointment_form.js
@@ -1,18 +1,25 @@
 /** @odoo-module **/
 
-import publicWidget from "web.public.widget";
-import { _t } from "web.core";
-import { ReCaptcha } from "google_recaptcha.ReCaptchaV3";
+import publicWidget from "@web/legacy/js/public/public_widget";
+import { ReCaptcha } from "@google_recaptcha/js/recaptcha";
+import { session } from "@web/session";
+import { _t } from "@web/core/l10n/translation";
 
 publicWidget.registry.appointmentForm.include({
     init: function () {
         this._super(...arguments);
         this._recaptcha = new ReCaptcha();
         this.notification = this.bindService("notification");
+        // dynamic get rather than import as we don't depend on this module
+        if (session.turnstile_site_key) {
+            const { turnStile } = odoo.loader.modules.get("@website_cf_turnstile/js/turnstile");
+            this._turnstile = turnStile;
+        }
     },
 
     willStart: async function () {
         this._recaptcha.loadLibs();
+        this._addTurnstile(document.querySelector("form.appointment_submit_form"));
         return this._super(...arguments);
     },
 
@@ -54,6 +61,25 @@ publicWidget.registry.appointmentForm.include({
             recaptchaTokenInput.setAttribute("value", tokenObj.token);
             form.appendChild(recaptchaTokenInput);
         }
+        return true;
+    },
+
+    _addTurnstile: function (form) {
+        if (!this._turnstile) {
+            return false;
+        }
+
+        const turnstileNodes = this._turnstile.addTurnstile("appointment_form_submission");
+        const turnstileContainer =
+            turnstileNodes[0].classList.contains("s_turnstile_container") && turnstileNodes[0];
+        turnstileContainer.classList.remove("float-end");
+        turnstileContainer.classList.add("float-start");
+
+        const submitButton = form.querySelector("button.o_appointment_form_confirm_btn");
+        this._turnstile.addSpinnerNoMangle(submitButton);
+        turnstileNodes.insertAfter(submitButton);
+        this._turnstile.renderTurnstile(turnstileNodes);
+
         return true;
     },
 });
