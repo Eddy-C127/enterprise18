Eddy-C127/enterprise18

commit e17144385067e660347c8e92b04d384a28001e1c

[FIX] documents(,_spreadsheet): ensure shortcuts handler integrity
Shortcuts held incomplete values: file_size was missing (very
visible in list view), handler was missing for spreadsheets.

Note that a constraint for shortcut `handler` can only be
added in master, but it's not too bad as we can read
`shortcutTarget` to get the correct handler on opening the
document even if the preview is broken.

It also solves the issue where the list renderer is embedded
as `shortcutTarget` is not known when opening a dialog to
search for a document (reproduce: open `Parent folder`
"Search More" in "Configure"/"Details").

Task-4387563

Part-of: odoo/enterprise#75696
Signed-off-by: Patrick Hoste (pko) <pko@odoo.com>

documents/models/documents_document.py
@@ -52,9 +52,9 @@ class Document(models.Model):
     raw = fields.Binary(related='attachment_id.raw', related_sudo=True, readonly=False, prefetch=False)
     file_extension = fields.Char('File Extension', copy=True, store=True, readonly=False,
                                  compute='_compute_file_extension', inverse='_inverse_file_extension')
-    file_size = fields.Integer(related='attachment_id.file_size', store=True)
+    file_size = fields.Integer(compute='_compute_file_size', store=True)
     checksum = fields.Char(related='attachment_id.checksum')
-    mimetype = fields.Char(related='attachment_id.mimetype')
+    mimetype = fields.Char(related='attachment_id.mimetype', inverse='_inverse_mimetype')
     res_model = fields.Char('Resource Model', compute="_compute_res_record", recursive=True,
                             inverse="_inverse_res_model", store=True)
     res_id = fields.Many2oneReference('Resource ID', compute="_compute_res_record", recursive=True,
@@ -210,17 +210,33 @@ class Document(models.Model):
         for record in accessible_records - folders:
             record.display_name = record.name
 
-    @api.depends('name', 'type')
+    @api.depends('name', 'type', 'shortcut_document_id.name')
     def _compute_file_extension(self):
         for record in self:
             if record.type != 'binary':
                 record.file_extension = False
+            elif record.shortcut_document_id.name:
+                file_extension = _sanitize_file_extension(get_extension(record.shortcut_document_id.name.strip()))
+                record.file_extension = file_extension or False
             elif record.name:
                 record.file_extension = _sanitize_file_extension(get_extension(record.name.strip())) or False
 
+    @api.depends('attachment_id', 'shortcut_document_id.attachment_id')
+    def _compute_file_size(self):
+        shortcuts = self.filtered('shortcut_document_id')
+        for document in self - shortcuts:
+            document.file_size = document.attachment_id.file_size
+        for document in shortcuts:
+            document.file_size = document.shortcut_document_id.file_size
+
     def _inverse_file_extension(self):
         for record in self:
-            record.file_extension = _sanitize_file_extension(record.file_extension) if record.file_extension else False
+            file_extension = _sanitize_file_extension(record.file_extension) if record.file_extension else False
+            (record | record.shortcut_ids).file_extension = file_extension
+
+    def _inverse_mimetype(self):
+        for record in self.filtered('shortcut_ids'):
+            record.shortcut_ids.mimetype = record.mimetype
 
     @api.constrains('shortcut_document_id', 'shortcut_ids', 'type', 'folder_id', 'children_ids', 'company_id')
     def _check_shortcut_fields(self):
@@ -674,11 +690,8 @@ class Document(models.Model):
         return self.sudo().create([{
             "folder_id": location.id,
             "shortcut_document_id": document.id,
-            "name": document.name,
-            "type": document.type,
             "access_internal": document.access_internal or 'view',
             "access_via_link": document.access_via_link or 'none',
-            "company_id": document.company_id.id,
             "access_ids": [
                 Command.create({
                     "partner_id": access.partner_id.id,
@@ -686,9 +699,17 @@ class Document(models.Model):
                 })
                 for access in document.access_ids if access.role
             ],
-            "is_multipage": document.is_multipage,
-            "is_access_via_link_hidden": document.is_access_via_link_hidden,
+            **{
+                field_name: (value.id if isinstance((value := document[field_name]), models.Model) else value)
+                for field_name in self._get_shortcuts_copy_fields()
+            }
         } for document in self]).sudo(False)
+
+    @api.model
+    def _get_shortcuts_copy_fields(self):
+        # Note that current simple usage in action_create_shortcut supports scalar and m2o fields.
+        return {'company_id', 'file_size', 'file_extension', 'is_access_via_link_hidden', 'is_multipage', 'mimetype',
+                'name', 'partner_id', 'type'}
 
     def action_update_access_rights(self, access_internal=None, access_via_link=None, is_access_via_link_hidden=None,
                                     partners=None, notify=False, message=""):

documents/static/src/views/documents_type_icon/documents_type_icon.xml
@@ -2,9 +2,8 @@
 <templates xml:space="preserve">
 
     <t t-name="documents.DocumentsTypeIcon">
-        <t t-set="recordOrShortcutTarget" t-value="this.props.record.shortcutTarget"/>
-        <t t-set="mimetype" t-value="recordOrShortcutTarget.data['mimetype']"/>
-        <t t-set="type" t-value="recordOrShortcutTarget.data['type']"/>
+        <t t-set="mimetype" t-value="this.props.record.data['mimetype']"/>
+        <t t-set="type" t-value="this.props.record.data['type']"/>
 
         <i t-if="type === 'folder'" class="fa fa-folder-o ms-1" title="Folder"/>
         <i t-elif="type === 'url'" class="fa fa-link ms-1" title="Link"/>

documents/tests/test_documents_document.py
@@ -13,8 +13,7 @@ from odoo.tests import users
 from .test_documents_common import TransactionCaseDocuments, GIF, TEXT
 
 DATA = "data:application/zip;base64,R0lGODdhAQABAIAAAP///////ywAAAAAAQABAAACAkQBADs="
-file_a = {'name': 'doc.zip', 'data': 'data:application/zip;base64,R0lGODdhAQABAIAAAP///////ywAAAAAAQABAAACAkQBADs='}
-file_b = {'name': 'icon.zip', 'data': 'data:application/zip;base64,R0lGODdhAQABAIAAAP///////ywAAAAAAQABAAACAkQBADs='}
+file_a = {'name': 'doc.zip', 'datas': 'data:application/zip;base64,R0lGODdhAQABAIAAAP///////ywAAAAAAQABAAACAkQBADs='}
 
 
 class TestCaseDocuments(TransactionCaseDocuments):
@@ -35,6 +34,24 @@ class TestCaseDocuments(TransactionCaseDocuments):
             ('partner_id', '=', self.env.user.partner_id.id),
         ])
         self.assertTrue(access)
+
+    def test_documents_action_create_shortcut(self):
+        # Make sure we test copying m2o too
+        self.document_gif.partner_id = self.env.user.partner_id
+        shortcut = self.document_gif.action_create_shortcut()
+        original_file_size = self.document_gif.file_size
+        for field_name in self.env['documents.document']._get_shortcuts_copy_fields():
+            with self.subTest(field_name=field_name):
+                self.assertEqual(shortcut[field_name], self.document_gif[field_name])
+        attachment = self.env['ir.attachment'].create({
+            **file_a,
+            'res_model': 'documents.document',
+            'res_id': 0,
+        })
+        self.document_gif.attachment_id = attachment
+        self.assertNotEqual(self.document_gif.file_size, original_file_size)
+        self.assertEqual(shortcut.file_size, self.document_gif.file_size)
+        self.assertEqual(shortcut.file_extension, self.document_gif.file_extension)
 
     def test_documents_create_from_attachment(self):
         """

documents_spreadsheet/models/documents_document.py
@@ -351,6 +351,10 @@ class Document(models.Model):
             docs = docs[offset:]
         return docs.read(["display_name", "thumbnail"])
 
+    @api.model
+    def _get_shortcuts_copy_fields(self):
+        return super()._get_shortcuts_copy_fields() | {'handler'}
+
     def clone_xlsx_into_spreadsheet(self, archive_source=False):
         """Clone an XLSX document into a new document with its content unzipped, and return the new document id"""
         self.ensure_one()

documents_spreadsheet/static/src/documents_view/documents_spreadsheet_controller_mixin.js
@@ -38,15 +38,17 @@ export const DocumentsSpreadsheetControllerMixin = () => ({
      * @override
      */
     async _onOpenDocumentsPreview({ mainDocument }) {
-        if (["spreadsheet", "frozen_spreadsheet"].includes(mainDocument.data.handler)) {
+        const mainDocumentOrTarget = mainDocument.shortcutTarget;
+        if (["spreadsheet", "frozen_spreadsheet"].includes(mainDocumentOrTarget.data.handler)) {
             this.action.doAction({
                 type: "ir.actions.client",
                 tag: "action_open_spreadsheet",
                 params: {
-                    spreadsheet_id: mainDocument.resId,
+                    spreadsheet_id: mainDocumentOrTarget.resId,
                 },
             });
-        } else if (XLSX_MIME_TYPES.includes(mainDocument.data.mimetype)) {
+        } else if (XLSX_MIME_TYPES.includes(mainDocumentOrTarget.data.mimetype)) {
+            // Keep MainDocument as `active` can be different for shortcut and target.
             if (!mainDocument.data.active) {
                 this.dialogService.add(ConfirmationDialog, {
                     title: _t("Restore file?"),
@@ -67,7 +69,7 @@ export const DocumentsSpreadsheetControllerMixin = () => ({
                     title: _t("Excel file preview"),
                     cancel: () => {},
                     cancelLabel: _t("Discard"),
-                    documentId: mainDocument.resId,
+                    documentId: mainDocumentOrTarget.resId,
                     confirmLabel: _t("Open with Odoo Spreadsheet"),
                 });
             }

documents_spreadsheet/tests/test_spreadsheet.py
@@ -121,6 +121,12 @@ class SpreadsheetDocuments(SpreadsheetTestCommon):
         self.assertEqual(frozen_spreadsheet.access_internal, 'view')
         self.assertEqual(frozen_spreadsheet.access_ids.role, 'view')
 
+    def test_action_create_shortcut(self):
+        self.archive_existing_spreadsheet()
+        document = self.create_spreadsheet()
+        shortcut = document.action_create_shortcut()
+        self.assertEqual(shortcut.handler, "spreadsheet")
+
     def archive_existing_spreadsheet(self):
         """Existing spreadsheet in the database can influence some test results"""
         self.env["documents.document"].search([("handler", "=", "spreadsheet")]).active = False

commit 1f430b41c9cbedca56a24550c5cbda4b9e28678b

[FIX] documents: fix creating and deleting shortcuts
Reproduce:
1. Create a shortcut for a folder.
2. Move the shortcut to the trash.
3. Go to the trash and try deleting the shortcut.
4. You get a 'Missing Record' message.

This occurred because e1714438 introduced a bug that
creates attachments for shortcuts.
(The mimetype is a related field so it cannot be "synced" with the target
as it is not worth creating attachments for this.)

Also, removing two useless tests setups (feature change) and
completing the document unlink test for res_model `False`.

Task-4433373

Part-of: odoo/enterprise#76220
Signed-off-by: Stéphane Debauche (std) <std@odoo.com>

documents/models/documents_document.py
@@ -54,7 +54,7 @@ class Document(models.Model):
                                  compute='_compute_file_extension', inverse='_inverse_file_extension')
     file_size = fields.Integer(compute='_compute_file_size', store=True)
     checksum = fields.Char(related='attachment_id.checksum')
-    mimetype = fields.Char(related='attachment_id.mimetype', inverse='_inverse_mimetype')
+    mimetype = fields.Char(related='attachment_id.mimetype')
     res_model = fields.Char('Resource Model', compute="_compute_res_record", recursive=True,
                             inverse="_inverse_res_model", store=True)
     res_id = fields.Many2oneReference('Resource ID', compute="_compute_res_record", recursive=True,
@@ -233,10 +233,6 @@ class Document(models.Model):
         for record in self:
             file_extension = _sanitize_file_extension(record.file_extension) if record.file_extension else False
             (record | record.shortcut_ids).file_extension = file_extension
-
-    def _inverse_mimetype(self):
-        for record in self.filtered('shortcut_ids'):
-            record.shortcut_ids.mimetype = record.mimetype
 
     @api.constrains('shortcut_document_id', 'shortcut_ids', 'type', 'folder_id', 'children_ids', 'company_id')
     def _check_shortcut_fields(self):
@@ -710,7 +706,7 @@ class Document(models.Model):
     @api.model
     def _get_shortcuts_copy_fields(self):
         # Note that current simple usage in action_create_shortcut supports scalar and m2o fields.
-        return {'company_id', 'file_size', 'file_extension', 'is_access_via_link_hidden', 'is_multipage', 'mimetype',
+        return {'company_id', 'file_size', 'file_extension', 'is_access_via_link_hidden', 'is_multipage',
                 'name', 'partner_id', 'type'}
 
     def action_update_access_rights(self, access_internal=None, access_via_link=None, is_access_via_link_hidden=None,

documents/static/src/views/documents_type_icon/documents_type_icon.xml
@@ -2,7 +2,8 @@
 <templates xml:space="preserve">
 
     <t t-name="documents.DocumentsTypeIcon">
-        <t t-set="mimetype" t-value="this.props.record.data['mimetype']"/>
+        <t t-set="recordOrShortcutTarget" t-value="this.props.record.shortcutTarget || this.props.record"/>
+        <t t-set="mimetype" t-value="recordOrShortcutTarget.data['mimetype']"/>
         <t t-set="type" t-value="this.props.record.data['type']"/>
 
         <i t-if="type === 'folder'" class="fa fa-folder-o ms-1" title="Folder"/>

documents/tests/test_documents_document.py
@@ -39,6 +39,7 @@ class TestCaseDocuments(TransactionCaseDocuments):
         # Make sure we test copying m2o too
         self.document_gif.partner_id = self.env.user.partner_id
         shortcut = self.document_gif.action_create_shortcut()
+        self.assertFalse(shortcut.attachment_id)
         original_file_size = self.document_gif.file_size
         for field_name in self.env['documents.document']._get_shortcuts_copy_fields():
             with self.subTest(field_name=field_name):
@@ -113,30 +114,6 @@ class TestCaseDocuments(TransactionCaseDocuments):
                 'folder_id': folder.id,
                 'type': 'binary',
             } for folder in folders])
-
-    def test_documents_share_links(self):
-        """
-        Tests document share links
-        """
-        # todo: transform into testing sharing a shortcut document with expiration
-        # by Folder
-        pass
-
-    def test_documents_share_popup(self):
-        shared_folder = self.env['documents.document'].create({
-            'type': 'folder',
-            'name': 'share folder',
-            'children_ids': [
-                Command.create({'type': 'binary', 'datas': GIF, 'name': 'file.gif', 'mimetype': 'image/gif'}),
-                Command.create({'type': 'url', 'url': 'https://odoo.com'}),
-            ],
-        })
-        share_tag = self.env['documents.tag'].create({
-            'name': "share category > share tag",
-        })
-        shared_folder.children_ids[0].tag_ids = [Command.set(share_tag.ids)]
-        # todo
-        # self.assertEqual(shared_folder.links_count, 0, "There should be no links counted in this share")
 
     def test_default_res_id_model(self):
         """
@@ -319,7 +296,7 @@ class TestCaseDocuments(TransactionCaseDocuments):
         """
         Tests a documents.document unlink method.
         Attachments should be deleted when related documents are deleted,
-        for which res_model is not 'documents.document'.
+        for which res_model is not 'documents.document' or `False`.
 
         Test case description:
             Case 1:
@@ -331,21 +308,23 @@ class TestCaseDocuments(TransactionCaseDocuments):
             Case 2:
             - ensure the existing flow for res_model 'documents.document'
               does not break.
-        """
-        document = self.env['documents.document'].create({
-            'datas': GIF,
-            'folder_id': self.folder_b.id,
-            'res_model': 'res.partner',
-        })
-        self.assertTrue(document.attachment_id.exists(), 'the attachment should exist')
-        attachment = document.attachment_id
-        document.unlink()
-        self.assertFalse(attachment.exists(), 'the attachment should not exist')
 
-        self.assertTrue(self.document_txt.attachment_id.exists(), 'the attachment should exist')
-        attachment_a = self.document_txt.attachment_id
-        self.document_txt.unlink()
-        self.assertFalse(attachment_a.exists(), 'the attachment should not exist')
+            Case 3:
+            - ensure the existing flow for res_model `False` does not break.
+        """
+        documents = self.env['documents.document'].create([{
+                'datas': GIF,
+                'folder_id': self.folder_b.id,
+                'res_model': res_model,
+            } for res_model in ('res.partner', 'documents.document', False)
+        ])
+        documents[2].res_model = False
+        for document in documents:
+            with self.subTest(res_model=document.res_model):
+                self.assertTrue(document.attachment_id.exists())
+                attachment = document.attachment_id
+                document.unlink()
+                self.assertFalse(attachment.exists())
 
     def test_archive_and_unarchive_document(self):
         self.document_txt.action_archive()