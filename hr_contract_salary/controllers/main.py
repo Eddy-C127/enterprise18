Eddy-C127/enterprise18

commit 8e50071471d194bc8b594a79a95fe951cd5055ab

[IMP] hr_contract_salary: Conditionally require document fields
Document fields in the salary configurator now check for existing files
on the current contract. Fields are set to required only if no
associated file is detected on the contract, otherwise we just show
the already existed file.

Task-3468245

closes odoo/enterprise#64298

X-original-commit: 0b03146e4e2a901e58c2aae8cc3edc7736312ee0
Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

hr_contract_salary/controllers/main.py
@@ -312,6 +312,7 @@ class HrContractSalary(http.Controller):
                 else:
                     content = False
                 initial_values[personal_info.field] = content
+                initial_values[personal_info.field + '_filename'] = contract[personal_info.field + '_filename'] or personal_info.field
             else:
                 initial_values[personal_info.field] = target[personal_info.field] if personal_info.field in target else ''
 
@@ -520,17 +521,7 @@ class HrContractSalary(http.Controller):
                 continue
 
             if field_name in employee_infos and personal_info.applies_on == 'employee':
-                special_document_fields = ('id_card', 'driving_license', 'mobile_invoice', 'sim_card', 'internet_invoice')
-                if employee._origin and field_name in special_document_fields:
-                    attachment_create_vals.append({
-                        'name': field_name,
-                        'res_model': 'hr.employee',
-                        'res_id': employee.id,
-                        'type': 'binary',
-                        'datas': employee_infos.get(field_name),
-                    })
-                else:
-                    employee_vals[field_name] = resolve_value(field_name, employee_infos)
+                employee_vals[field_name] = resolve_value(field_name, employee_infos)
             elif field_name in bank_account_infos and personal_info.applies_on == 'bank_account':
                 bank_account_vals[field_name] = resolve_value(field_name, bank_account_infos)
 

hr_contract_salary/views/hr_contract_salary_templates.xml
@@ -411,8 +411,16 @@
                 t-att-name="personal_info.field"
                 t-att-applies-on="personal_info.applies_on"
                 t-att-class="'form-control document personal_info' + (' benefit_input' if personal_info.impacts_net_salary else '')"
-                t-att-required="personal_info.is_required and not hidden_children"
+                t-att-required="personal_info.is_required and not initial_values[personal_info.field]"
                 t-att-accept="document_accept"/>
+            <t t-if="initial_values[personal_info.field]">
+                <span class="py-10 px-10">
+                    Existing file:
+                    <a t-att-href="initial_values[personal_info.field]" download="">
+                        <i class="fa fa-download"/>
+                    </a>
+                </span>
+            </t>
         </t>
         <t t-if="personal_info.display_type == 'checkbox'">
             <label class="hr_cs_control hr_cs_control_checkbox"><t t-esc="personal_info.name"/>

commit 0f4e8ce54be746db02e9f4a6974e387d90608a08

[FIX] hr_contract_salary: Custom document field not working
Steps:
- Create a new file field on employee
- Open salary package configurator / personal info
- Create a new info with the created field
    - category: personal documents
    - display type: document
- Open Employee/Abigail
- Open its contract
- Generate an offer
    - 17
        - Send by email
        - From the generated template, copy the button link
        -  Open the copied link in another tab
    - master
        - Just open the Link field

Actual result:
- KeyError for the filename of the new file field

Expected result:
- No error
- Filename field is also used when using a binary field
- File input is well show in the website configurator

opw-4382086

Caused by https://github.com/odoo/enterprise/commit/8e50071471d194bc8b594a79a95fe951cd5055ab & https://github.com/odoo/enterprise/commit/6f40549b98e65f813456442f7df35e9349a7639c

closes odoo/enterprise#75868

Signed-off-by: Jurgen Gjini (jugj) <jugj@odoo.com>

hr_contract_salary/controllers/main.py
@@ -310,7 +310,13 @@ class HrContractSalary(http.Controller):
                 else:
                     content = False
                 initial_values[personal_info.field] = content
-                initial_values[personal_info.field + '_filename'] = contract[personal_info.field + '_filename'] or personal_info.field
+                filename_field = personal_info.field + '_filename'
+                if filename_field in contract._fields:
+                    initial_values[personal_info.field + '_filename'] = contract[filename_field]
+                elif filename_field in target:
+                    initial_values[personal_info.field + '_filename'] = target[filename_field]
+                else:
+                    initial_values[personal_info.field + '_filename'] = personal_info.field
             else:
                 initial_values[personal_info.field] = target[personal_info.field] if personal_info.field in target else ''
 