c5b829b671089abefb76eb6bda51e2adb91765f6
Abdelrahman Mahmoud (amah) <amah@odoo.com>
2025-03-05 09:24:37 +0100

    [FIX] l10n_be_hr_contract_salary: drop contract_type_id default
    Issue: When an offer is generated for an employee from
    Company A, the Contract Type field is incorrectly fetching a
    value from Company B. This causes an access error
    
    Solve: drop the default value from be localization to let the user
    choose the correct value from the available values
    for the company
    
    closes odoo/enterprise#80740
    
    Task: 4625747
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- l10n_be_hr_contract_salary/wizard/generate_simulation_link.py
+++ l10n_be_hr_contract_salary/wizard/generate_simulation_link.py
@@ -7,9 +7,7 @@ from odoo import api, fields, models, _
 class GenerateSimulationLink(models.TransientModel):
     _inherit = 'generate.simulation.link'
 
-    contract_type_id = fields.Many2one('hr.contract.type', "Contract Type",
-                                       default=lambda self: self.env.ref('l10n_be_hr_payroll.l10n_be_contract_type_cdi',
-                                                                         raise_if_not_found=False))
+    contract_type_id = fields.Many2one('hr.contract.type', "Contract Type")
 
     new_car = fields.Boolean(string="Force New Cars List", help="The employee will be able to choose a new car even if the maximum number of used cars available is reached.")
     show_new_car = fields.Boolean(compute='_compute_show_new_car', store=True)
