5fa7380d8cd430c96294168e95efbc26a342dae4
Hesham Saleh <hsal@odoo.com>
2025-03-25 11:58:39 +0000

    [FIX] account_reports: Fixed reports scroll position on navigation
    Since 17.0, and before this commit, when the user navigates from an account report to another page
    and then comes back to the report through the breadcrumb, the user is brought to the top of the report.
    In 16.0, the user would be brought to the same position they were looking at.
    This commit brings this behavior back.
    
    task-4578468
    
    closes odoo/enterprise#82465
    
    X-original-commit: 1ae81683b47a0cc8e27663cd34ef415ee261aaf0
    Signed-off-by: Habib Ayob (ayh) <ayh@odoo.com>
    Signed-off-by: Hesham Saleh (hsal) <hsal@odoo.com>

--- account_reports/static/src/components/account_report/account_report.js
+++ account_reports/static/src/components/account_report/account_report.js
@@ -4,7 +4,7 @@ import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 import { ControlPanel } from "@web/search/control_panel/control_panel";
 
-import { Component, onWillStart, useState, useSubEnv } from "@odoo/owl";
+import { Component, onWillStart, useRef, useState, useSubEnv } from "@odoo/owl";
 
 import { AccountReportController } from "@account_reports/components/account_report/controller";
 import { AccountReportButtonsBar } from "@account_reports/components/account_report/buttons_bar/buttons_bar";
@@ -41,8 +41,9 @@ export class AccountReport extends Component {
     static defaultComponentsMap = [];
 
     setup() {
-
+        this.rootRef = useRef("root");
         useSetupAction({
+            rootRef: this.rootRef,
             getLocalState: () => {
                 return {
                     keep_journal_groups_options: true,  // used when using the breadcrumb
